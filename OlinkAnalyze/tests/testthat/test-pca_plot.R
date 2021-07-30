998d9cc1e55a5e02ca9589ec140d3f787870ebaa
Pascal Pucholt <pascal.pucholt@olink.com>
2021-07-30 14:51:51 +0200

    test that no warning is thrown by olink_pca_plot()
    https://testthat.r-lib.org/reference/expect_error.html

--- OlinkAnalyze/tests/testthat/test-pca_plot.R
+++ OlinkAnalyze/tests/testthat/test-pca_plot.R
@@ -2,3 +2,8 @@ test_that("plot output", {
   p <- olink_pca_plot(npx_data1 %>% filter(!str_detect(SampleID,"CONTROL")))
   vdiffr::expect_doppelganger("PCA plot", p)
 })
+
+
+test_that("No warning", {
+  expect_warning(olink_pca_plot(npx_data1 %>% filter(!str_detect(SampleID,"CONTROL"))), regexp = NA)
+})
