32a4da5168bd5c521a3746a196ad415d378a4325
Simon Forsberg <sf9@princeton.edu>
2021-08-27 20:12:06 +0200

    test for olink_lmer

---
+++ OlinkAnalyze/tests/testthat/test-linear_mixed_model.R
@@ -0,0 +1,29 @@
+#Load reference results
+refRes_file <- '../data/refResults.RData'
+load(refRes_file)
+
+#Run olink_lmer
+lmer_results_1 <- olink_lmer(df = npx_data1,
+                             variable = c('Treatment', "Time"),
+                             random = "Subject") %>%
+  mutate(id = as.character(OlinkID)) %>%
+  arrange(id, term) %>% #Since OlinkID is not unique here (=> ties), term is used to break the ties
+  select(-id)
+
+#Run olink_lmer_posthoc
+lmer_results_1_posthoc <- olink_lmer_posthoc(df = npx_data1,
+                                             variable = c('Treatment', "Time"),
+                                             random = "Subject",
+                                             olinkid_list = {lmer_results_1 %>%
+                                                 dplyr::filter(term == 'Treatment:Time') %>%
+                                                 dplyr::filter(Threshold == 'Significant') %>%
+                                                 dplyr::pull(OlinkID)} ,
+                                             effect = c('Treatment', "Time")) %>%
+  mutate(id = as.character(OlinkID)) %>%
+  arrange(id, contrast) %>%
+  select(-id)
+
+test_that("olink_lmer works", {
+  expect_equal(lmer_results_1, ref_results$lmer_results_1)
+  expect_error(olink_lmer(npx_data1))
+})
