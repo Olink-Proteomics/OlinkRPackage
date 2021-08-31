fd341908eca5fce129c99c42c2f93fbf8c8ad1fd
Simon Forsberg <sf9@princeton.edu>
2021-08-31 22:08:48 +0200

    A few more tests for olink_pca_plot

--- OlinkAnalyze/tests/testthat/test-pca_plot.R
+++ OlinkAnalyze/tests/testthat/test-pca_plot.R
@@ -1,8 +1,27 @@
-sampleSubset <- c('A1', 'A10', 'A11', 'A12', 'A13', 'A14', 'B79', 'B8', 'B9')
+set.seed(10)
+#Load reference results
+refRes_file <- '../data/refResults.RData'
+load(refRes_file)
+
 pca_plot <- npx_data1 %>%
-  dplyr::filter(SampleID %in% sampleSubset) %>%
+  mutate(SampleID = paste(SampleID, "_", Index, sep = "")) %>%
   olink_pca_plot()
+
+pca_plot_treatCol <- npx_data1 %>%
+  mutate(SampleID = paste(SampleID, "_", Index, sep = "")) %>%
+  filter(!is.na(Treatment)) %>% #Or else, a warning shows up in the test results
+  olink_pca_plot(color_g = 'Treatment')
+
+pca_plot_treatCol_topLoadings <- npx_data1 %>%
+  mutate(SampleID = paste(SampleID, "_", Index, sep = "")) %>%
+  filter(!is.na(Treatment)) %>% #Or else, a warning shows up in the test results
+  olink_pca_plot(color_g = 'Treatment',
+                 loadings_list = {ref_results$ttestresults %>%
+                     head(5) %>%
+                     pull(OlinkID)})
 
 test_that("olink_pca_plot works", {
   vdiffr::expect_doppelganger('PCA plot', pca_plot)
+  vdiffr::expect_doppelganger('PCA plot color by treatment', pca_plot_treatCol)
+  vdiffr::expect_doppelganger('PCA plot with loadings', pca_plot_treatCol_topLoadings)
 })
