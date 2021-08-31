47a0f998ef0830593f5ce2463c71e085a849d64b
Simon Forsberg <sf9@princeton.edu>
2021-08-31 19:53:39 +0200

    Update of plotting tests
    Changed all the plotting tests to rely on vdiffr::expect_doppelganger, rather than "manual" comparisons of ggplot objects

--- OlinkAnalyze/tests/testthat/test-dist_plot.R
+++ OlinkAnalyze/tests/testthat/test-dist_plot.R
@@ -1,17 +1,8 @@
-#Load reference results
-refImages_file <- '../data/refImages.RData'
-load(refImages_file)
-
-#Run function
-sampleSubset <- c('A1', 'A10', 'A11', 'A12', 'A13', 'A14', 'B79', 'B8', 'B9')
+sampleSubset <- c('A1', 'A10', 'A11', 'A12', 'A13', 'A14', 'B79', 'B8', 'B9') #To keep the file size down
 distribution_plot <- npx_data1 %>%
   dplyr::filter(SampleID %in% sampleSubset) %>%
   olink_dist_plot()
 
 test_that("olink_dist_plot works", {
-  expect_s3_class(distribution_plot, class = "ggplot")
-  expect_equal(distribution_plot$data, ref_images$distribution_plot$data)
-  expect_equal(distribution_plot$layers[[1]]$geom_params, ref_images$distribution_plot$layers[[1]]$geom_params)
-  expect_equal(ggplot2::ggplot_build(distribution_plot)$data[[1]],
-               ggplot2::ggplot_build(ref_images$distribution_plot)$data[[1]])
+  vdiffr::expect_doppelganger('Distribution plot', distribution_plot)
 })
--- OlinkAnalyze/tests/testthat/test-pca_plot.R
+++ OlinkAnalyze/tests/testthat/test-pca_plot.R
@@ -1,16 +1,8 @@
-#Load reference results
-refImages_file <- '../data/refImages.RData'
-load(refImages_file)
-
-#Run function
 sampleSubset <- c('A1', 'A10', 'A11', 'A12', 'A13', 'A14', 'B79', 'B8', 'B9')
 pca_plot <- npx_data1 %>%
   dplyr::filter(SampleID %in% sampleSubset) %>%
   olink_pca_plot()
 
 test_that("olink_pca_plot works", {
-  expect_s3_class(pca_plot, class = "ggplot")
-  expect_equal(pca_plot[[1]], ref_images$pca_plot[[1]])
-  expect_equal(ggplot2::ggplot_build(pca_plot)$data[[1]],
-               ggplot2::ggplot_build(ref_images$pca_plot)$data[[1]])
+  vdiffr::expect_doppelganger('PCA plot', pca_plot)
 })
