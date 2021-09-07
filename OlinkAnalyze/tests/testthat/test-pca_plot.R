e6b60fbee797c4f17f7bebd65478a22c37ce2b7e
Pascal Pucholt <pascal.pucholt@olink.com>
2021-09-07 11:14:12 +0200

    unit test for PCA plot with drop_assays and drop_samples

--- OlinkAnalyze/tests/testthat/test-pca_plot.R
+++ OlinkAnalyze/tests/testthat/test-pca_plot.R
@@ -21,8 +21,19 @@ pca_plot_treatCol_topLoadings <- npx_data1 %>%
                      pull(OlinkID)})
 
 test_that("olink_pca_plot works", {
+
+  # Two Warnings thrown: for dropped assays and droppes samples
+  expect_warning(
+    expect_warning(
+      pca_plot_drop <- npx_data1 %>%
+      mutate(SampleID = paste(SampleID, "_", Index, sep = "")) %>%
+      olink_pca_plot(drop_assays = TRUE, drop_samples = TRUE)
+    )
+  )
+
   skip_on_ci()
   vdiffr::expect_doppelganger('PCA plot', pca_plot)
   vdiffr::expect_doppelganger('PCA plot color by treatment', pca_plot_treatCol)
   vdiffr::expect_doppelganger('PCA plot with loadings', pca_plot_treatCol_topLoadings)
+  vdiffr::expect_doppelganger('PCA plot drop_assays and drop_samples', pca_plot_drop)
 })
