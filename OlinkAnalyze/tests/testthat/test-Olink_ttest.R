ac427c11c4a0ff3573714f82a2e0b0b51ea13e40
Pascal Pucholt <pascal.pucholt@olink.com>
2021-09-15 12:45:49 +0200

    Change test scope for paired t-test
    Only test for the content of the returned dataframe not for the names of the elements of the estimate column
    
    This change gives compatibility with a slight format change introduced in the current R develop release

--- OlinkAnalyze/tests/testthat/test-Olink_ttest.R
+++ OlinkAnalyze/tests/testthat/test-Olink_ttest.R
@@ -11,7 +11,7 @@ t.test_results_paired <- npx_data1 %>% #Paired t-test
 
 test_that("T-test function works", {
   expect_equal(t.test_results, ref_results$t.test_results)  # compare results to ref
-  expect_equal(t.test_results_paired, ref_results$t.test_results_paired)  # compare results to ref
+  expect_equal(as.matrix(t.test_results_paired), as.matrix(ref_results$t.test_results_paired))  # compare results to ref
   expect_error(olink_ttest(df = npx_data1)) # no input data
   expect_error(olink_ttest(npx_data1, "Time")) # more than 2 levels in the grouping variable
 })
