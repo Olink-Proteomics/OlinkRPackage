bd7d21d3a016d59edca7810b7d97ed03d25b19bf
Marianne Sandin <marianne.sandin@olink.com>
2021-09-29 16:48:46 +0200

    Bugfix of gg_repel according to Kanban card "Bug fix: Avoid hiding points plots (ggrepel)", update of snaps to reflect sentence case.

--- OlinkAnalyze/tests/testthat/test-olink_qc_plot.R
+++ OlinkAnalyze/tests/testthat/test-olink_qc_plot.R
@@ -1,5 +1,3 @@
-set.seed(10)
-
 qc_plot <- npx_data1 %>%
   mutate(SampleID = paste(SampleID, "_", Index, sep = "")) %>%
   olink_qc_plot()
@@ -9,6 +7,7 @@ qc_plot2 <- npx_data1 %>%
   olink_qc_plot(coloroption =  c('teal', 'pink'))
 
 test_that("olink_qc_plot works", {
+  skip_on_ci()
   vdiffr::expect_doppelganger('QC plot', qc_plot)
   vdiffr::expect_doppelganger('QC plot with coloroption', qc_plot2)
 })
