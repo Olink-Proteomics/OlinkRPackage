0e5a116ff96df899739534469eae818b93a110ff
kathy-nevola <72228735+kathy-nevola@users.noreply.github.com>
2022-04-11 15:47:57 -0400

    create pathway enrichment tests

---
+++ OlinkAnalyze/tests/testthat/test-olink_Pathway_Enrichment.R
@@ -0,0 +1,68 @@
+npx_df <- npx_data1 %>% 
+  dplyr::filter(!stringr::str_detect(SampleID, "CONTROL"))
+ttest_results <- olink_ttest(df = npx_df, variable = "Treatment")
+anova_results <- olink_anova(df = npx_df, variable = "Site")
+anova_posthoc_results <- olink_anova_posthoc(npx_df, variable = "Site", effect = "Site")
+lme_results <- olink_lmer_posthoc(npx_df, variable = "Time", random = "Site", effect = "Time")
+set.seed(123)
+tt_gsea <- olink_pathway_enrichment(npx_df, test_results = ttest_results)
+tt_gsea_reactome <- olink_pathway_enrichment(npx_df, test_results = ttest_results, ontology = "Reactome")
+tt_gsea_kegg <- olink_pathway_enrichment(npx_df, test_results = ttest_results, ontology = "KEGG")
+tt_gsea_go <- olink_pathway_enrichment(npx_df, test_results = ttest_results, ontology = "GO")
+
+tt_ora <- olink_pathway_enrichment(npx_df, test_results = ttest_results, method = "ORA", ontology = "MSigDb")
+tt_ora_reactome <- olink_pathway_enrichment(npx_df, test_results = ttest_results, method = "ORA", ontology = "Reactome")
+tt_ora_kegg <- olink_pathway_enrichment(npx_df, test_results = ttest_results, method = "ORA", ontology = "KEGG")
+tt_ora_go <- olink_pathway_enrichment(npx_df, test_results = ttest_results, method = "ORA", ontology = "GO")
+
+ttest_results_no_estimate <- ttest_results %>% dplyr::select(-estimate)
+
+test_that("T-test GSEA works", {
+  expect_equal(nrow(tt_gsea), 565)
+})
+
+test_that("Reactome GSEA works", {
+  expect_equal(nrow(tt_gsea_reactome), 18)
+})
+
+test_that("KEGG GSEA works", {
+  expect_equal(nrow(tt_gsea_kegg), 5)
+})
+
+test_that("GO GSEA works", {
+  expect_equal(nrow(tt_gsea_go), 387)
+})
+
+
+test_that("T-test ORA works", {
+  expect_equal(nrow(tt_ora), 366)
+})
+
+test_that("Reactome ORA works", {
+  expect_equal(nrow(tt_ora_reactome), 15)
+})
+
+test_that("KEGG ORA works", {
+  expect_equal(nrow(tt_ora_kegg), 3)
+})
+
+test_that("GO ORA works", {
+  expect_equal(nrow(tt_ora_go), 263)
+})
+
+
+test_that("Error if more than 1 contrast", {
+  expect_error(olink_pathway_enrichment(npx_df, anova_posthoc_results))
+})
+
+test_that("Nonsense method errors",{
+  expect_error(olink_pathway_enrichment(npx_df, anova_posthoc_results, method = "IRA"))
+})
+
+test_that("Unsupported databases flag",{
+  expect_error(olink_pathway_enrichment(npx_df, anova_posthoc_results, ontology = "WikiPathways"))
+})
+
+test_that("Estimate column must be present",{
+  expect_error(olink_pathway_enrichment(npx_df, ttest_results_no_estimate))
+})
