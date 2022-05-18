a6ab55508b56cae46ef9d70a1392734142118e2a
Pascal Pucholt <pascal.pucholt@olink.com>
2022-05-18 16:50:00 +0200

    Make example that depends on suggested package resilient

--- OlinkAnalyze/R/olink_Pathway_Enrichment.R
+++ OlinkAnalyze/R/olink_Pathway_Enrichment.R
@@ -1,31 +1,31 @@
-#' Performs pathway enrichment using over-representation analysis (ORA) or gene set enrichment analysis (GSEA) 
-#' 
-#' This function performs enrichment analysis based on statistical test results and full data using clusterProfiler's gsea and enrich functions for MSigDB. 
-#' 
-#' @details 
-#' MSigDB is subset if the  ontology argument is KEGG, GO, or Reactome. test_results must contain estimates for all assays. 
+#' Performs pathway enrichment using over-representation analysis (ORA) or gene set enrichment analysis (GSEA)
+#'
+#' This function performs enrichment analysis based on statistical test results and full data using clusterProfiler's gsea and enrich functions for MSigDB.
+#'
+#' @details
+#' MSigDB is subset if the  ontology argument is KEGG, GO, or Reactome. test_results must contain estimates for all assays.
 #' Posthoc results can be used but should be filtered for one contrast to improve interpretability.
 #' clusterProfiler is originally developed by Guangchuang Yu at the School of Basic Medical Sciences at Southern Medical University.
-#' 
-#' T Wu, E Hu, S Xu, M Chen, P Guo, Z Dai, T Feng, L Zhou, W Tang, L Zhan, X Fu, S Liu, X Bo, and G Yu. 
-#' clusterProfiler 4.0: A universal enrichment tool for interpreting omics data. The Innovation. 2021, 2(3):100141. 
+#'
+#' T Wu, E Hu, S Xu, M Chen, P Guo, Z Dai, T Feng, L Zhou, W Tang, L Zhan, X Fu, S Liu, X Bo, and G Yu.
+#' clusterProfiler 4.0: A universal enrichment tool for interpreting omics data. The Innovation. 2021, 2(3):100141.
 #' doi: 10.1016/j.xinn.2021.100141
-#' 
+#'
 #' \strong{A few notes on Pathway Enrichment with Olink Data}
-#' 
+#'
 #' It is important to note that sometimes the proteins that are assayed in Olink Panels
-#'  are related to specific biological areas and therefore do not represent an unbiased overview of the proteome as a whole. 
-#'  Pathways can only interpreted based on the background/context they came from. For this reason, an estimate for all assays measured must 
-#'  be provided. Furthermore, certain pathways cannot come up based on Olink's  coverage in this area.  Additionally, 
+#'  are related to specific biological areas and therefore do not represent an unbiased overview of the proteome as a whole.
+#'  Pathways can only interpreted based on the background/context they came from. For this reason, an estimate for all assays measured must
+#'  be provided. Furthermore, certain pathways cannot come up based on Olink's  coverage in this area.  Additionally,
 #'   if only the Inflammation panel was run, then the available pathways would be given based on a background
-#'   of proteins related to inflammation. Both ORA and GSEA can provide mechanistic and disease related insight and are best to use when 
-#'   trying to uncover pathways/annotations of interest. It is recommended to only use pathway enrichment for hypothesis generating data, which 
-#'   is more well suited for data on the Explore platform or on multiple Target 96 panels. For smaller lists of proteins it may be more informative to use biological annotation in directed research, 
+#'   of proteins related to inflammation. Both ORA and GSEA can provide mechanistic and disease related insight and are best to use when
+#'   trying to uncover pathways/annotations of interest. It is recommended to only use pathway enrichment for hypothesis generating data, which
+#'   is more well suited for data on the Explore platform or on multiple Target 96 panels. For smaller lists of proteins it may be more informative to use biological annotation in directed research,
 #'   to discover which significant assay are related to keywords of interest.
-#'  
-#'   
+#'
+#'
 #' @param data NPX data frame in long format with at least protein name (Assay), OlinkID, UniProt,SampleID, QC_Warning, NPX, and LOD
-#' @param test_results a dataframe of statistical test results including Adjusted_pval and estimate columns. 
+#' @param test_results a dataframe of statistical test results including Adjusted_pval and estimate columns.
 #' @param method Either "GSEA" (default) or "ORA"
 #' @param ontology Supports "MSigDb" (default), "KEGG", "GO", and "Reactome" as arguments. MSigDb contains C2 and C5 genesets. C2 and C5 encompass KEGG, GO, and Reactome.
 #' @param organism Either "human" (default) or "mouse"
@@ -33,7 +33,7 @@
 #' Columns for ORA include:
 #' \itemize{
 #'  \item{ID:} "character" Pathway ID from MSigDB
-#'  \item{Description:} "character" Description of Pathway from MSigDB  
+#'  \item{Description:} "character" Description of Pathway from MSigDB
 #'  \item{GeneRatio:} "character" ratio of input proteins that are annotated in a term
 #'  \item{BgRatio:} "character" ratio of all genes that are annotated in this term
 #'  \item{pvalue:} "numeric" p-value of enrichment
@@ -43,17 +43,17 @@
 #'  \item{geneID:} "character" list of input proteins (Gene Symbols) annotated in a term delimited by "/"
 #'  \item{Count:} "integer" Number of input proteins that are annotated in a term
 #' }
-#' 
+#'
 #' Columns for GSEA:
 #' \itemize{
 #'  \item{ID:} "character" Pathway ID from MSigDB
-#'  \item{Description:} "character" Description of Pathway from MSigDB  
+#'  \item{Description:} "character" Description of Pathway from MSigDB
 #'  \item{setSize:} "integer" ratio of input proteins that are annotated in a term
 #'  \item{enrichmentScore:} "numeric" Enrichment score, degree to which a gene set is over-represented at the top or
 #'  bottom of the ranked list of genes
 #'  \item{NES:} "numeric" Normalized Enrichment Score, normalized to account for differences in gene set size and in
 #'  correlations between gene sets and expression data sets. NES can be used to compare analysis results
-#'  across gene sets. 
+#'  across gene sets.
 #'  \item{pvalue:} "numeric" p-value of enrichment
 #'  \item{p.adjust:} "numeric" Adjusted p-value (Benjamini-Hochberg)
 #'  \item{qvalue:} "numeric" false discovery rate, the estimated probability that the normalized enrichment score represents
@@ -64,7 +64,7 @@
 #'  score is obtained. Signal represents the enrichment signal strength and combines the tag and list.
 #'  \item{core_enrichment:} "character" list of input proteins (Gene Symbols) annotated in a term delimited by "/"
 #' }
-#' 
+#'
 #' @examples
 #' \donttest{
 #' library(dplyr)
@@ -74,11 +74,13 @@
 #'   variable = "Treatment",
 #'   alternative = "two.sided"
 #' )
+#' try({ # This expression might fail if dependencies are not installed
 #' gsea_results <- olink_pathway_enrichment(data = npx_data1, test_results = ttest_results)
 #' ora_results <- olink_pathway_enrichment(
 #'   data = npx_data1,
 #'   test_results = ttest_results, method = "ORA"
 #' )
+#' }, silent = TRUE)
 #' }
 #' @seealso \itemize{
 #' \item{\code{\link[OlinkAnalyze:olink_pathway_heatmap]{olink_pathway_heatmap}} for generating a heat map of results}
@@ -95,41 +97,41 @@ olink_pathway_enrichment <- function(data, test_results, method = "GSEA", ontolo
   if(!requireNamespace("clusterProfiler", quietly = TRUE) ){
     stop(" Pathway enrichment requires clusterProfiler package.
          Please install clusterProfiler before continuing.
-         
+
          if (!require(\"BiocManager\", quietly = TRUE))
             install.packages(\"BiocManager\")
          BiocManager::install(\"clusterProfiler\")")
   }
-   
+
   if(!requireNamespace("msigdbr", quietly = TRUE)){
     stop(" Pathway enrichment requires msigdbr package.
          Please install msigdbr before continuing.
-         
+
          install.packages(\"msigdbr\")")
   }
   # Data Checks
-  
+
   if(length(unique(data$OlinkID)) != length(unique(test_results$OlinkID))) {
     warning("The number of Olink IDs in the data does not equal the number of Olink IDs in the test results.")
   }
-  
+
   if("contrast" %in% colnames(test_results) && length(unique(test_results$contrast)) > 1){
     stop("More than one contrast is specified in test results. Filter test_results for desired contrast.")
   }
   if(!("estimate" %in% colnames(test_results))){
     stop("Estimate column is not present in test results. Please check arguments.")
   }
-  
+
   if(!(method %in% c("GSEA", "ORA"))){
     stop("Method must be \"GSEA\" or \"ORA\".")
   }
-  
+
   if(!(ontology %in% c("MSigDb", "Reactome", "KEGG", "GO"))){
     stop("Ontology must be one of MSigDb, Reactome, KEGG, or GO.")
   }
   data2 <- data_prep(data = data)
   test_results2 <- test_prep(data = data2, test_results = test_results, organism = organism)
-  
+
   if (method == "ORA") {
     results <- ora_pathwayenrichment(
       test_results = test_results2, ontology = ontology,
@@ -182,7 +184,7 @@ gsea_pathwayenrichment <- function(geneList, ontology, organism) {
   } else if (organism == "mouse") {
     msig_df <- msigdbr::msigdbr(species = "Mus musculus", category = "C2") %>%
       rbind(msigdbr::msigdbr(species = "Mus musculus", category = "C5"))
-    
+
   } else {
     stop(print("organism should be \"human\" or \"mouse\""))
   }
@@ -201,7 +203,7 @@ gsea_pathwayenrichment <- function(geneList, ontology, organism) {
   msig_df <- msig_df  %>% dplyr::select(gs_name, gene_symbol)
   if(length(setdiff(names(geneList), msig_df$gene_symbol) != 0)){
      message(paste0(length(setdiff(names(geneList), msig_df$gene_symbol)),
-                        " assays are not found in the database. Please check the Assay names for the following assays:\n ", 
+                        " assays are not found in the database. Please check the Assay names for the following assays:\n ",
                         toString(setdiff(names(geneList), msig_df$gene_symbol))))
   }
   GSEA <- clusterProfiler::GSEA(geneList = geneList, TERM2GENE = msig_df, pvalueCutoff = 1)
@@ -210,8 +212,8 @@ gsea_pathwayenrichment <- function(geneList, ontology, organism) {
 ora_pathwayenrichment <- function(test_results, organism, ontology = ontology, pvalue_cutoff = 0.05, estimate_cutoff = 0) {
   sig_genes <- test_results %>%
     filter(Adjusted_pval < pvalue_cutoff) %>%
-    filter(abs(estimate) > estimate_cutoff) %>% 
-    distinct(Assay) %>% 
+    filter(abs(estimate) > estimate_cutoff) %>%
+    distinct(Assay) %>%
     pull(Assay)
   universe <- test_results %>%
     distinct(Assay) %>%
@@ -222,7 +224,7 @@ ora_pathwayenrichment <- function(test_results, organism, ontology = ontology, p
   } else if (organism == "mouse") {
     msig_df <- msigdbr::msigdbr(species = "Mus musculus", category = "C2") %>%
       rbind(msigdbr::msigdbr(species = "Mus musculus", category = "C5"))
-    
+
   } else {
     stop(print("organism should be \"human\" or \"mouse\""))
   }
@@ -241,7 +243,7 @@ ora_pathwayenrichment <- function(test_results, organism, ontology = ontology, p
   msig_df <- msig_df  %>% dplyr::select(gs_name, gene_symbol)
   if(length(setdiff(names(universe), msig_df$gene_symbol) != 0)){
     message(paste0(length(setdiff(names(geneList), msig_df$gene_symbol)),
-                   " assays are not found in the database. Please check the Assay names for the following assays:\n ", 
+                   " assays are not found in the database. Please check the Assay names for the following assays:\n ",
                    toString(setdiff(names(universe), msig_df$gene_symbol))))
   }
   ORA <- clusterProfiler::enricher(gene = sig_genes, universe = universe, TERM2GENE = msig_df, pvalueCutoff = 1)
