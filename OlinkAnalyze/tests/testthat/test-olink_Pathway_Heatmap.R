31b23abef9151095504e5b5b475cea766b7b9888
Pascal Pucholt <pascal.pucholt@olink.com>
2022-05-18 16:46:04 +0200

    Refactor pathway enrichment test to run only when possible

--- OlinkAnalyze/tests/testthat/test-olink_Pathway_Heatmap.R
+++ OlinkAnalyze/tests/testthat/test-olink_Pathway_Heatmap.R
@@ -3,19 +3,23 @@ npx_df <- npx_data1 %>% filter(!grepl('control',SampleID, ignore.case = TRUE))
 ttest_results <- olink_ttest(df=npx_df,
                              variable = 'Treatment',
                              alternative = 'two.sided')
-gsea_results <- olink_pathway_enrichment(data = npx_data1, test_results = ttest_results)
-ora_results <- olink_pathway_enrichment(data = npx_data1,
-                                        test_results = ttest_results, method = "ORA")
-gsea_heatmap<-olink_pathway_heatmap(enrich_results = gsea_results, test_results = ttest_results)
-ora_heatmap <- olink_pathway_heatmap(enrich_results = ora_results, test_results = ttest_results,
-                      method = "ORA", keyword = "cell")
 
 test_that("Valid Keyword needed", {
+  skip_if_not_installed("clusterProfiler")
+  gsea_results <- olink_pathway_enrichment(data = npx_data1, test_results = ttest_results)
   set.seed(123)
   expect_error(olink_pathway_heatmap(enrich_results = gsea_results, test_results = ttest_results, keyword = "asfhdlk"))
 })
 
 test_that("Plot works",{
+  skip_if_not_installed("clusterProfiler")
+  gsea_results <- olink_pathway_enrichment(data = npx_data1, test_results = ttest_results)
+  ora_results <- olink_pathway_enrichment(data = npx_data1,
+                                          test_results = ttest_results, method = "ORA")
+  gsea_heatmap<-olink_pathway_heatmap(enrich_results = gsea_results, test_results = ttest_results)
+  ora_heatmap <- olink_pathway_heatmap(enrich_results = ora_results, test_results = ttest_results,
+                                       method = "ORA", keyword = "cell")
+
   set.seed(123)
   skip_on_ci()
   vdiffr::expect_doppelganger("GSEA Heatmap", gsea_heatmap)
--- OlinkAnalyze/tests/testthat/test-olink_Pathway_Visualization.R
+++ OlinkAnalyze/tests/testthat/test-olink_Pathway_Visualization.R
@@ -3,20 +3,29 @@ npx_df <- npx_data1 %>% filter(!grepl('control',SampleID, ignore.case = TRUE))
 ttest_results <- olink_ttest(df=npx_df,
                              variable = 'Treatment',
                              alternative = 'two.sided')
-gsea_results <- olink_pathway_enrichment(data = npx_data1, test_results = ttest_results)
-ora_results <- olink_pathway_enrichment(data = npx_data1,
-                                      test_results = ttest_results, method = "ORA")
-gsea_vis<-olink_pathway_visualization(enrich_results = gsea_results)
-gsea_vis_keyword <- olink_pathway_visualization(enrich_results = gsea_results, keyword = "immune")
-ora_vis_terms <- olink_pathway_visualization(enrich_results = ora_results, method = "ORA", number_of_terms = 15)
-ora_vis_keyword <- olink_pathway_visualization(enrich_results = ora_results, method = "ORA", keyword = "vir")
 
 test_that("Valid Keyword needed", {
+  skip_if_not_installed("clusterProfiler")
+  gsea_results <- olink_pathway_enrichment(data = npx_data1, test_results = ttest_results)
+  ora_results <- olink_pathway_enrichment(data = npx_data1,
+                                          test_results = ttest_results, method = "ORA")
+
   expect_error(olink_pathway_visualization(enrich_results = gsea_results, keyword = "hfdklahfajikshf"))
   expect_error(olink_pathway_visualization(enrich_results = ora_results, method = "ORA", keyword = "hfdklahfajikshf"))
 })
 
 test_that("Plot works",{
+  skip_if_not_installed("clusterProfiler")
+
+  gsea_results <- olink_pathway_enrichment(data = npx_data1, test_results = ttest_results)
+  ora_results <- olink_pathway_enrichment(data = npx_data1,
+                                          test_results = ttest_results, method = "ORA")
+  gsea_vis<-olink_pathway_visualization(enrich_results = gsea_results)
+  gsea_vis_keyword <- olink_pathway_visualization(enrich_results = gsea_results, keyword = "immune")
+  ora_vis_terms <- olink_pathway_visualization(enrich_results = ora_results, method = "ORA", number_of_terms = 15)
+  ora_vis_keyword <- olink_pathway_visualization(enrich_results = ora_results, method = "ORA", keyword = "vir")
+
+
   set.seed(123)
   skip_on_ci()
   vdiffr::expect_doppelganger("GSEA Visualization", gsea_vis)
