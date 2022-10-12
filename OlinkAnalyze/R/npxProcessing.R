7e77be440ca230f484e3d1781c820bbed1de895f
Simon Forsberg <simon.kg.forsberg@gmail.com>
2022-10-12 09:48:32 +0200

    Update npxProcessing.R
    Moved the NPX=NA check into a new function called npxCheck to avoid redundancy. The intention is to eventually expand this function

--- OlinkAnalyze/R/npxProcessing.R
+++ OlinkAnalyze/R/npxProcessing.R
@@ -1,3 +1,5 @@
+#### Internal functions ####
+
 npxProcessing_forDimRed <- function(df, color_g, drop_assays, drop_samples, verbose){
   #### Set up plotting colors ####
   if (color_g == "QC_Warning"){
@@ -154,4 +156,26 @@ npxProcessing_forDimRed <- function(df, color_g, drop_assays, drop_samples, verb
               df_wide_matrix = df_wide_matrix,
               dropped_assays.na = dropped_assays.na,
               dropped_assays.missingness = dropped_assays.missingness))
+}
+
+#This function is called by various other functions to perform checks of NPX-data. For now, it only looks for assays which have all NPX=NA, but there are other redundant tasks that could be moved here
+npxCheck <- function(df){
+  #Identify assays that have only NA:s
+  all_nas <- df  %>%
+    dplyr::group_by(OlinkID) %>%
+    dplyr::summarise(n = dplyr::n(), n_na = sum(is.na(NPX))) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(n == n_na) %>%
+    dplyr::pull(OlinkID)
+
+  if(length(all_nas) > 0) {
+
+    warning(paste0('The assays ',
+                   paste(all_nas, collapse = ', '),
+                   ' have NPX=NA for all samples. They will not be tested.'),
+            call. = FALSE)
+
+  }
+
+  return(list(all_nas = all_nas))
 }
