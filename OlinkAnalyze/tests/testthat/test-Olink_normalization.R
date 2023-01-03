8b64efccbc34777dfeb9224dabe9ee6038ff4f77
kathy-nevola <72228735+kathy-nevola@users.noreply.github.com>
2023-01-03 16:54:34 -0500

    add unit tests

--- OlinkAnalyze/tests/testthat/test-Olink_normalization.R
+++ OlinkAnalyze/tests/testthat/test-Olink_normalization.R
@@ -21,6 +21,16 @@ normalization_results.bridged <- olink_normalization(df1 = npx_data1,
                                                      reference_project = '20200001') %>%
   filter(SampleID %in% sampleSubset)
 
+
+# Warning if column names arent the same but results still match
+npx_df2 <- OlinkAnalyze::npx_data2 %>% dplyr::mutate(Project = 'P2')
+npx_df_test<-OlinkAnalyze::npx_data1 %>%
+  dplyr::mutate(Sample_type = "Sample") %>%
+  dplyr::select(SampleID, Sample_type, Index, OlinkID, UniProt, Assay, MissingFreq, Panel_Version, PlateID, QC_Warning, LOD, NPX, Subject)
+overlap_samples2 <- intersect(npx_data1$SampleID, npx_data2$SampleID) %>%
+  data.frame() %>%
+  filter(!str_detect(., 'CONTROL_SAMPLE')) %>% #Remove control samples
+  pull(.)
 # Intensity normalization
 normalization_results.intensity <- olink_normalization(df1 = npx_data1,
                                                        df2 = npx_data2,
@@ -67,6 +77,12 @@ npxBridged_proj2ref <- olink_normalization(df1 = npx_data_format221010,
                                            reference_project = 'P2')
 
 test_that("olink_normalization works", {
+  expect_warning(expect_warning(olink_normalization(df1 = npx_df_test,
+                                     df2 = npx_df2,
+                                     overlapping_samples_df1 = overlap_samples2,
+                                     df1_project_nr = 'P1',
+                                     df2_project_nr = 'P2',
+                                     reference_project = 'P1')))
   expect_equal(normalization_results.bridged, ref_results$normalization_results.bridged)
   expect_equal(normalization_results.intensity, ref_results$normalization_results.intensity)
   expect_equal(normalization_results.subset, ref_results$normalization_results.subset)
