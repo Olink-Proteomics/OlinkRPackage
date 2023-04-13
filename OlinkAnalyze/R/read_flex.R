d5185d21648fa85c3ab928a19d5a3281528a494f
kathy-nevola <72228735+kathy-nevola@users.noreply.github.com>
2023-04-13 14:09:13 -0400

    Add support for QC data in flex files

--- OlinkAnalyze/R/read_flex.R
+++ OlinkAnalyze/R/read_flex.R
@@ -5,7 +5,7 @@
 #' @param filename where the file is located
 #'
 #' @return tibble of data
-read_npflex <- function(filename) {
+read_flex <- function(filename) {
 
   project <- readxl::read_excel(path = filename,
                                 range = "A1",
@@ -48,6 +48,7 @@ read_npflex <- function(filename) {
                          UniProt = unlist(as.vector(assay_data[3,])),
                          OlinkID = unlist(as.vector(assay_data[4,]))) |> 
     dplyr::slice(-1) |> 
+    dplyr::mutate(OlinkID = ifelse(stringr::str_detect(Assay, "Ctrl"), paste0("OID_",Assay), OlinkID)) |> 
     dplyr::filter(!is.na(OlinkID))
   
   # Define number of proteins per panel
@@ -56,13 +57,37 @@ read_npflex <- function(filename) {
     table() |> 
     as.data.frame()
   
+  # Is QC Deviation data present 
+  qc_dev_cols <- readxl::read_excel(path = filename, 
+                     skip = 2+1,
+                     col_names = FALSE,
+                     .name_repair="minimal",
+                     n_max = 2) |>
+    as.matrix() |> 
+    t() |> 
+    as.data.frame() |> 
+    dplyr::mutate(cols = paste(V1,V2)) |> 
+    dplyr::filter(stringr::str_detect(cols, "QC Deviation")) |> 
+    dplyr::select(cols) |> 
+    as.vector() |> 
+    unlist() |> 
+    unique()
+  
+    
+  
   # Load NPX or QUANT data including the last rows of meta data
   dat <- readxl::read_excel(path = filename,
                             skip = n_max_meta_data + 2 + 1,
                             col_names = FALSE,
                             .name_repair="minimal",
-                            col_types = c('text'))
-  names(dat) <- c("SampleID", metadata$OlinkID, paste0(rep("PlateID", panel_number), 1:panel_number), paste0(rep("QC_Warning", panel_number), 1:panel_number))
+                            col_types = NULL)
+  
+  data_cols <- c("SampleID", metadata$OlinkID, paste0(rep("PlateID", panel_number), 1:panel_number), paste0(rep("QC_Warning", panel_number), 1:panel_number))
+  if( length(qc_dev_cols) != 0){
+    names(dat) <- c(data_cols, paste0(rep(qc_dev_cols), 1:panel_number))
+  } else{
+    names(dat) <- data_cols
+  }
   
   # Calculate number of plates
  
@@ -73,31 +98,31 @@ read_npflex <- function(filename) {
     nrow()
   
   # Extract ending metadata
-   
+
   missfreq <- dat |> dplyr::filter(stringr::str_detect(SampleID, "Missing Data freq.")) |> 
     tidyr::pivot_longer(cols = tidyselect::starts_with("OID"),
                         names_to = "OlinkID",
                         values_to = "MissingFreq") |> 
-    dplyr::select(-SampleID, -tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC_Warning"))
+    dplyr::select(-SampleID, -tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC"))
   norm_method <- dat |> dplyr::filter(stringr::str_detect(SampleID, "Normalization")) |> 
     tidyr::pivot_longer(cols = tidyselect::starts_with("OID"),
                         names_to = "OlinkID",
                         values_to = "Normalization") |> 
-    dplyr::select(-SampleID, -tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC_Warning"))
+    dplyr::select(-SampleID, -tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC"))
   
   if (!is_npx_data) {
     assay_warning <- dat |> dplyr::filter(stringr::str_detect(SampleID, "Assay warning")) |> 
       tidyr::pivot_longer(cols = tidyselect::starts_with("OID"),
                           names_to = "OlinkID",
                           values_to = "Assay_Warning") |> 
-      dplyr::select(-SampleID, -tidyselect::starts_with("QC_Warning"))
+      dplyr::select(-SampleID, -tidyselect::starts_with("QC"))
     
     # # of rows = # of plates
     Plate_LQL <- dat |> dplyr::filter(stringr::str_detect(SampleID, "Lowest quantifiable level")) |> 
       tidyr::pivot_longer(cols = tidyselect::starts_with("OID"),
                           names_to = "OlinkID",
                           values_to = "Plate_LQL") |> 
-      dplyr::select(-tidyselect::starts_with("QC_Warning")) |> 
+      dplyr::select(-tidyselect::starts_with("QC")) |> 
       dplyr::select(-SampleID)
     
     # # of rows = # of plates
@@ -105,20 +130,20 @@ read_npflex <- function(filename) {
       tidyr::pivot_longer(cols = tidyselect::starts_with("OID"),
                           names_to = "OlinkID",
                           values_to = "Plat_LOD") |> 
-      dplyr::select(-tidyselect::starts_with("QC_Warning")) |> 
+      dplyr::select(-tidyselect::starts_with("QC")) |> 
       dplyr::select(-SampleID)
     
     LLOQ <- dat |> dplyr::filter(stringr::str_detect(SampleID, "LLOQ"))  |> 
       tidyr::pivot_longer(cols = tidyselect::starts_with("OID"),
                           names_to = "OlinkID",
                           values_to = "LLOQ") |> 
-      dplyr::select(-SampleID, -tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC_Warning"))
+      dplyr::select(-SampleID, -tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC"))
     
     ULOQ <- dat |> dplyr::filter(stringr::str_detect(SampleID, "ULOQ"))  |> 
       tidyr::pivot_longer(cols = tidyselect::starts_with("OID"),
                           names_to = "OlinkID",
                           values_to = "ULOQ") |> 
-      dplyr::select(-SampleID, -tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC_Warning"))
+      dplyr::select(-SampleID, -tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC"))
     
     meta_data_per_assay <- dplyr::left_join(missfreq,norm_method, by = "OlinkID") |> 
       dplyr::left_join(assay_warning, by = "OlinkID", multiple ="all") |>
@@ -127,13 +152,13 @@ read_npflex <- function(filename) {
       dplyr::left_join(LOD, by = c("OlinkID", "PlateID1")) 
   } else {
     LOD <- dat |> dplyr::filter(stringr::str_detect(SampleID, "LOD")) |> 
-      dplyr::select(-SampleID, -tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC_Warning")) |> 
+      dplyr::select(-SampleID, -tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC")) |> 
       tidyr::pivot_longer(cols = tidyselect::starts_with("OID"),
                           names_to = "OlinkID",
                           values_to = "LOD") 
     meta_data_per_assay <- dplyr::left_join(missfreq,norm_method, by = c("OlinkID")) |> 
       dplyr::left_join(LOD, by = c("OlinkID")) |> 
-      dplyr::select(-tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC_Warning"))
+      dplyr::select(-tidyselect::starts_with("PlateID"), -tidyselect::starts_with("QC"))
   }
   
   
@@ -158,18 +183,31 @@ read_npflex <- function(filename) {
     dplyr::select(SampleID, OlinkID,
                   UniProt, Assay, MissingFreq,
                   Panel,PlateID,
-                  QC_Warning,dplyr::matches("Plate_LQL"),
-                  dplyr::matches("LOD"),
-                  dplyr::matches("Plat_LOD"),
-                  dplyr::matches("LLOQ"),
-                  dplyr::matches("ULOQ"),
-                  dplyr::matches("NPX"),
-                  dplyr::matches("Quantified_value"),
-                  dplyr::matches("Unit"),
-                  dplyr::matches("Assay_Warning"),
-                  dplyr::matches("Normalization"),
-                  dplyr::matches("*Inc Ctrl*"),
-                  dplyr::matches("*Det Ctrl*"))
+                  QC_Warning,
+                  dplyr::any_of(dplyr::matches(c("Plate_LQL",
+                                          "LOD",
+                                          "Plat_LOD",
+                                          "LLOQ",
+                                          "ULOQ",
+                                          "NPX",
+                                          "Quantified_value",
+                                          "Unit",
+                                          "Assay_Warning",
+                                          "Normalization",
+                                          "*Inc Ctrl*",
+                                          "*Det Ctrl*")))) |> 
+    dplyr::mutate(suppressWarnings(dplyr::across(dplyr::any_of(dplyr::matches(c("MissingFreq",
+                                  "Plate_LQL",
+                                  "LOD",
+                                  "Plat_LOD",
+                                  "LLOQ",
+                                  "ULOQ",
+                                  "NPX",
+                                  "Quantified_value",
+                                  "*Inc Ctrl*",
+                                  "*Det Ctrl*"))), as.numeric))) |> 
+    dplyr::mutate(OlinkID = ifelse(stringr::str_detect(OlinkID,"Ctrl"), NA, OlinkID))
+          
   message(paste(panel_number, "Flex panel(s) were detected with", nr_plates, 
                 "plate(s) of samples."))
   return(dat)
