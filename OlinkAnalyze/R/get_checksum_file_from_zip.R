d9bd6dc1c4ee042c21f37a40f3104a424f9b98f8
Klev Diamanti <kdiamanti@outlook.com>
2023-11-06 12:59:31 +0100

    Update function to get checksum file

--- OlinkAnalyze/R/get_checksum_file_from_zip.R
+++ OlinkAnalyze/R/get_checksum_file_from_zip.R
@@ -1,36 +1,37 @@
-#' Title
+#' Helper function to get the name of the checksum file from a zip NPX.
 #'
-#' @param files
+#' @param files A vector of file names without any path prefix.
 #'
-#' @return
+#' @return The name of checksum file or NA if the file is not present.
 #'
-#' @importFrom rlang .env
-#'
-get_checksum_file_from_zip <- function(files) {
+get_checksum_file_from_zip <-
+  function(files,
+           accepted_checksum_files = c("MD5_checksum.txt",
+                                       "checksum_sha256.txt")) {
 
-  # if none of the files matches the accepted file names
-  if (!any(files %in% .env[["accepted_checksum_files"]])) {
-    return(NA_character_)
+    # if none of the files matches the accepted file names
+    if (!any(files %in% accepted_checksum_files)) {
+      return(NA_character_)
+    }
+
+    # if more than one files are in the accepted file names
+    if (sum(files %in% accepted_checksum_files) > 1) {
+      cli::cli_abort(
+        c(
+          "x" = "Too many checksum files!",
+          "i" = "The input *.zip file should contain {.strong only} one checksum
+          file: { glue::glue_collapse(x = accepted_checksum_files,
+                                      sep = \", \",
+                                      last = \" or \") }."
+        ),
+        call = NULL,
+        wrap = FALSE
+      )
+    }
+
+    # get the checksum file
+    checksum_file <- files[files %in% accepted_checksum_files]
+
+    # return
+    return(checksum_file)
   }
-
-  # if more than one files are in the accepted file names
-  if (sum(files %in% .env[["accepted_checksum_files"]]) > 1) {
-    cli::cli_abort(
-      c(
-        "x" = "Too many checksum files!",
-        "i" = "The input *.zip file should contain {.strong only} one checksum
-        file: { glue::glue_collapse(x = .env[[\"accepted_checksum_files\"]],
-                                    sep = \", \",
-                                    last = \" or \") }."
-      ),
-      call = NULL,
-      wrap = FALSE
-    )
-  }
-
-  # get the checksum file
-  checksum_file <- files[files %in% .env[["accepted_checksum_files"]]]
-
-  # return
-  return(checksum_file)
-}
