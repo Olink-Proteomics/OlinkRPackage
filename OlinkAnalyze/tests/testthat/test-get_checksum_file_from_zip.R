cbab73c338e7d7ea8785f20ca930919d4a303d4d
Klev Diamanti <kdiamanti@outlook.com>
2023-11-06 14:50:15 +0100

    Better splitting of tests for get checksum from zip

--- OlinkAnalyze/tests/testthat/test-get_checksum_file_from_zip.R
+++ OlinkAnalyze/tests/testthat/test-get_checksum_file_from_zip.R
@@ -16,22 +16,25 @@ test_that(
       ),
       "MD5_checksum.txt"
     )
+  }
+)
 
+test_that(
+  "get checksum file from zip - No checksum file",
+  {
     # missing checksum file
-    expect_identical(
-      get_checksum_file_from_zip(
-        files = c("test.csv")
-      ),
-      NA_character_
-    )
-
     expect_identical(
       get_checksum_file_from_zip(
         files = c("MD51_checksum.txt", "test.csv")
       ),
       NA_character_
     )
+  }
+)
 
+test_that(
+  "get checksum file from zip - 2 MD5 checksums",
+  {
     # multiple checksum files
     expect_error(
       get_checksum_file_from_zip(
@@ -39,7 +42,12 @@ test_that(
       ),
       regexp = "The compressed file contains an unknown NPX file!"
     )
+  }
+)
 
+test_that(
+  "get checksum file from zip - 2 sha256 checksums",
+  {
     expect_error(
       get_checksum_file_from_zip(
         files = c("checksum_sha256.txt", "checksum_sha256.txt")
@@ -47,6 +55,18 @@ test_that(
       regexp = "The compressed file contains an unknown NPX file!"
     )
 
+    expect_error(
+      get_checksum_file_from_zip(
+        files = c("MD5_checksum.txt", "checksum_sha256.txt")
+      ),
+      regexp = "The compressed file contains an unknown NPX file!"
+    )
+  }
+)
+
+test_that(
+  "get checksum file from zip - 1 sha256 and 1 MD5 checksum",
+  {
     expect_error(
       get_checksum_file_from_zip(
         files = c("MD5_checksum.txt", "checksum_sha256.txt")
