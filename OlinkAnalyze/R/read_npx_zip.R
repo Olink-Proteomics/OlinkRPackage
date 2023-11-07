99f939ed25e8bc7090a3776f056af185e6fd531a
Klev Diamanti <kdiamanti@outlook.com>
2023-11-07 17:24:34 +0100

    Aesthetic changes in read_npx_zip

--- OlinkAnalyze/R/read_npx_zip.R
+++ OlinkAnalyze/R/read_npx_zip.R
@@ -43,9 +43,9 @@ read_npx_zip <-
       dplyr::filter(!(.data[["Name"]] %in% .env[[".ignore_files"]])) |>
       dplyr::pull(.data[["Name"]])
 
-
     # check: files vector contains no entries
     if (length(compressed_file_contents) == 0L) {
+
       cli::cli_abort(
         c(
           "x" = "The compressed file does not contain checksum or NPX files!"
@@ -53,10 +53,11 @@ read_npx_zip <-
         call = NULL,
         wrap = FALSE
       )
+
     }
 
     # Get the name of the checksum file, if available
-    compressed_file_chksm <- get_checksum_file_from_zip(
+    compressed_file_checksum <- get_checksum_file_from_zip(
       files = compressed_file_contents
     )
 
@@ -66,9 +67,9 @@ read_npx_zip <-
     )
 
     # Array of files to extract
-    files_to_extract <- c(compressed_file_chksm,
+    files_to_extract <- c(compressed_file_checksum,
                           compressed_file_npx)
-    # Remove NA from checksum (if NA was returned)
+    # Remove NA strings (if NA was returned from checksum function)
     files_to_extract <- files_to_extract[!is.na(files_to_extract)]
 
     # **** Extract ****
@@ -87,69 +88,41 @@ read_npx_zip <-
     # **** Checksum ****
 
     # Checksum of the NPX file
-    if (!is.na(compressed_file_chksm)) {
+    if (!is.na(compressed_file_checksum)) {
 
       # Extracted checksum file
-      extracted_file_chksm <- file.path(tmp_unzip_dir,
-                                        compressed_file_chksm)
+      extracted_file_chksm <- file.path(tmp_unzip_dir, compressed_file_checksum)
 
-      # make the checksum filename easier to parse
-      chksm_string <- tools::file_path_sans_ext(compressed_file_chksm) |>
-        tolower()
+      checksum_check <- check_checksum_matches(
+        checksum_file = extracted_file_chksm,
+        npx_file = extracted_file_npx
+      )
 
-      if (grepl("md5", chksm_string)) {
-        # MD5 checksum on the NPX file
-        chksm_npx <- tools::md5sum(extracted_file_npx) |>
-          unname()
-      } else if(grepl("sha256", chksm_string)) {
-        # SHA256 checksum on the NPX file
-        chksm_npx <- openssl::sha256(file(extracted_file_npx)) |>
-          stringr::str_replace(pattern = ":",
-                               replacement = "")
-      }
+      # print error message if checksum did not work
+      if (!is.na(checksum_check)) {
 
-      # check that checksum matches NPX csv file
-      extracted_file_chksm_con <- file(extracted_file_chksm, "r")
-
-      # read in the checksum extracted from the compressed file
-      chksm_extracted <- readLines(con = extracted_file_chksm_con,
-                                   n = 1L,
-                                   warn = FALSE)
-
-      # clean up files
-      close(extracted_file_chksm_con)
-
-      # check if checksums match
-      if (chksm_extracted != chksm_npx) {
         # cleanup temporary directory with extracted files
-        invisible(
-          unlink(x = tmp_unzip_dir,
-                 recursive = TRUE)
-        )
+        invisible(unlink(x = tmp_unzip_dir, recursive = TRUE))
 
         # error message
         cli::cli_abort(
           c(
-            "x" = "The checksum of the NPX file does not match that of the
-                checksum file!",
+            "x" = paste0(checksum_check, ": {file}"),
             "i" = "Potential loss of data or corrupt file."
           ),
           call = NULL,
           wrap = FALSE
         )
+
       }
+
     }
 
     # read the NPX file
-    df_npx <- read_npx_csv(
-      filename = extracted_file_npx
-    )
+    df_npx <- read_npx_csv(filename = extracted_file_npx)
 
     # cleanup temporary directory with extracted files
-    invisible(
-      unlink(x = tmp_unzip_dir,
-             recursive = TRUE)
-    )
+    invisible(unlink(x = tmp_unzip_dir, recursive = TRUE))
 
     return(df_npx)
   }
