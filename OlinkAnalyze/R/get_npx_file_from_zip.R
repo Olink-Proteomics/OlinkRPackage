94b25743f19a1b695132f376243d0f3ce8ccf790
Klev Diamanti <kdiamanti@outlook.com>
2023-11-09 12:10:54 +0100

    Make accepted_npx_file_ext a global variable

--- OlinkAnalyze/R/get_npx_file_from_zip.R
+++ OlinkAnalyze/R/get_npx_file_from_zip.R
@@ -1,59 +1,52 @@
 #' Help function to get the name of the NPX file from a zip NPX.
 #'
 #' @param files A vector of file names without any path prefix.
-#' @param accepted_npx_files Vector of accepted extensions of NPX files.
 #'
 #' @return The name of the NPX file.
 #'
 #' @keywords zip NPX
 #'
-get_npx_file_from_zip <-
-  function(files,
-           accepted_npx_files = c("xls",
-                                  "xlsx",
-                                  "csv",
-                                  "txt",
-                                  "parquet")) {
+get_npx_file_from_zip <- function(files) {
 
-    # remove (if any) checksum files
-    files_no_checksum <- files[!grepl("(MD5|sha256).*\\.txt$", files)]
+  # remove (if any) checksum files
+  files_no_checksum <- files[!(files %in% accepted_checksum_files)]
 
-    # get file extension(s)
-    df_files <- dplyr::tibble(files = files_no_checksum) |>
-      dplyr::mutate(
-        files_extension = tools::file_ext(.data[["files"]])
-      )
+  # get file extension(s)
+  df_files <- dplyr::tibble(files = files_no_checksum) |>
+    dplyr::mutate(
+      files_extension = tools::file_ext(.data[["files"]])
+    )
 
-    # check: no file with the accepted suffix
-    if (sum(df_files$files_extension %in% accepted_npx_files) != 1L) {
+  # check: no file with the accepted suffix
+  if (sum(df_files$files_extension %in% accepted_npx_files) != 1L) {
 
-      if (sum(df_files$files_extension %in% accepted_npx_files) == 0L) {
-        err_msg <- "no" # nolint object_usage_linter
-      } else if (sum(df_files$files_extension %in% accepted_npx_files) > 1L) {
-        err_msg <- "multiple" # nolint object_usage_linter
-      }
+    if (sum(df_files$files_extension %in% accepted_npx_files) == 0L) {
+      err_msg <- "no" # nolint object_usage_linter
+    } else if (sum(df_files$files_extension %in% accepted_npx_files) > 1L) {
+      err_msg <- "multiple" # nolint object_usage_linter
+    }
 
-      cli::cli_abort(
-        c(
-          "x" = "The compressed file contains {err_msg} NPX files!",
-          "i" = "The compressed input file should contain {.strong only} one NPX
+    cli::cli_abort(
+      c(
+        "x" = "The compressed file contains {err_msg} NPX files!",
+        "i" = "The compressed input file should contain {.strong only} one NPX
           file with extension: { glue::glue_collapse(x = accepted_npx_files,
                                                      sep = \", \",
                                                      last = \" or \") }."
-        ),
-        call = NULL,
-        wrap = FALSE
-      )
-    }
-
-    # get the NPX file
-    npx_file <- df_files |>
-      dplyr::filter(
-        .data[["files_extension"]] %in% .env[["accepted_npx_files"]]
-      ) |>
-      dplyr::slice_head(n = 1L) |>
-      dplyr::pull(.data[["files"]])
-
-    # return
-    return(npx_file)
+      ),
+      call = NULL,
+      wrap = FALSE
+    )
   }
+
+  # get the NPX file
+  npx_file <- df_files |>
+    dplyr::filter(
+      .data[["files_extension"]] %in% .env[["accepted_npx_files"]]
+    ) |>
+    dplyr::slice_head(n = 1L) |>
+    dplyr::pull(.data[["files"]])
+
+  # return
+  return(npx_file)
+}
--- OlinkAnalyze/R/globals.R
+++ OlinkAnalyze/R/globals.R
@@ -5,4 +5,11 @@ utils::globalVariables(c(".env", ".data"))
 
 # the checksum files we accept
 accepted_checksum_files <- c("MD5_checksum.txt",
-                            "checksum_sha256.txt")
+                             "checksum_sha256.txt")
+
+# the extensions of NPX files that we accept
+accepted_npx_file_ext <- c("xls",
+                           "xlsx",
+                           "csv",
+                           "txt",
+                           "parquet")
