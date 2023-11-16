9731427d3fab9a4358ec9dd979ed0f83b938accf
klevdiamanti <kdiamanti@outlook.com>
2023-11-16 18:51:01 +0100

    Lintr improvements

--- OlinkAnalyze/R/globals.R
+++ OlinkAnalyze/R/globals.R
@@ -1,20 +1,31 @@
 # To prevent lint errors when using .data and .env
-utils::globalVariables(c(".env", ".data"))
+utils::globalVariables(
+  c(
+    ".env",
+    ".data"
+  )
+)
 
 # Global variables ----
 
 # the checksum files we accept
-accepted_checksum_files <- c("MD5_checksum.txt",
-                             "checksum_sha256.txt")
+accepted_checksum_files <- c(
+  "MD5_checksum.txt",
+  "checksum_sha256.txt"
+)
 
 # the extensions of NPX files that we accept
-accepted_npx_file_ext <- c("xls",
-                           "xlsx",
-                           "csv",
-                           "txt",
-                           "parquet",
-                           "zip")
+accepted_npx_file_ext <- c(
+  wide_1       = "xls",
+  wide_2       = "xlsx",
+  delim_1      = "csv",
+  delim_2      = "txt",
+  parquet_1    = "parquet",
+  compressed_1 = "zip"
+)
 
 # the separators of the Olink long files
-accepted_field_sep <- c(";",
-                        ",")
+accepted_field_sep <- c(
+  ";",
+  ","
+)
