760353ce97c65554a8fc1b02c74dfb6e3abf3d51
Klev Diamanti <kdiamanti@outlook.com>
2023-12-20 11:17:46 +0100

    New function check_is_list

---
+++ OlinkAnalyze/tests/testthat/test-check_list.R
@@ -0,0 +1,186 @@
+# Test check_is_list ----
+
+test_that(
+  "check is list works - TRUE",
+  {
+
+    expect_true(
+      object = check_is_list(lst = list("I_Shall_Pass"),
+                             error = FALSE)
+    )
+
+    expect_true(
+      object = check_is_list(lst = list("I_Shall_Pass"),
+                             error = TRUE)
+    )
+
+    expect_true(
+      object = check_is_list(lst = list(c("I_Shall_Pass")),
+                             error = FALSE)
+    )
+
+    expect_true(
+      object = check_is_list(lst = list(c("I_Shall_Pass")),
+                             error = TRUE)
+    )
+
+    expect_true(
+      object = check_is_list(lst = list(c("I_Shall_Pass",
+                                          NULL)),
+                             error = FALSE)
+    )
+
+    expect_true(
+      object = check_is_list(lst = list(c("I_Shall_Pass",
+                                          NULL)),
+                             error = TRUE)
+    )
+
+    expect_true(
+      object = check_is_list(lst = list(c("I_Shall_Pass",
+                                          "I_Shall_Not_Pass")),
+                             error = TRUE)
+    )
+
+    expect_true(
+      object = check_is_list(lst = list(c("I_Shall_Pass",
+                                          "I_Shall_Not_Pass")),
+                             error = FALSE)
+    )
+
+    expect_true(
+      object = check_is_list(lst = list(c("I_Shall_Pass",
+                                          1,
+                                          1.1,
+                                          TRUE)),
+                             error = TRUE)
+    )
+
+    expect_true(
+      object = check_is_list(lst = list(c("I_Shall_Pass",
+                                          1,
+                                          1.1,
+                                          TRUE)),
+                             error = FALSE)
+    )
+
+    expect_true(
+      object = check_is_list(lst = list("A" = c("I_Shall_Pass",
+                                                1,
+                                                1.1,
+                                                TRUE),
+                                        "B" = "Im_good"),
+                             error = TRUE)
+    )
+
+    expect_true(
+      object = check_is_list(lst = list("A" = c("I_Shall_Pass",
+                                                1,
+                                                1.1,
+                                                TRUE),
+                                        "B" = "Im_good"),
+                             error = FALSE)
+    )
+
+  }
+)
+
+test_that(
+  "check is list works - FALSE",
+  {
+
+    expect_false(
+      object = check_is_list(lst = c("I_Shall_Pass",
+                                     NA_character_),
+                             error = FALSE)
+    )
+
+    expect_false(
+      object = check_is_list(lst = NA_character_,
+                             error = FALSE)
+    )
+
+    expect_false(
+      object = check_is_list(lst = NULL,
+                             error = FALSE)
+    )
+
+    expect_false(
+      object = check_is_list(lst = 1,
+                             error = FALSE)
+    )
+
+    expect_false(
+      object = check_is_list(lst = 1L,
+                             error = FALSE)
+    )
+
+    expect_false(
+      object = check_is_list(lst = TRUE,
+                             error = FALSE)
+    )
+
+  }
+)
+
+test_that(
+  "check is list works - ERROR",
+  {
+
+    expect_error(
+      object = check_is_list(lst = c("I_Shall_Pass",
+                                     NA_character_),
+                             error = TRUE),
+      regexp = gsub(pattern = " ",
+                    replacement = "([[:space:]].*|\\n.*)?",
+                    x = "is not a list!",
+                    fixed = TRUE)
+    )
+
+    expect_error(
+      object = check_is_list(lst = NA_character_,
+                             error = TRUE),
+      regexp = gsub(pattern = " ",
+                    replacement = "([[:space:]].*|\\n.*)?",
+                    x = "is not a list!",
+                    fixed = TRUE)
+    )
+
+    expect_error(
+      object = check_is_list(lst = NULL,
+                             error = TRUE),
+      regexp = gsub(pattern = " ",
+                    replacement = "([[:space:]].*|\\n.*)?",
+                    x = "is not a list!",
+                    fixed = TRUE)
+    )
+
+    expect_error(
+      object = check_is_list(lst = 1,
+                             error = TRUE),
+      regexp = gsub(pattern = " ",
+                    replacement = "([[:space:]].*|\\n.*)?",
+                    x = "is not a list!",
+                    fixed = TRUE)
+    )
+
+    expect_error(
+      object = check_is_list(lst = 1L,
+                             error = TRUE),
+      regexp = gsub(pattern = " ",
+                    replacement = "([[:space:]].*|\\n.*)?",
+                    x = "is not a list!",
+                    fixed = TRUE)
+    )
+
+    expect_error(
+      object = check_is_list(lst = TRUE,
+                             error = TRUE),
+      regexp = gsub(pattern = " ",
+                    replacement = "([[:space:]].*|\\n.*)?",
+                    x = "is not a list!",
+                    fixed = TRUE)
+    )
+
+  }
+)
