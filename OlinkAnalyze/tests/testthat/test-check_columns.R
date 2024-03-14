2e3610085bc1c493cf623fa37a8c987845645c12
klevdiamanti <kdiamanti@outlook.com>
2024-03-14 13:34:37 +0100

    check_columns handles arrow input

--- OlinkAnalyze/tests/testthat/test-check_columns.R
+++ OlinkAnalyze/tests/testthat/test-check_columns.R
@@ -1,7 +1,7 @@
 # Test check_columns ----
 
 test_that(
-  "check columns works",
+  "check_columns - works - tibble",
   {
 
     tmp_data <- dplyr::tibble(
@@ -77,7 +77,84 @@ test_that(
 )
 
 test_that(
-  "check columns - incorrect list",
+  "check_columns - works - arrow",
+  {
+
+    tmp_data <- dplyr::tibble(
+      "A" = c(1L, 2L, 3L),
+      "B" = c(TRUE, TRUE, FALSE),
+      "C" = c("A", "B", "C"),
+      "D" = c(FALSE, FALSE, TRUE)
+    ) |>
+      arrow::as_arrow_table()
+
+    # both A and B exist
+    expect_no_condition(
+      object = check_columns(df = tmp_data,
+                             col_list = list("A",
+                                             "B"))
+    )
+
+    # A and (B or C) exist
+    expect_no_condition(
+      object = check_columns(df = tmp_data,
+                             col_list = list("A",
+                                             c("B", "C")))
+    )
+
+    # (A or D) and (B or C) exist
+    expect_no_condition(
+      object = check_columns(df = tmp_data,
+                             col_list = list(c("A", "D"),
+                                             c("B", "C")))
+    )
+
+    # A exists but E does not
+    expect_error(
+      object = check_columns(df = tmp_data,
+                             col_list = list("A",
+                                             "E")),
+      regexp = "is missing the required columns: E!"
+    )
+
+    # A exists but E and F do not
+    expect_error(
+      object = check_columns(df = tmp_data,
+                             col_list = list("A",
+                                             "E",
+                                             "F")),
+      regexp = "is missing the required columns: E and F!"
+    )
+
+    # A and (B or E) exist -> no error as B exists
+    expect_no_condition(
+      object = check_columns(df = tmp_data,
+                             col_list = list("A",
+                                             c("B", "E")))
+    )
+
+    # A and (F or E) exist -> error as neither E nor F exist
+    expect_error(
+      object = check_columns(df = tmp_data,
+                             col_list = list("A",
+                                             c("F", "E"))),
+      regexp = "at least one of the vectors of required columns"
+    )
+
+    # A and (F or E) exist -> error as neither E nor F exist
+    expect_error(
+      object = check_columns(df = tmp_data,
+                             col_list = list("A",
+                                             c("F", "E"),
+                                             c("M", "N"))),
+      regexp = "at least one of the vectors of required columns"
+    )
+
+  }
+)
+
+test_that(
+  "check_columns - error - incorrect input",
   {
 
     tmp_data <- dplyr::tibble(
