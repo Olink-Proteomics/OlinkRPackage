f108a04cee611ee6b263a5da38f36fa1b9e54bf4
MasoumehSheikhi <90918414+MasoumehSheikhi@users.noreply.github.com>
2024-05-23 12:14:24 -0400

    updating olink_lod example

--- OlinkAnalyze/R/olink_lod.R
+++ OlinkAnalyze/R/olink_lod.R
@@ -11,13 +11,22 @@
 #'
 #' \dontrun{
 #'   \donttest{
-#'     # Estimate LOD from negative controls
+#'   try({ # This will fail if the files do not exist.
+#'
+#'   # Import NPX data
+#'     npx_data <- read_NPX("path/to/npx_file")
+#'
+#'   # Estimate LOD from negative controls
 #'     npx_data_lod_NC <– olink_lod(data = npx_data,  lod_method = "NCLOD")
 #'
-#'     # Estimate LOD from fixed LOD
+#'   # Estimate LOD from fixed LOD
+#'   ## Locate the fixed LOD file
+#'     lod_file_path <- "path/to/lod_file"
+#'
 #'     npx_data_lod_Fixed <– olink_lod(data = npx_data,
-#'                                     lod_file_path = "path/to/lod_file",
+#'                                     lod_file_path = lod_file_path,
 #'                                     lod_method = "FixedLOD")
+#'      })
 #'   }
 #' }
 #'
