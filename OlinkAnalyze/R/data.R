024a674f310d3a24e358f9e74719b7a12a06a1de
Klev Diamanti <kdiamanti@outlook.com>
2024-07-17 20:18:40 +0200

    Update data

--- OlinkAnalyze/R/data.R
+++ OlinkAnalyze/R/data.R
@@ -1,16 +1,18 @@
 #' NPX Data in Long format.
 #'
+#' @description
 #' This is a synthetic dataset aiming to use-cases of functions from this
 #' package.
 #'
+#' @details
 #' A tibble with 29,440 rows and 17 columns.
 #'
-#' `npx_data1` is an Olink NPX data file (tibble) in long format with 158 unique
-#' Sample ID's (including 2 repeats each of control samples:
-#' CONTROL_SAMPLE_AS 1 CONTROL_SAMPLE_AS 2). The data also contains 1104 assays
-#' (uniquely identified using OlinkID) over 2 Panels.
+#' \var{npx_data1} is an Olink NPX data file (tibble) in long format with 158
+#' unique Sample identifiers (including 2 repeats each of control samples:
+#' CONTROL_SAMPLE_AS 1 and CONTROL_SAMPLE_AS 2). The data also contains 1104
+#' assays uniquely identified using OlinkID over 2 Olink Panels.
 #'
-#' @format In addition to standard read_NPX() columns, this dataset also
+#' @format In addition to standard read_npx() columns, this dataset also
 #' contains columns:
 #' \describe{
 #'   \item{Subject}{Subject Identifier}
@@ -24,25 +26,23 @@
 
 #' NPX Data in Long format, a follow-up.
 #'
+#' @description
 #' This is a synthetic dataset aiming to use-cases of functions from this
-#' package.
+#' package. The format is very similar to \var{npx_data1}. Both datasets can be
+#' used to demonstrate the use of normalization functionality.
 #'
-#' The format is very similar to `npx_data1`. Both datasets can be used to
-#' demonstrate the use of normalization functionality.
-#'
+#' @details
 #' A tibble with 32,384 rows and 17 columns.
 #'
-#' `npx_data2` is an Olink NPX data file (tibble) in long format  with 174
-#' unique Sample ID's (including 2 repeats each of control samples:
-#' CONTROL_SAMPLE_AS 1 CONTROL_SAMPLE_AS 2). The data also contains 1,104 assays
-#' (uniquely identified using OlinkID) over 2 Panels. This dataset also contain
-#' 16 bridge samples with SampleID's that are also present in data (npx_data1).
-#' These sample ID's are: A13, A29, A30, A36, A45, A46, A52, A63, A71, A73, B3,
-#' B4, B37, B45, B63, B75
+#' \var{npx_data2} is an Olink NPX data file (tibble) in long format  with 174
+#' unique Sample identifiers (including 2 repeats each of control samples:
+#' \emph{CONTROL_SAMPLE_AS 1} and \emph{CONTROL_SAMPLE_AS 2}). The data also
+#' contains 1,104 assays uniquely identified using OlinkID over 2 Panels. This
+#' dataset also contains 16 bridge samples with SampleIDs that are also present
+#' in data \var{npx_data1}. These samples are: A13, A29, A30, A36, A45, A46,
+#' A52, A63, A71, A73, B3, B4, B37, B45, B63 and B75.
 #'
-#'
-#'
-#' @format In addition to standard read_NPX() columns, this dataset also
+#' @format In addition to standard read_npx() columns, this dataset also
 #' contains columns:
 #' \describe{
 #'   \item{Subject}{Subject Identifier}
@@ -56,14 +56,14 @@
 
 #' Example Sample Manifest
 #'
+#' @description
 #' Synthetic sample manifest to demonstrate use of functions in this package.
 #'
-#'
+#' @details
 #' A tibble with 138 rows and 4 columns. This manifest contains 26 example
 #' subjects, with 6 visits and 2 sites.
 #'
-#'
-#' @format This dataset  contains columns:
+#' @format This dataset contains columns:
 #' \describe{
 #'   \item{SubjectID}{Subject Identifier, A-Z}
 #'   \item{Visit}{Visit Number, 1-6}
