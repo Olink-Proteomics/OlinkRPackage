3dd24f19b4e54b0f5047752bee1d1084643a1111
klevdiamanti <kdiamanti@outlook.com>
2024-08-02 21:03:40 +0200

    data-raw/mapping.R loads the mapping dataset

--- OlinkAnalyze/data-raw/mapping.R
+++ OlinkAnalyze/data-raw/mapping.R
@@ -1,8 +1,10 @@
-## code to prepare `mapping` dataset goes here
+# code to prepare `mapping` dataset goes here
+# Raw data is generated here:
+# Data_Science/Internal Projects/3k_to_HT_bridging/R code/Bridgeable/mapping_file.R
 
-# See Data_Science/Internal Projects/3k_to_HT_bridging/R code/Bridgeable/mapping_file.R for generating from raw data
-
-load("inst/extdata/OlinkIDMapping.rds")
-
-usethis::use_data(mapping, overwrite = TRUE)
-
+e3k_eHT_mapping_rds <- system.file("extdata",
+                                   "OlinkIDMapping.rds",
+                                   package = "OlinkAnalyze",
+                                   mustWork = TRUE)
+e3k_eHT_mapping <- readRDS(file = e3k_eHT_mapping_rds)
+rm(e3k_eHT_mapping_rds)
