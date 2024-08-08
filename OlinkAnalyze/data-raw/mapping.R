7efcbe7ff9a07b8264fa84e68a7cf31ed25071e2
Kathy Nevola <72228735+kathy-nevola@users.noreply.github.com>
2024-08-08 10:33:19 -0400

    Update OlinkAnalyze/data-raw/mapping.R

--- OlinkAnalyze/data-raw/mapping.R
+++ OlinkAnalyze/data-raw/mapping.R
@@ -1,6 +1,5 @@
 # code to prepare `mapping` dataset goes here
 # Raw data is generated here:
-# Data_Science/Internal Projects/3k_to_HT_bridging/R code/Bridgeable/mapping_file.R
 
 e3k_eHT_mapping_rds <- system.file("extdata",
                                    "OlinkIDMapping.rds",
