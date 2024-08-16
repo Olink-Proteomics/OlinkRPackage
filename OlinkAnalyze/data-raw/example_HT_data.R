79390c85c8d743c88b14c2e8abe7ee4f232d8bc9
Kathy Nevola <72228735+kathy-nevola@users.noreply.github.com>
2024-08-16 15:57:18 -0400

    example of low count

--- OlinkAnalyze/data-raw/example_HT_data.R
+++ OlinkAnalyze/data-raw/example_HT_data.R
@@ -1,20 +1,20 @@
 # Generating Example HT Data
 # Create Sample data
 sampleIDs <- c(paste0("Sample_", LETTERS[1:26]),
-               paste0("Sample_A", LETTERS[1:26]), 
-               paste0("Sample_B", LETTERS[1:26]), 
+               paste0("Sample_A", LETTERS[1:26]),
+               paste0("Sample_B", LETTERS[1:26]),
                paste0("Sample_C", LETTERS[1:26]),
                paste0("Sample_D", LETTERS[1:26]),
-               paste0("Sample_E", LETTERS[1:26]), 
+               paste0("Sample_E", LETTERS[1:26]),
                paste0("Sample_F", LETTERS[1:26])  )[1:172]
 control_sampleIDs <- c(paste0("Sample_Control_", 1:6))
 plate_control_IDs <- c(paste0("Plate_Control_", 1:10))
-negative_control_ids <- c(paste0("Negative_Control_", 1:4))  
+negative_control_ids <- c(paste0("Negative_Control_", 1:4))
 
-samples<-data.frame(SampleID = c(sampleIDs, 
-                        control_sampleIDs, 
-                        plate_control_IDs, 
-                        negative_control_ids), 
+samples<-data.frame(SampleID = c(sampleIDs,
+                        control_sampleIDs,
+                        plate_control_IDs,
+                        negative_control_ids),
            SampleType = c(rep("SAMPLE", times = length(sampleIDs)),
                           rep("SAMPLE_CONTROL", times = length(control_sampleIDs)),
                           rep("PLATE_CONTROL", times = length(plate_control_IDs)),
@@ -42,24 +42,26 @@ assays <- data.frame(OlinkID = assays_map$OlinkID[1:100],
 data <- expand.grid(samples$SampleID, assays$OlinkID)
 names(data) <- c("SampleID", "OlinkID")
 
-data <- data |> 
-  left_join(samples) |> 
-  left_join(assays)
+data <- data |>
+  dplyr::left_join(samples) |>
+  dplyr::left_join(assays)
 
 # Generate NPX
 set.seed(1234)
-data <-data |> 
-  group_by(OlinkID, SampleType) |> 
-  mutate(NPX = ifelse(SampleType == "SAMPLE", rnorm(n = length(sampleIDs), sd = 3, mean = 1),
-                      ifelse(SampleType != "NEGATIVE_CONTROL", 
+data <-data |>
+  dplyr::group_by(OlinkID, SampleType) |>
+  dplyr::mutate(NPX = ifelse(SampleType == "SAMPLE", rnorm(n = length(sampleIDs), sd = 3, mean = 1),
+                      ifelse(SampleType != "NEGATIVE_CONTROL",
                              rnorm(length(c(plate_control_IDs, control_sampleIDs)), sd = 2, mean = 0),
-                             rnorm(length(negative_control_ids), sd = 1, mean = -1)))) |> 
-  ungroup() |> 
-  mutate(PCNormalizedNPX = NPX) |> 
-  mutate(Count = sample(x = 1:1000, size = nrow(data), replace = TRUE))
+                             rnorm(length(negative_control_ids), sd = 1, mean = -1)))) |>
+  dplyr::ungroup() |>
+  dplyr::mutate(PCNormalizedNPX = NPX) |>
+  dplyr::mutate(Count = sample(x = 1:1000, size = nrow(data), replace = TRUE)) |>
+  dplyr::mutate(Count = ifelse(OlinkID %in% assays_map$OlinkID[5], sample(x = 1:150, size = nrow(samples), replace = TRUE), Count))
 
-data <- data |> 
-  mutate(Normalization = "Plate control",
+data |>  dplyr::filter(OlinkID %in% assays_map$OlinkID[5]) |> dplyr::group_by(OlinkID) |> dplyr::summarise(median = median(Count))
+data <- data |>
+  dplyr::mutate(Normalization = "Plate control",
          AssayQC = "PASS",
          SampleQC = "PASS")
 data_ht <- data
