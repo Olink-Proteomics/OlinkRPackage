158678473d5614362db3d68753addb27e24973c7
klevdiamanti <kdiamanti@outlook.com>
2024-08-28 13:24:07 +0200

    rename to eHT_e3072 to reflect the dataset that should be retained downstream

--- OlinkAnalyze/data-raw/sysdata.R
+++ OlinkAnalyze/data-raw/sysdata.R
@@ -1,15 +1,15 @@
 # mapping.R ----
 
-e3k_eHT_mapping_file <- system.file("data-raw",
-                                    "mapping.R",
-                                    package = "OlinkAnalyze",
-                                    mustWork = TRUE)
-source(e3k_eHT_mapping_file)
-rm(e3k_eHT_mapping_file)
+eHT_e3072_mapping_file <- system.file("data-raw",
+                                      "mapping.R",
+                                      package = "OlinkAnalyze",
+                                      mustWork = TRUE)
+source(eHT_e3072_mapping_file)
+rm(eHT_e3072_mapping_file)
 
 # save to R/R/sysdata.rda ----
 
-usethis::use_data(e3k_eHT_mapping,
+usethis::use_data(eHT_e3072_mapping,
                   overwrite = TRUE,
                   internal = TRUE,
                   compress = "xz",
