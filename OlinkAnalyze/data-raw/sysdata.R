234ef17f72c7cea2b148910c318c8ea1a011268e
klevdiamanti <kdiamanti@outlook.com>
2024-08-29 17:17:12 +0200

    Cleanup sysdata.R script

--- OlinkAnalyze/data-raw/sysdata.R
+++ OlinkAnalyze/data-raw/sysdata.R
@@ -7,14 +7,23 @@ e3k_eHT_mapping_file <- system.file("data-raw",
 source(e3k_eHT_mapping_file)
 rm(e3k_eHT_mapping_file)
 
-source(system.file("data-raw",
-            "example_HT_data.R",
-            package = "OlinkAnalyze",
-            mustWork = TRUE))
-source(system.file("data-raw",
-                   "example_3k_data.R",
-                   package = "OlinkAnalyze",
-                   mustWork = TRUE))
+# example HT data ----
+
+data_ht_file <- system.file("data-raw",
+                            "example_HT_data.R",
+                            package = "OlinkAnalyze",
+                            mustWork = TRUE)
+source(data_ht_file)
+rm(data_ht_file)
+
+# example 3k data ----
+
+data_3k_file <- system.file("data-raw",
+                            "example_3k_data.R",
+                            package = "OlinkAnalyze",
+                            mustWork = TRUE)
+source(data_3k_file)
+rm(data_3k_file)
 
 # save to R/R/sysdata.rda ----
 
