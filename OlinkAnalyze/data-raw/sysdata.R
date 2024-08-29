42883e3b6c5273fd9f1ab78873868d171704578c
klevdiamanti <kdiamanti@outlook.com>
2024-08-29 17:24:57 +0200

    Update with example data 3k and HT

--- OlinkAnalyze/data-raw/sysdata.R
+++ OlinkAnalyze/data-raw/sysdata.R
@@ -1,15 +1,35 @@
 # mapping.R ----
 
-eHT_e3072_mapping_file <- system.file("data-raw",
-                                      "mapping.R",
-                                      package = "OlinkAnalyze",
-                                      mustWork = TRUE)
-source(eHT_e3072_mapping_file)
-rm(eHT_e3072_mapping_file)
+e3k_eHT_mapping_file <- system.file("data-raw",
+                                    "mapping.R",
+                                    package = "OlinkAnalyze",
+                                    mustWork = TRUE)
+source(e3k_eHT_mapping_file)
+rm(e3k_eHT_mapping_file)
+
+# example HT data ----
+
+data_ht_file <- system.file("data-raw",
+                            "example_HT_data.R",
+                            package = "OlinkAnalyze",
+                            mustWork = TRUE)
+source(data_ht_file)
+rm(data_ht_file)
+
+# example 3k data ----
+
+data_3k_file <- system.file("data-raw",
+                            "example_3k_data.R",
+                            package = "OlinkAnalyze",
+                            mustWork = TRUE)
+source(data_3k_file)
+rm(data_3k_file)
 
 # save to R/R/sysdata.rda ----
 
 usethis::use_data(eHT_e3072_mapping,
+                  data_ht,
+                  data_3k,
                   overwrite = TRUE,
                   internal = TRUE,
                   compress = "xz",
