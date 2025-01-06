a0e3f934fcff0d18af9114533685bbddbd72ce84
Kristyn Chin <kristyn.chin@olink.com>
2025-01-06 14:12:47 -0500

    Add parameter documentation to bridgeable_plot

--- OlinkAnalyze/R/bridgeable_plot.R
+++ OlinkAnalyze/R/bridgeable_plot.R
@@ -13,6 +13,10 @@
 #'  threshold for the two platforms. Default is 150
 #' @param min_count Numeric value to use for minimum counts
 #'  for the two platforms. Default is 10
+#' @param bridge_sampleid Character vector containing overlapping SampleIDs
+#'  between the two bridging projects. If this argument is not provided,
+#'  the function will look for overlapping SampleID values between the two 
+#'  projects in the bridged dataframe.  
 #'
 #' @return An object of class "ggplot", 4 combined plots for each protein.
 #' @export
