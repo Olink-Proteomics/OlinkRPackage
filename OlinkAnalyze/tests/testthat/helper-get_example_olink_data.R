c6ffb9bebfb23b932b355fef50fc5c98b7f0a3ea
Kathy Nevola <72228735+kathy-nevola@users.noreply.github.com>
2025-01-23 16:02:51 -0500

    fix test example data interaction

--- OlinkAnalyze/tests/testthat/helper-get_example_olink_data.R
+++ OlinkAnalyze/tests/testthat/helper-get_example_olink_data.R
@@ -1,8 +1,9 @@
 # load example datasets for Olink Explore products
 get_example_data <- function(filename) {
-  ref_norm_res_file <- test_path("testthat","data", filename)
+  ref_norm_res_file <- test_path("data", filename)
+  print(ref_norm_res_file)
   #check that file exists
-  expect_true(file.exists(ref_norm_res_file))
+  expect_true(file.exists(ref_norm_res_file),label = ref_norm_res_file)
   # read rds data
   readRDS(file = ref_norm_res_file)
 }
