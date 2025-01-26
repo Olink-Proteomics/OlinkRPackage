468f9a02651c8ca5b978cf94aa3b9331bd8c4edb
Kathy Nevola <72228735+kathy-nevola@users.noreply.github.com>
2025-01-26 13:51:15 -0500

    remove print statment

--- OlinkAnalyze/tests/testthat/helper-get_example_olink_data.R
+++ OlinkAnalyze/tests/testthat/helper-get_example_olink_data.R
@@ -1,7 +1,6 @@
 # load example datasets for Olink Explore products
 get_example_data <- function(filename) {
   ref_norm_res_file <- test_path("data", filename)
-  print(ref_norm_res_file)
   #check that file exists
   expect_true(file.exists(ref_norm_res_file),label = ref_norm_res_file)
   # read rds data
