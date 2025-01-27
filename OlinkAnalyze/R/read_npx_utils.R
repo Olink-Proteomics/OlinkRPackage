497878a3964fa016f617270f6c027b4801eeeb1e
klevdiamanti <kdiamanti@outlook.com>
2025-01-27 17:23:36 +0100

    Move get and check for accepted_olink_platforms to the new R script "get_olink_platforms"

--- OlinkAnalyze/R/read_npx_utils.R
+++ OlinkAnalyze/R/read_npx_utils.R
@@ -4,9 +4,11 @@
 #' @author
 #'   Klev Diamanti
 #'
-#' @param out_df Class of output data frame. One of `r get_df_output()`.
+#' @param out_df Class of output data frame. One of
+#' `r ansi_collapse_quot(x = read_npx_df_output, sep = "and")`.
 #'
-#' @return Error if \var{out_df} is not one of `r get_df_output()`.
+#' @return Error if \var{out_df} is not one of
+#' `r ansi_collapse_quot(x = read_npx_df_output, sep = "and")`.
 #'
 check_out_df_arg <- function(out_df) {
 
@@ -18,8 +20,8 @@ check_out_df_arg <- function(out_df) {
 
     cli::cli_abort(
       message = c(
-        "x" = "Unknown output argument {.val {out_df}}!",
-        "i" = "Expecting one of {.val {read_npx_df_output}}"
+        "x" = "Unknown output argument {.arg out_df}!",
+        "i" = "Expected one of {.val {read_npx_df_output}}"
       ),
       call = rlang::caller_env(),
       wrap = FALSE
@@ -29,14 +31,15 @@ check_out_df_arg <- function(out_df) {
 
 }
 
-#' Help function converting the output data frame from a read_npx* function to
-#' `r get_df_output_print()`.
+#' Help function converting the output dataset from read_npx* functions to
+#' `r ansi_collapse_quot(x = get_df_output_print(), sep = "or")`.
 #'
 #' @author
 #'   Klev Diamanti
 #'
 #' @param df Dataset to be converted.
-#' @param out_df Class of output data frame. One of `r get_df_output()`.
+#' @param out_df Class of output data frame. One of
+#' `r ansi_collapse_quot(x = read_npx_df_output, sep = "and")`.
 #'
 #' @return The dataset in the requested class.
 #'
@@ -64,9 +67,7 @@ convert_read_npx_output <- function(df,
     cli::cli_abort(
       message = c(
         "x" = "Unexpected input dataset {.arg df}!",
-        "i" = "Expecting: { cli::ansi_collapse(x = read_npx_df_output,
-                                               sep2 = \" or \",
-                                               last = \", or \") }"
+        "i" = "Expected one of: {.val {read_npx_df_output}}."
       ),
       call = rlang::caller_env(),
       wrap = FALSE
@@ -76,174 +77,17 @@ convert_read_npx_output <- function(df,
 
 }
 
-#' Help function checking that \var{olink_platform} is acceptable.
-#'
-#' @author
-#'   Klev Diamanti
-#'
-#' @param x Name of Olink platform. One of
-#' `r ansi_collapse_quot(x = get_all_olink_platforms())`.
-#' @param broader_platform Name of the broader Olink platform. One of
-#' `r ansi_collapse_quot(x = get_olink_broader_platforms())`.
-#'
-#' @return
-#' `NULL` if platform is ok, otherwise an error.
-#'
-check_olink_platform <- function(x,
-                                 broader_platform = NULL) {
-
-  # input check ----
-
-  check_is_scalar_character(string = x,
-                            error = TRUE)
-
-  if (!is.null(broader_platform)) {
-    check_olink_broader_platform(x = broader_platform)
-  }
-
-  # check platform ----
-
-  # filter the global variable accepted_olink_platforms to have a collection
-  # of platforms available.
-  if (is.null(broader_platform)) {
-
-    olink_platforms <- accepted_olink_platforms
-
-  } else {
-
-    olink_platforms <- accepted_olink_platforms |>
-      dplyr::filter(.data[["broader_platform"]] == .env[["broader_platform"]])
-
-  }
-
-  # Throw an error if unexpected platform
-  if (!(x %in% olink_platforms$name)) {
-
-    cli::cli_abort(
-      message = c(
-        "x" = "Unexpected Olink platform {.val {x}}!",
-        "i" = "Expected one of:
-        {ansi_collapse_quot(x = get_all_olink_platforms())}"
-      ),
-      call = rlang::caller_env(),
-      wrap = FALSE
-    )
-
-  }
-
-}
-
-#' Help function checking that the Olink data_type is acceptable.
-#'
-#' @author
-#'   Klev Diamanti
-#'
-#' @param x The name of the Olink data type. One of
-#' `r accepted_olink_platforms$quant_method |> unlist() |> unique() |> sort() |> cli::ansi_collapse(sep2 = " or ", last = ", or ")`. # nolint
-#' @param broader_platform Name of the broader Olink platform. One of
-#' `r unique(accepted_olink_platforms$broader_platform) |> cli::ansi_collapse(sep2 = " or ", last = ", or ")`. # nolint
-#'
-#' @return
-#' `NULL` if data_type is ok, otherwise an error.
-#'
-check_olink_data_type <- function(x,
-                                  broader_platform = NULL) {
-
-  # input check ----
-
-  check_is_scalar_character(string = x,
-                            error = TRUE)
-
-  if (!is.null(broader_platform)) {
-    check_olink_broader_platform(x = broader_platform)
-  }
-
-  # check data_type ----
-
-  # filter the global variable accepted_olink_platforms to have a collection
-  # of data types available.
-  if (is.null(broader_platform)) {
-
-    olink_quant_methods <- accepted_olink_platforms
-
-  } else {
-
-    olink_quant_methods <- accepted_olink_platforms |>
-      dplyr::filter(
-        .data[["broader_platform"]] == .env[["broader_platform"]]
-      )
-  }
-
-  olink_quant_methods <- olink_quant_methods |>
-    dplyr::pull(
-      .data[["quant_method"]]
-    ) |>
-    unlist() |>
-    unique()
-
-  # Throw an error if unexpected data_type
-  if (!(x %in% olink_quant_methods)) {
-
-    cli::cli_abort(
-      message = c(
-        "x" = "Unexpected Olink data type {.arg {rlang::caller_arg(x)}}!",
-        "i" = "Expected one of: {olink_quant_methods}"
-      ),
-      call = rlang::caller_env(),
-      wrap = FALSE
-    )
-
-  }
-
-}
-
-#' Help function checking that the broader Olink platform is acceptable.
-#'
-#' @author
-#'   Klev Diamanti
-#'
-#' @param x Name of the broader Olink platform. One of
-#' `r unique(accepted_olink_platforms$broader_platform) |> cli::ansi_collapse(sep2 = " or ", last = ", or ")`. # nolint
-#'
-#' @return
-#' `NULL` if broader Olink platform is ok, otherwise an error.
-#'
-check_olink_broader_platform <- function(x) {
-
-  # input check ----
-
-  check_is_scalar_character(string = x,
-                            error = TRUE)
-
-  # check broader platform ----
-
-  if (!(x %in% unique(accepted_olink_platforms$broader_platform))) {
-
-    cli::cli_abort(
-      message = c(
-        "x" = "Unexpected Olink broader platform
-        {.arg {rlang::caller_arg(x)}}!",
-        "i" = "Expected one of:
-        {unique(accepted_olink_platforms$broader_platform)}"
-      ),
-      call = rlang::caller_env(),
-      wrap = FALSE
-    )
-
-  }
-
-}
-
-#' Help function checking whether a data set contains NA or empty strings on
+#' Help function checking whether a dataset contains NA or empty strings on
 #' its column names
 #'
 #' @author
 #'   Klev Diamanti
 #'
-#' @param df Tibble or ArrowObject with Olink data in wide or long format.
+#' @param df Olink dataset
+#' (`r ansi_collapse_quot(x =get_df_output_print(), sep = "or")`)
+#' in wide or long format.
 #' @param file Path to Olink software output file in wide or long format.
-#' Expecting file extensions
-#' `r cli::ansi_collapse(x = accepted_npx_file_ext, sep2 = " or ", last = ", or ")`. # nolint
+#' Expected file types: `r get_accepted_file_ext_summary()`.
 #'
 #' @return Error is file contains problematic column names. `NULL` otherwise.
 #'
