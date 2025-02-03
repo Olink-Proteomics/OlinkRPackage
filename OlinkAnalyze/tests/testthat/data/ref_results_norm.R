4d0c0114c10cf13614dd0bb116fc4567299e7a6c
Klev Diamanti <klev.diamanti@olink.com>
2025-02-03 16:40:49 +0100

    Add namespace for OA and a header desribing how to reproduce results

--- OlinkAnalyze/tests/testthat/data/ref_results_norm.R
+++ OlinkAnalyze/tests/testthat/data/ref_results_norm.R
@@ -1,5 +1,9 @@
 # This script generated reference data for olink_normalization using
-# OlinkAnalyze v3.8.2
+# OlinkAnalyze v3.8.2. To revert to OA version OA 3.8.2 please use the following
+# command:
+# remotes::install_version("OlinkAnalyze", version = "3.8.2")
+# and check the packageVersion using:
+# packageVersion("OlinkAnalyze")
 
 # datasets ----
 
@@ -8,7 +12,7 @@ lst_df <- list()
 ## npx_data1 ----
 
 # npx_data1 does not contain column Normalization
-lst_df$df1_no_norm <- npx_data1 |>
+lst_df$df1_no_norm <- OlinkAnalyze::npx_data1 |>
   dplyr::select(
     -dplyr::all_of(
       c("Subject", "Treatment", "Site", "Time", "Project")
@@ -16,7 +20,7 @@ lst_df$df1_no_norm <- npx_data1 |>
   )
 
 # npx_data1 with Normalization column
-lst_df$df1_norm <- npx_data1 |>
+lst_df$df1_norm <- OlinkAnalyze::npx_data1 |>
   dplyr::select(
     -dplyr::all_of(
       c("Subject", "Treatment", "Site", "Time", "Project")
@@ -27,7 +31,7 @@ lst_df$df1_norm <- npx_data1 |>
   )
 
 # npx_data1 with Normalization column, but no LOD column
-lst_df$df1_no_lod <- npx_data1 |>
+lst_df$df1_no_lod <- OlinkAnalyze::npx_data1 |>
   dplyr::select(
     -dplyr::all_of(
       c("Subject", "Treatment", "Site", "Time", "Project", "LOD")
@@ -38,7 +42,7 @@ lst_df$df1_no_lod <- npx_data1 |>
   )
 
 # npx_data1 with Normalization column, and PlateLOD+MaxLOD instead of LOD
-lst_df$df1_multiple_lod <- npx_data1 |>
+lst_df$df1_multiple_lod <- OlinkAnalyze::npx_data1 |>
   dplyr::select(
     -dplyr::all_of(
       c("Subject", "Treatment", "Site", "Time", "Project")
@@ -55,7 +59,7 @@ lst_df$df1_multiple_lod <- npx_data1 |>
 ## npx_data2 ----
 
 # npx_data2 does not contain column Normalization
-lst_df$df2_no_norm <- npx_data2 |>
+lst_df$df2_no_norm <- OlinkAnalyze::npx_data2 |>
   dplyr::select(
     -dplyr::all_of(
       c("Subject", "Treatment", "Site", "Time", "Project")
@@ -63,7 +67,7 @@ lst_df$df2_no_norm <- npx_data2 |>
   )
 
 # npx_data2 with Normalization column
-lst_df$df2_norm <- npx_data2 |>
+lst_df$df2_norm <- OlinkAnalyze::npx_data2 |>
   dplyr::select(
     -dplyr::all_of(
       c("Subject", "Treatment", "Site", "Time", "Project")
@@ -74,7 +78,7 @@ lst_df$df2_norm <- npx_data2 |>
   )
 
 # npx_data2 with Normalization column, but no LOD column
-lst_df$df2_no_lod <- npx_data2 |>
+lst_df$df2_no_lod <- OlinkAnalyze::npx_data2 |>
   dplyr::select(
     -dplyr::all_of(
       c("Subject", "Treatment", "Site", "Time", "Project", "LOD")
@@ -85,7 +89,7 @@ lst_df$df2_no_lod <- npx_data2 |>
   )
 
 # npx_data2 with Normalization column, and PlateLOD+MaxLOD instead of LOD
-lst_df$df2_multiple_lod <- npx_data2 |>
+lst_df$df2_multiple_lod <- OlinkAnalyze::npx_data2 |>
   dplyr::select(
     -dplyr::all_of(
       c("Subject", "Treatment", "Site", "Time", "Project")
@@ -101,7 +105,7 @@ lst_df$df2_multiple_lod <- npx_data2 |>
 
 ## reference_medians ----
 
-lst_df$ref_med <- npx_data1 |>
+lst_df$ref_med <- OlinkAnalyze::npx_data1 |>
   dplyr::group_by(
     dplyr::pick(
       c("OlinkID")
@@ -136,18 +140,18 @@ lst_sample$sample_subset <- c("A6", "A38", "B47", "B22", "A43", "D75", "D79",
 ## bridge samples ----
 
 lst_sample$bridge_samples <- intersect(
-  x = npx_data1$SampleID,
-  y = npx_data2$SampleID
+  x = OlinkAnalyze::npx_data1$SampleID,
+  y = OlinkAnalyze::npx_data2$SampleID
 ) |>
   (\(x) x[!grepl(pattern = "CONTROL_SAMPLE", x = x)])()
 
 ## npx_data1 samples ----
 
-lst_sample$df1_all <- npx_data1$SampleID |>
+lst_sample$df1_all <- OlinkAnalyze::npx_data1$SampleID |>
   unique() |>
   (\(x) x[!grepl(pattern = "CONTROL_SAMPLE", x = x)])()
 
-lst_sample$df1_subset <- npx_data1$SampleID |>
+lst_sample$df1_subset <- OlinkAnalyze::npx_data1$SampleID |>
   unique() |>
   sort() |>
   (\(x) x[!grepl(pattern = "CONTROL_SAMPLE", x = x)])() |>
@@ -155,11 +159,11 @@ lst_sample$df1_subset <- npx_data1$SampleID |>
 
 ## npx_data2 samples ----
 
-lst_sample$df2_all <- npx_data2$SampleID |>
+lst_sample$df2_all <- OlinkAnalyze::npx_data2$SampleID |>
   unique() |>
   (\(x) x[!grepl(pattern = "CONTROL_SAMPLE", x = x)])()
 
-lst_sample$df2_subset <- npx_data2$SampleID |>
+lst_sample$df2_subset <- OlinkAnalyze::npx_data2$SampleID |>
   unique() |>
   sort() |>
   (\(x) x[!grepl(pattern = "CONTROL_SAMPLE", x = x)])() |>
