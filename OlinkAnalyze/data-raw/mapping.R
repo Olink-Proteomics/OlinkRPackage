ce1afe8539e7061297d98aa8b80080e93ea62565
Kathy Nevola <72228735+kathy-nevola@users.noreply.github.com>
2025-02-05 11:54:25 -0500

    add reveal mapping to sysdata

--- OlinkAnalyze/data-raw/mapping.R
+++ OlinkAnalyze/data-raw/mapping.R
@@ -27,3 +27,32 @@ eHT_e3072_mapping <- readRDS(file = eHT_e3072_mapping_rds) |>
   ) |>
   dplyr::as_tibble()
 rm(eHT_e3072_mapping_rds)
+
+# Explore 3072 to Reveal Mapping
+
+reveal_e3072_mapping_rds <- system.file("extdata",
+                                     "OlinkID_Reveal_mapping.rds",
+                                     package = "OlinkAnalyze",
+                                     mustWork = TRUE)
+reveal_e3072_mapping <- readRDS(file = reveal_e3072_mapping_rds) |>
+  dplyr::rename(
+    "Assay" = "gene_name",
+    "UniProt" = "uniprot",
+    "OlinkID_Reveal" = "olink_id_reveal",
+    "OlinkID_E3072" = "olink_id",
+    "Panel_E3072" = "panel",
+    "Block_E3072" = "block"
+  ) |>
+  dplyr::select(
+    dplyr::all_of(
+      c("Assay", "UniProt")
+    ),
+    dplyr::ends_with("_Reveal"),
+    dplyr::ends_with("_E3072")
+  ) |>
+  dplyr::mutate(
+    OlinkID = paste(.data[["OlinkID_Reveal"]], .data[["OlinkID_E3072"]], sep = "_"),
+    Block_Reveal = "Reveal"
+  ) |>
+  dplyr::as_tibble()
+rm(reveal_e3072_mapping_rds)
