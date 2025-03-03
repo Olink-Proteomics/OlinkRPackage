026a0625e09cd86e1f8cca2bd4b7b9969a390543
klevdiamanti <kdiamanti@outlook.com>
2025-03-03 15:18:57 +0100

    merge get_df_output with get_accepted_file_ext_summary into one script

--- OlinkAnalyze/R/get_others.R
+++ OlinkAnalyze/R/get_others.R
@@ -1,3 +1,16 @@
+#' Prints class type output from read_npx* functions.
+#'
+#' @return A scalar character vector with the class type of outputs from
+#' read_npx* functions.
+#'
+get_df_output_print <- function() {
+  stringr::str_replace_all(
+    string = read_npx_df_output,
+    pattern = "arrow",
+    replacement = "ArrowObject"
+  )
+}
+
 #' Describes acceptable file extension for each file type.
 #'
 #' @return A scalar character vector with one sentence describing the acceptable
--- OlinkAnalyze/tests/testthat/test-get_others.R
+++ OlinkAnalyze/tests/testthat/test-get_others.R
@@ -1,3 +1,17 @@
+# Test get_df_output_print ----
+
+test_that(
+  "get_df_output_print - works",
+  {
+    expect_true(
+      object = read_npx_df_output |>
+        stringr::str_replace_all("arrow", "ArrowObject") |>
+        (\(.) . %in% get_df_output_print())() |>
+        all()
+    )
+  }
+)
+
 # Test get_accepted_file_ext_summary ----
 
 test_that(
