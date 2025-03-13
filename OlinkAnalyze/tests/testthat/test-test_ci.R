fff6e1758d6d9af284c95a69b10719debeb7f75f
Kathy Nevola <72228735+kathy-nevola@users.noreply.github.com>
2025-03-13 11:28:41 -0400

    skip test ci

--- OlinkAnalyze/tests/testthat/test-test_ci.R
+++ OlinkAnalyze/tests/testthat/test-test_ci.R
@@ -1,4 +1,6 @@
-testthat::test_that("Error Message",{
-  testthat::expect_equal(2,3)
+testthat::test_that("Skip Message",{
+  x = 3
+  skip_if(x != 2, "Test is skipped.")
+  testthat::expect_equal(2,2)
 })
 
