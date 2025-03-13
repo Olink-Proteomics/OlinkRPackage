51684339fb2cde3dbe12b38351ff73f7cc2c3174
klevdiamanti <kdiamanti@outlook.com>
2025-03-13 11:20:59 +0100

    Skip tests if msigdbr and msigdbdf are not installed

--- OlinkAnalyze/tests/testthat/test-olink_Pathway_Enrichment.R
+++ OlinkAnalyze/tests/testthat/test-olink_Pathway_Enrichment.R
@@ -1,5 +1,7 @@
 skip_on_cran()
 skip_if_not_installed("clusterProfiler")
+skip_if_not_installed("msigdbr")
+skip_if_not_installed("msigdbdf")
 
 load(file = test_path('data','npx_data_format221010.RData'))
 
@@ -11,26 +13,26 @@ anova_posthoc_results <- olink_anova_posthoc(npx_df, variable = "Site", effect =
 lme_results <- olink_lmer_posthoc(npx_df, variable = "Time", random = "Site", effect = "Time")
 set.seed(123)
 
-duplicate_assay_data <- npx_data1 |> 
-  dplyr::filter(Assay == "MET") |> 
-  dplyr::mutate(OlinkID = "OID01254") |> 
+duplicate_assay_data <- npx_data1 |>
+  dplyr::filter(Assay == "MET") |>
+  dplyr::mutate(OlinkID = "OID01254") |>
   dplyr::mutate(LOD = LOD + 1)
 
-npx_platelod <- npx_data1 |> 
-  rbind(duplicate_assay_data) |> 
-  dplyr::mutate(SampleQC = QC_Warning) |> 
-  dplyr::mutate(PlateLOD = LOD) |> 
+npx_platelod <- npx_data1 |>
+  rbind(duplicate_assay_data) |>
+  dplyr::mutate(SampleQC = QC_Warning) |>
+  dplyr::mutate(PlateLOD = LOD) |>
   dplyr::select(-QC_Warning, -LOD)
 
-npx_maxlod <- npx_data1 |> 
+npx_maxlod <- npx_data1 |>
   rbind(duplicate_assay_data) |>
-  dplyr::mutate(SampleQC = QC_Warning) |> 
-  dplyr::mutate(MaxLOD = LOD) |> 
+  dplyr::mutate(SampleQC = QC_Warning) |>
+  dplyr::mutate(MaxLOD = LOD) |>
   dplyr::select(-QC_Warning, -LOD)
 
-npx_nolod <- npx_data1 |> 
+npx_nolod <- npx_data1 |>
   rbind(duplicate_assay_data) |>
-  dplyr::mutate(SampleQC = QC_Warning) |> 
+  dplyr::mutate(SampleQC = QC_Warning) |>
   dplyr::select(-QC_Warning, -LOD)
 
 ttest_results_no_estimate <- ttest_results %>% dplyr::select(-estimate)
@@ -45,6 +47,8 @@ test_that("Input data equal for different LOD names", {
 
 test_that("T-test GSEA works", {
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   tt_gsea <- olink_pathway_enrichment(npx_df, test_results = ttest_results)
   set.seed(123)
   expect_equal(nrow(tt_gsea), 560)
@@ -53,6 +57,8 @@ test_that("T-test GSEA works", {
 
 test_that("Reactome GSEA works", {
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   tt_gsea_reactome <- olink_pathway_enrichment(npx_df, test_results = ttest_results, ontology = "Reactome")
   set.seed(123)
   expect_equal(nrow(tt_gsea_reactome), 18)
@@ -60,6 +66,8 @@ test_that("Reactome GSEA works", {
 
 test_that("KEGG GSEA works", {
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   tt_gsea_kegg <- olink_pathway_enrichment(npx_df, test_results = ttest_results, ontology = "KEGG")
   set.seed(123)
   expect_equal(nrow(tt_gsea_kegg), 5)
@@ -67,6 +75,8 @@ test_that("KEGG GSEA works", {
 
 test_that("GO GSEA works", {
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   tt_gsea_go <- olink_pathway_enrichment(npx_df, test_results = ttest_results, ontology = "GO")
   set.seed(123)
   expect_equal(nrow(tt_gsea_go), 384)
@@ -75,6 +85,8 @@ test_that("GO GSEA works", {
 
 test_that("T-test ORA works", {
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   tt_ora <- olink_pathway_enrichment(npx_df, test_results = ttest_results, method = "ORA", ontology = "MSigDb")
   set.seed(123)
   expect_equal(nrow(tt_ora), 329)
@@ -82,6 +94,8 @@ test_that("T-test ORA works", {
 
 test_that("Reactome ORA works", {
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   tt_ora_reactome <- olink_pathway_enrichment(npx_df, test_results = ttest_results, method = "ORA", ontology = "Reactome")
   set.seed(123)
   expect_equal(nrow(tt_ora_reactome), 15)
@@ -89,6 +103,8 @@ test_that("Reactome ORA works", {
 
 test_that("KEGG ORA works", {
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   tt_ora_kegg <- olink_pathway_enrichment(npx_df, test_results = ttest_results, method = "ORA", ontology = "KEGG")
   set.seed(123)
   expect_equal(nrow(tt_ora_kegg), 3)
@@ -96,6 +112,8 @@ test_that("KEGG ORA works", {
 
 test_that("GO ORA works", {
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   tt_ora_go <- olink_pathway_enrichment(npx_df, test_results = ttest_results, method = "ORA", ontology = "GO")
   set.seed(123)
   expect_equal(nrow(tt_ora_go), 226)
@@ -104,24 +122,32 @@ test_that("GO ORA works", {
 
 test_that("Error if more than 1 contrast", {
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   set.seed(123)
   expect_error(olink_pathway_enrichment(npx_df, anova_posthoc_results))
 })
 
 test_that("Nonsense method errors",{
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   set.seed(123)
   expect_error(olink_pathway_enrichment(npx_df, anova_posthoc_results, method = "IRA"))
 })
 
 test_that("Unsupported databases flag",{
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   set.seed(123)
   expect_error(olink_pathway_enrichment(npx_df, anova_posthoc_results, ontology = "WikiPathways"))
 })
 
 test_that("Estimate column must be present",{
   skip_if_not_installed("clusterProfiler")
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
   set.seed(123)
   expect_error(olink_pathway_enrichment(npx_df, ttest_results_no_estimate))
 })
@@ -129,16 +155,20 @@ test_that("Estimate column must be present",{
 
 test_that(" ORA warns assays not found in database. ", {
   skip_if_not_installed("clusterProfiler")
-  messages <- capture_messages(olink_pathway_enrichment(npx_data1, test_results = ttest_results, 
-                                                        method = "ORA", ontology = "MSigDb")) 
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
+  messages <- capture_messages(olink_pathway_enrichment(npx_data1, test_results = ttest_results,
+                                                        method = "ORA", ontology = "MSigDb"))
   messages <- paste(messages, collapse = "")
   expect_true(grepl(pattern = "assays are not found in the database.", x = messages))
-  
+
 })
 
 test_that(" gsea warns assays not found in database. ", {
   skip_if_not_installed("clusterProfiler")
-  
+  skip_if_not_installed("msigdbr")
+  skip_if_not_installed("msigdbdf")
+
   messages <- capture_messages(olink_pathway_enrichment(npx_df, test_results = ttest_results))
   messages <- paste(messages, collapse = "")
   expect_true(grepl(pattern = "assays are not found in the database.", x = messages))
