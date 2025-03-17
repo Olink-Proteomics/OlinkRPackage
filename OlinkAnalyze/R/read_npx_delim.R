10ad8063031969b5025a3046b71c29189148f87c
klevdiamanti <kdiamanti@outlook.com>
2025-03-17 17:10:19 +0100

    Use fread from data.table to read delimited files

--- OlinkAnalyze/R/read_npx_delim.R
+++ OlinkAnalyze/R/read_npx_delim.R
@@ -31,8 +31,7 @@
 #'   \code{\link{read_npx_delim_long}}
 #'
 read_npx_delim <- function(file,
-                           out_df = "arrow",
-                           sep = NULL) {
+                           out_df = "arrow") {
 
   # check input ----
 
@@ -45,65 +44,55 @@ read_npx_delim <- function(file,
 
   # read file ----
 
-  # using two tryCatch to check if file is long or wide
+  # using two tryCatch to check if file can be read as long format
+  df_olink <- tryCatch(
+    {
+      data.table::fread(
+        file = file,
+        header = TRUE,
+        stringsAsFactors = FALSE,
+        na.strings = c("NA", ""),
+        check.names = TRUE,
+        data.table = FALSE
+      ) |>
+        dplyr::as_tibble()
+    }, error = function(msg) {
+      cli::cli_abort( # nolint return_linter
+        c(
+          "x" = "Unable to open delimited file: {.file {file}}!",
+          "i" = "Check if {.arg file} is delimited or corrupt."
+        ),
+        call = rlang::caller_env(),
+        wrap = FALSE
+      )
+    }
+  )
 
-  # check if long long works
-  df_olink <- tryCatch({
-    # Try executing long
-    read_npx_delim_long(file = file, sep = sep)
-  }, warning = function(w) {
-    # Handle warnings
-    return("warning")
-  }, error = function(e) {
-    # Handle errors
-    return("error")
-  })
-
-  # if long failed, try wide
-  if (is.character(df_olink) && df_olink %in% c("warning", "error")) {
-    df_olink <- tryCatch({
-      # Try executing wide
-      read_npx_delim_wide(file = file, sep = sep)
-    }, warning = function(w) {
-      # Handle warnings
-      return("warning")
-    }, error = function(e) {
-      # Handle errors
-      return("error")
-    })
+  # if long has only the first two columns not matching VX, where X is
+  # the column name, then then file is in wide format
+  if (identical(x = tail(x = names(df_olink), n = -2L),
+                y = tail(x = paste0("V", seq_len(ncol(df_olink))), n = -2L))
+      && nrow(df_olink) > 2L) {
+    df_olink <- data.table::fread(
+      file = file,
+      header = FALSE,
+      stringsAsFactors = FALSE,
+      na.strings = c("NA", ""),
+      check.names = TRUE,
+      data.table = FALSE
+    ) |>
+      dplyr::as_tibble()
+    names(df_olink) <- paste0("V", seq_len(ncol(df_olink)))
   }
 
-  # check if any error or warning occurred ----
-
-  if (is.character(df_olink) && df_olink %in% c("warning", "error")) {
-
-    # If both functions throw errors, return an error message
-    cli::cli_abort(
-      c(
-        "x" = "Unable to open delimited file: {.file {file}}",
-        "i" = "Check if the input {.arg file} is a delimited file."
-      ),
-      call = rlang::caller_env(),
-      wrap = FALSE
-    )
-
-  }
-
-  # additional checks ----
-
-  # only one column
-
-  if (length(names(df_olink)) == 1L) {
-
+  if (ncol(df_olink) == 1L) {
     cli::cli_warn(
       message = c(
-        "i" = "The delimited file {.file {file}} has only one column. Wrong
-        input {.arg sep} = {.val {sep}}?"
+        "i" = "The delimited file {.file {file}} has only one column!"
       ),
       call = rlang::caller_env(),
       wrap = FALSE
     )
-
   }
 
   # top row is as expected for the corresponding format
@@ -123,262 +112,3 @@ read_npx_delim <- function(file,
 
 #' @rdname read_npx_delim
 read_npx_csv <- read_npx_delim
-
-#' Help function to read long format
-#' `r ansi_collapse_quot(get_olink_data_types())` data from delimited file
-#' exported from Olink software.
-#'
-#' @description
-#' The function can handle delimited files in long format.
-#'
-#' @author
-#'   Klev Diamanti;
-#'   Christoffer Cambronero;
-#'   Kathleen Nevola
-#'
-#' @param file Path to Olink software output delimited file in long format.
-#' Expecting file extensions
-#' `r get_file_ext(name_sub = "delim") |> ansi_collapse_quot()`.
-#' @param sep Character separator of delimited input file. One of `NULL`
-#' (default) for auto-detection, or `r ansi_collapse_quot(accepted_field_sep)`.
-#' Used only for delimited output files from Olink software.
-#'
-#' @return `r ansi_collapse_quot(x = get_df_output_print(), sep = "or")` with
-#' Olink data in long format.
-#'
-#' @seealso
-#'   \code{\link{read_npx_delim_wide}}
-#'   \code{\link{read_npx_delim}}
-#'
-read_npx_delim_long <- function(file,
-                                sep = NULL) {
-
-  # check input ----
-
-  # check if file exists
-  check_file_exists(file = file,
-                    error = TRUE)
-  # check separator
-  sep <- check_field_separator(file = file,
-                               sep = sep)
-
-  # read long file ----
-
-  df_olink <- arrow::open_delim_dataset(
-    sources = file,
-    delim = sep,
-    col_names = TRUE,
-    quoted_na = TRUE,
-    na = c("", "NA")
-  )
-
-  # return ----
-
-  return(df_olink)
-
-}
-
-#' Help function to read wide format
-#' `r get_file_ext(name_sub = "delim") |> ansi_collapse_quot()` data from
-#' delimited file exported from Olink software.
-#'
-#' @description
-#' The function can handle delimited files in wide format.
-#'
-#' @author
-#'   Klev Diamanti
-#'
-#' @param file Path to Olink software output delimited file in wide format.
-#' Expecting file extensions
-#' `r get_file_ext(name_sub = "delim") |> ansi_collapse_quot()`.
-#' @param sep Character separator of delimited input file. One of `NULL`
-#' (default) for auto-detection, or `r ansi_collapse_quot(accepted_field_sep)`.
-#' Used only for delimited output files from Olink software.
-#'
-#' @return `r ansi_collapse_quot(x = get_df_output_print(), sep = "or")` with
-#' Olink data in wide format.
-#'
-#' @seealso
-#'   \code{\link{read_npx_delim_long}}
-#'   \code{\link{read_npx_delim}}
-#'
-read_npx_delim_wide <- function(file,
-                                sep = NULL) {
-
-  # check input ----
-
-  # check if file exists
-  check_file_exists(file = file,
-                    error = TRUE)
-
-  # check separator
-  sep <- check_field_separator(file = file,
-                               sep = sep)
-
-  # read wide file ----
-
-  df_olink <- utils::read.delim(
-    file = file,
-    header = FALSE,
-    sep = sep,
-    blank.lines.skip = FALSE,
-    na.strings = c("", "NA")
-  )
-  colnames(df_olink) <- paste0("V", seq_len(ncol(df_olink)))
-
-  df_olink <- dplyr::as_tibble(df_olink)
-
-  # return ----
-
-  return(df_olink)
-
-}
-
-#' Help function to check and get the separator of a delimited file from Olink
-#' software.
-#'
-#' @author
-#'   Klev Diamanti
-#'
-#' @param file Path to Olink software output delimited file in wide format.
-#' Expecting file extensions
-#' `r get_file_ext(name_sub = "delim") |> ansi_collapse_quot()`.
-#' @param sep Character separator of delimited input file. One of `NULL`
-#' (default) for auto-detection, or `r ansi_collapse_quot(accepted_field_sep)`.
-#' Used only for delimited output files from Olink software.
-#'
-#' @return The file separator `r ansi_collapse_quot(accepted_field_sep)`.
-#'
-#' @seealso
-#'   \code{\link{read_npx_delim_long}}
-#'   \code{\link{read_npx_delim_wide}}
-#'   \code{\link{read_npx_delim}}
-#'
-check_field_separator <- function(file,
-                                  sep = NULL) {
-
-  # check separator
-  if (is.null(sep)) {
-
-    sep <- get_field_separator(file = file)
-
-  } else if (!check_is_scalar_character(string = sep, error = FALSE)) {
-
-    cli::cli_abort(
-      c(
-        "x" = "{.arg sep} should be a string!"
-      ),
-      call = rlang::caller_env(),
-      wrap = FALSE
-    )
-
-  } else if (!(sep %in% accepted_field_sep)) {
-
-    cli::cli_abort(
-      c(
-        "x" = "Unexpected separator: {.val {sep}}",
-        "i" = "Expected one of: {.val {accepted_field_sep}}"
-      ),
-      call = rlang::caller_env(),
-      wrap = FALSE
-    )
-
-  }
-
-  return(sep)
-}
-
-#' Help function to get the separator of a delimited file from Olink software.
-#'
-#' @description
-#' This function uses the first line of the provided file to determine the
-#' separator of the file.
-#'
-#' \strong{Note:} The function does not allow presence of commas and semicolons
-#' on the same line.
-#'
-#' @author
-#'   Klev Diamanti
-#'
-#' @param file Path to Olink software output delimited file in wide format.
-#' Expecting file extensions
-#' `r get_file_ext(name_sub = "delim") |> ansi_collapse_quot()`.
-#'
-#' @return The file separator `r ansi_collapse_quot(accepted_field_sep)`.
-#'
-#' @seealso
-#'   \code{\link{read_npx_delim_long}}
-#'   \code{\link{read_npx_delim_wide}}
-#'   \code{\link{read_npx_delim}}
-#'
-get_field_separator <- function(file) {
-
-  # check input is a string
-  check_is_scalar_character(string = file,
-                            error = TRUE)
-
-  # check if file exists
-  check_file_exists(file = file,
-                    error = TRUE)
-
-  # get the first line of the file
-  file_line_1 <- readLines(file, n = 1L)
-
-  # throw an error if line 1 is empty or the file is empty
-  if (!check_is_scalar_character(string = file_line_1, error = FALSE)) {
-
-    cli::cli_abort(
-      c(
-        "x" = "Unable to read header line from {.file {file}}",
-        "i" = "Empty header or empty {.arg file}?"
-      ),
-      call = rlang::caller_env(),
-      wrap = FALSE
-    )
-
-  }
-
-  # count fileds for semicolon
-  num_fields_semicolon <- utils::count.fields(
-    file = textConnection(file_line_1),
-    sep = ";"
-  )
-
-  # count fileds for comma
-  num_fields_comma <- utils::count.fields(
-    file = textConnection(file_line_1),
-    sep = ","
-  )
-
-  if (num_fields_semicolon > 1L && num_fields_comma == 1L) {
-
-    return(";")
-
-  } else if (num_fields_semicolon == 1L && num_fields_comma > 1L) {
-
-    return(",")
-
-  } else if (num_fields_semicolon > 1L && num_fields_comma > 1L) {
-
-    cli::cli_abort( # nolint return_linter
-      c(
-        "x" = "Unable to identify the separator of the file: {.file {file}}",
-        "i" = "Both {.val {accepted_field_sep}} are present in header line."
-      ),
-      call = rlang::caller_env(),
-      wrap = FALSE
-    )
-
-  } else {
-
-    cli::cli_abort( # nolint return_linter
-      c(
-        "x" = "Unable to identify the separator of the file: {.file {file}}",
-        "i" = "Expected one of {.val {accepted_field_sep}}!"
-      ),
-      call = rlang::caller_env(),
-      wrap = FALSE
-    )
-
-  }
-}
