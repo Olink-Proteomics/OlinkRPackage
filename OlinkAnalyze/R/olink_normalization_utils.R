975a058bdae19c74eb7d76d3815eca272caa1da7
klevdiamanti <kdiamanti@outlook.com>
2025-04-11 17:06:01 +0200

    Sketch mutli-quant support

--- OlinkAnalyze/R/olink_normalization_utils.R
+++ OlinkAnalyze/R/olink_normalization_utils.R
@@ -929,6 +929,81 @@ olink_norm_input_check_df_cols <- function(lst_df) {
 
 }
 
+# lst_req_col_quant is lst_req_col$quant from olink_norm_input_check_df_cols
+#
+# this functuion will be called by olink_norm_input_check_df_cols to resolve the
+# the quantification columns.
+olink_norm_input_check_quant <- function(lst_req_col_quant) {
+
+  if (any(sapply(lst_req_col_quant, length) == 0L)) {
+    # no quantification identified in at least one datasets -> ERROR
+  }
+
+  if (length(lst_req_col_quant) == 1L) {
+    # this holds if we are doing reference normalization, where one dataset is
+    # required.
+
+    if (length(lst_req_col_quant[[1L]]) > 1L) {
+      # choose quantification method by order from required_cols$quant in
+      # function "olink_norm_input_check_df_cols".
+      #
+      # Print an informative message.
+      #
+      # Here "lst_req_col_quant" has to be updated!
+    }
+    # if length(lst_req_col_quant[[1L]]) == 0L has been checked in the very
+    # beginning of the function.
+    #
+    # no need to check length(lst_req_col_quant[[1L]]) == 1L because then we
+    # have only one match, and nothing to do.
+  } else {
+    # this holds if we are doing bridge normalization, subset normalization, and
+    # cross-product normalization.
+
+    # get the quantification columns present in all datasets
+    quant_col_shared <- Reduce(intersect, lst_req_col_quant)
+
+    if (length(quant_col_shared) == 0L) {
+      # the datasets have one or more quantifications each, but none of them
+      # is common between/among them. -> ERROR
+      cli::cli_abort(
+        c(
+          "x" = "{cli::qty(quant_col)} Dataset{?s} are not quantified with the
+        same method:",
+          paste0("* ", names(quant_col), ": ", quant_col),
+          "i" = "Re-export data with shared quantifications."
+        ),
+        call = rlang::caller_env(),
+        wrap = FALSE
+      )
+      # remove section "check that quant methods are the same" from
+      # "olink_norm_input_check_df_cols" as they are overlapping.
+    } else if (length(quant_col_shared) == 1L) {
+      # if
+      # a. all datasets have only one quantification method
+      # b. one dataset has one quantification method, the other one has
+      #    multiple, but they have one quantification method in common.
+      # c. both datasets have multiple quantification columns, but they have one
+      #    quantification method in common.
+      #
+      # Print an informative message only if
+      # sapply(lst_req_col_quant, length) |> unique() |> length() != 1L
+      #
+      # Here "lst_req_col_quant" has to be updated!
+    } else {
+      # both datasets have more than one quantification methods. We should use
+      # choose quantification method by order in function
+      # "olink_norm_input_check_df_cols".
+      #
+      # Print an informative message.
+      #
+      # Here "lst_req_col_quant" has to be updated!
+    }
+  }
+
+  return(lst_req_col_quant) # the updated version of it
+}
+
 #' Check if bridge or cross-platform normalization
 #'
 #' @author
