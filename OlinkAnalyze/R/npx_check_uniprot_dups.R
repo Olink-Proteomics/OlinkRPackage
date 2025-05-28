83f4696ab5c66fb3be2414a0ec524426c37c6cab
Kathy Nevola <72228735+kathy-nevola@users.noreply.github.com>
2025-05-28 16:07:22 -0400

    Lint

--- OlinkAnalyze/R/npx_check_uniprot_dups.R
+++ OlinkAnalyze/R/npx_check_uniprot_dups.R
@@ -1,11 +1,11 @@
-assay_identifiers <- function(df){
+assay_identifiers <- function(df) {
   assay_ids <- df |>
     dplyr::select(dplyr::any_of(c("OlinkID", "UniProt"))) |>
     dplyr::distinct()
 
 
   # warning if duplicate set of identifiers for 1 assay
-  duplicated_oids<-unique(assay_ids$OlinkID[duplicated(assay_ids$OlinkID)])
+  duplicated_oids <- unique(assay_ids$OlinkID[duplicated(assay_ids$OlinkID)])
   uniprot_replace <- data.frame(OlinkID = "",
                                 UniProt = "",
                                 new_UniProt = "")
@@ -15,7 +15,7 @@ assay_identifiers <- function(df){
     uniprot_dups <- assay_ids |>
       dplyr::filter(OlinkID %in% duplicated_oids) |>
       dplyr::select(dplyr::any_of(c("OlinkID", "UniProt", "Assay")))
-    uniprot_original <- uniprot_dups[!duplicated(uniprot_dups$OlinkID),]
+    uniprot_original <- uniprot_dups[!duplicated(uniprot_dups$OlinkID), ]
     uniprot_original <- uniprot_original |>
       dplyr::rename("new_UniProt" = "UniProt")
     uniprot_replace <- dplyr::left_join(uniprot_dups,
@@ -27,26 +27,27 @@ assay_identifiers <- function(df){
                               "` will be replaced with UniProt ID `",
                               uniprot_replace$new_UniProt, "`.\n")
 
-    cli::cli_warn(c("!" =paste0(
-      "{length(duplicated_oids)} OlinkID{?s} ha{?s/ve} ",
-      "multiple unique UniProt IDs. ",
-      "The first iteration",
-      " will be used for downstream analysis."),
-      "*" = uniprot_message))
+    cli::cli_warn(c("!" = paste0(
+                                 "{length(duplicated_oids)} OlinkID{?s}",
+                                 " ha{?s/ve} multiple unique UniProt IDs. ",
+                                 "The first iteration",
+                                 " will be used for downstream analysis."),
+    "*" = uniprot_message))
   }
 
   return(uniprot_replace)
 }
 
-uniprot_replace <- function(df, npxCheck){
-  if (nrow(npxCheck$uniprot_replace) != 0){
+uniprot_replace <- function(df, npx_check) {
+  if (nrow(npx_check$uniprot_replace) != 0) {
     df <- df |>
-      dplyr::left_join(npxCheck$uniprot_replace,
+      dplyr::left_join(npx_check$uniprot_replace,
                        by = c("UniProt", "OlinkID")) |>
-      dplyr::mutate(new_UniProt = ifelse(is.na( .data[["new_UniProt"]]),
+      dplyr::mutate(new_UniProt = ifelse(is.na(.data[["new_UniProt"]]),
                                          .data[["UniProt"]],
                                          .data[["new_UniProt"]])) |>
-      dplyr::mutate(UniProt = ifelse(.data[["UniProt"]] !=  .data[["new_UniProt"]],
+      dplyr::mutate(UniProt = ifelse(.data[["UniProt"]] !=
+                                       .data[["new_UniProt"]],
                                      .data[["new_UniProt"]],
                                      .data[["UniProt"]])) |>
       dplyr::select(-dplyr::any_of("new_UniProt"))
