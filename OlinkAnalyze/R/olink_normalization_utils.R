2270ba8978e35d30d2dae749227f649c8b402b17
klevdiamanti <kdiamanti@outlook.com>
2025-07-01 14:00:03 +0200

    Update message

--- OlinkAnalyze/R/olink_normalization_utils.R
+++ OlinkAnalyze/R/olink_normalization_utils.R
@@ -965,11 +965,10 @@ olink_norm_input_check_quant <- function(lst_req_col_quant, quant_cols_set) {
         head(n = 1L)
 
       cli::cli_inform(
-        c("!" = "Multiple quantification methods detected.",
-          "i" = paste0(lst_req_col_quant[[1L]],
-                       " will be used for normalization."),
-          "Priority list  `'NPX','Quantified_value','Ct'` used  to determine
-          quantification used in the normalization process.")
+        c("!" = "Multiple quantification methods detected in dataset
+          {.val {names(lst_req_col_quant)[1L]}}.",
+          "i" = "{.val {lst_req_col_quant[1L]}} will be used for normalization
+          based on the priority list: {.val {quant_cols_set}}.")
       )
 
       lst_req_col_quant[[1L]] <- lst_req_col_quant[[1L]] |> as.character()
