dfff10e7f949398a3898adfa5e672f777168f804
Klev Diamanti <kdiamanti@outlook.com>
2025-07-28 08:02:11 +0300

    New function to inherit arguments

--- OlinkAnalyze/R/utils.R
+++ OlinkAnalyze/R/utils.R
@@ -63,3 +63,43 @@ ansi_collapse_quot <- function(x,
 #' (`default = FALSE`).
 #'
 .check_params <- function(error) {}
+
+#' Common parameters for read_npx-related functions.
+#'
+#' @author
+#'   Klev Diamanti
+#'
+#' @param filename Path to Olink software output file in wide or long format.
+#' Expecting extensions `r get_file_ext_summary()`.
+#' @param file Path to Olink software output file in wide or long format.
+#' Expecting extensions `r get_file_ext_summary()`.
+#' @param out_df The class of the output dataset. One of
+#' `r ansi_collapse_quot(x = read_npx_df_output, sep = "or")`. (default =
+#' "tibble")
+#' @param long_format Boolean marking format of input file. One of `NULL`
+#' (default) for auto-detection, or `TRUE` for long format and `FALSE` for
+#' wide format files.
+#' @param olink_platform Olink platform used to generate the input file.
+#' One of `NULL` (default) for auto-detection,
+#' or `r ansi_collapse_quot(x = accepted_olink_platforms$name)`.
+#' @param data_type Quantification method of the input data. One of `NULL`
+#' (default) for auto-detection, `r ansi_collapse_quot(get_olink_data_types())`.
+#' @param .ignore_files Character vector of files included in the zip-compressed
+#' Olink software output files that should be ignored. Used only for
+#' zip-compressed input files (default = \emph{c("README.txt")}).
+#' @param quiet Boolean to print a confirmation message when reading the input
+#' file. Applies to excel or delimited input only. `TRUE` (default) to not print
+#' and `FALSE` to print.
+#'
+#' @return Dataset,
+#' `r ansi_collapse_quot(x = get_df_output_print(), sep = "or")`, with Olink
+#' data in long format.
+#'
+.read_npx_args <- function(filename,
+                           file,
+                           out_df,
+                           long_format,
+                           olink_platform,
+                           data_type,
+                           .ignore_files,
+                           quiet) {}
