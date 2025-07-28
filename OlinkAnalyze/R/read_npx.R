40a2bc3b16291f19913b947231c0f38edc8dcd91
Klev Diamanti <kdiamanti@outlook.com>
2025-07-28 07:52:55 +0300

    Inherit params and return

--- OlinkAnalyze/R/read_npx.R
+++ OlinkAnalyze/R/read_npx.R
@@ -16,29 +16,10 @@
 #'   Olof Mansson;
 #'   Marianne Sandin
 #'
-#' @param filename Path to Olink software output file in wide or long format.
-#' Expecting extensions `r get_file_ext_summary()`.
-#' @param out_df The class of the output dataset. One of
-#' `r ansi_collapse_quot(read_npx_df_output)`. (default = "tibble")
-#' @param long_format Boolean marking format of input file. One of `NULL`
-#' (default) for auto-detection, or `TRUE` for long format and `FALSE` for
-#' wide format files.
-#' @param olink_platform Olink platform used to generate the input file.
-#' One of `NULL` (default) for auto-detection,
-#' or `r ansi_collapse_quot(x = accepted_olink_platforms$name)`.
-#' @param data_type Quantification method of the input data. One of `NULL`
-#' (default) for auto-detection, `r ansi_collapse_quot(get_olink_data_types())`.
-#' @param .ignore_files Character vector of files included in the zip-compressed
-#' Olink software output files that should be ignored. Used only for
-#' zip-compressed input files (default = \emph{c("README.txt")}).
-#' @param quiet Boolean to print a confirmation message when reading the input
-#' file. Applies to excel or delimited input only. `TRUE` (default) to not print
-#' and `FALSE` to print.
+#' @inherit .read_npx_args params return
 #' @param legacy Boolean to run the legacy version of the read_npx function.
 #' \strong{Important: should be used only to wide format files from Target 96 or
 #' Target 48 with NPX Software version earlier than 1.8!} (default `FALSE`).
-#'
-#' @return Dataset, `r get_df_output_print()`, with Olink data in long format.
 #'
 #' @keywords
 #'   NPX;
