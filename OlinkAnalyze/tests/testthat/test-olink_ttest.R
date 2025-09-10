a8f72d910e6d5be17d7354aa074673f64e04da93
klevdiamanti <kdiamanti@outlook.com>
2025-09-10 11:40:01 +0200

    Tests for olink_ttest

---
+++ OlinkAnalyze/tests/testthat/test-olink_ttest.R
@@ -0,0 +1,126 @@
+test_that(
+  "olink_ttest - works - non-paired t-test",
+  {
+    # Load reference results
+    # tests are skipped if files are absent
+    reference_results <- get_example_data(filename = "reference_results.rds")
+
+    skip_if_not_installed(pkg = "broom")
+
+    check_log <- check_npx(df = npx_data1) |>
+      suppressMessages() |>
+      suppressWarnings()
+
+    # run olink_ttest
+    expect_message(
+      object = expect_message(
+        object = expect_message(
+          object = t_test_res <- olink_ttest(
+            df = npx_data1,
+            variable = "Treatment",
+            check_log = check_log
+          ),
+          regexp = paste("Samples removed due to missing variable levels:",
+                         "CONTROL_SAMPLE_AS 1, CONTROL_SAMPLE_AS 2")
+        ),
+        regexp = "Variable converted from character to factor: Treatment"
+      ),
+      regexp = "T-test is performed on Treated - Untreated."
+    )
+
+    expect_equal(
+      object = t_test_res,
+      expected = reference_results$t_test,
+      tolerance = 1e-4
+    )
+  }
+)
+
+test_that(
+  "olink_ttest - works - paired t-test",
+  {
+    # Load reference results
+    # tests are skipped if files are absent
+    reference_results <- get_example_data(filename = "reference_results.rds")
+
+    skip_if_not_installed(pkg = "broom")
+
+    npx_df <- npx_data1 |>
+      dplyr::filter(
+        .data[["Time"]] %in% c("Baseline", "Week.6")
+      )
+
+    check_log <- check_npx(df = npx_df) |>
+      suppressMessages() |>
+      suppressWarnings()
+
+    # run olink_ttest
+    expect_message(
+      object = expect_message(
+        object = paired_t_test_res <- olink_ttest(
+          df = npx_df,
+          variable = "Time",
+          pair_id = "Subject",
+          check_log = check_log
+        ),
+        regexp = "Variable converted from character to factor: Time"
+      ),
+      regexp = "Paired t-test is performed on Baseline - Week.6."
+    )
+
+    expect_equal(
+      object = paired_t_test_res,
+      expected = reference_results$t_test_paired,
+      tolerance = 1e-4
+    )
+  }
+)
+
+test_that(
+  "olink_ttest - error - no df and/or variable provided",
+  {
+    skip_if_not_installed(pkg = "broom")
+
+    expect_error(
+      object = olink_ttest(),
+      regexp = "The df and variable arguments need to be specified."
+    )
+
+    expect_error(
+      object = olink_ttest(
+        df = npx_data1
+      ),
+      regexp = "The df and variable arguments need to be specified."
+    )
+
+    expect_error(
+      object = olink_ttest(
+        variable = "Treatment"
+      ),
+      regexp = "The df and variable arguments need to be specified."
+    )
+  }
+)
+
+test_that(
+  "olink_ttest - error - more than 1 levels",
+  {
+    skip_if_not_installed(pkg = "broom")
+
+    expect_error(
+      object = expect_message(
+        object = expect_message(
+          object = olink_ttest(
+            df = npx_data1,
+            variable = "Time"
+          ),
+          regexp = paste("Samples removed due to missing variable levels:",
+                         "CONTROL_SAMPLE_AS 1, CONTROL_SAMPLE_AS 2")
+        ),
+        regexp = "Variable converted from character to factor: Time"
+      ),
+      regexp = paste("The number of levels in the factor needs to be 2.",
+                     "Your factor has 3 levels.")
+    )
+  }
+)
