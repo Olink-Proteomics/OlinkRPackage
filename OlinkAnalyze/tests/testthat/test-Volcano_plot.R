ab159b5f5a8cb49d914c50a72aba97416c3a2f9d
Klev Diamanti <kdiamanti@outlook.com>
2025-09-23 21:18:08 +0200

    Restore tests

---
+++ OlinkAnalyze/tests/testthat/test-Volcano_plot.R
@@ -0,0 +1,21 @@
+#Load reference results
+
+refRes_file <- testthat::test_path('data','refResults.RData')
+load(refRes_file)
+
+set.seed(10) #There's some randomness to how the labels are placed on the plot => failed test. Setting the seed should avoid this
+volcano_plot <- olink_volcano_plot(ref_results$t.test_results,
+                                   olinkid_list = c(""))
+
+volcano_plot2 <- olink_volcano_plot(ref_results$t.test_results,
+                                   olinkid_list = c(""),
+                                   coloroption =  c('teal', 'pink'))
+
+
+test_that("olink_volcano_plot works", {
+  skip_if_not_installed("vdiffr")
+  skip_on_cran()
+
+  vdiffr::expect_doppelganger('volcano plot', volcano_plot)
+  vdiffr::expect_doppelganger('volcano plot with coloroption', volcano_plot2)
+})
---
+++ OlinkAnalyze/tests/testthat/test-olink_qc_plot.R
@@ -0,0 +1,26 @@
+#Load data with hidden/excluded assays (all NPX=NA)
+load(file = test_path('data','npx_data_format221010.RData'))
+load(file = test_path('data','npx_data_format221121.RData'))
+
+
+qc_plot <- npx_data1 %>%
+  mutate(SampleID = paste(SampleID, "_", Index, sep = "")) %>%
+  olink_qc_plot(label_outliers = FALSE)
+
+qc_plot2 <- npx_data1 %>%
+  mutate(SampleID = paste(SampleID, "_", Index, sep = "")) %>%
+  olink_qc_plot(coloroption =  c('teal', 'pink'), label_outliers = FALSE)
+
+test_that("olink_qc_plot works", {
+  expect_warning(olink_qc_plot(npx_data_format221010)) # data with all NPX=NA for some assays
+  expect_warning(olink_qc_plot(npx_data_format221121)) # data with all NPX=NA for some assays
+  expect_warning(olink_qc_plot(npx_data_extended_format221121)) # data with all NPX=NA for some assays
+})
+
+test_that("olink_qc_plot works - vdiffr", {
+  skip_on_cran()
+  skip_if_not_installed("vdiffr")
+
+  vdiffr::expect_doppelganger('QC plot', qc_plot)
+  vdiffr::expect_doppelganger('QC plot with coloroption', qc_plot2)
+})
