bf43855fbc27b4f3af364e70f581b8248d247d00
klevdiamanti <kdiamanti@outlook.com>
2025-09-23 16:22:26 +0200

    Clean up theme

--- OlinkAnalyze/R/Olink_theme.R
+++ OlinkAnalyze/R/Olink_theme.R
@@ -26,7 +26,20 @@ set_plot_theme <- function(font = "Arial") {
 
   if (getOption("OlinkAnalyze.allow.font.load", default = TRUE)) {
     if (requireNamespace("showtext", quietly = TRUE)) {
-      if (font %in% fonts_system()) {
+      # if in testing mode, use a common font across operating systems
+      if (is_testing()) {
+        # If the font is already available in systemfonts, or showtext, add it
+        # One approach: try to see if the font is installed in system; if not,
+        # you may register it manually (via font_add or similar).
+        usefont <- "Arimo Regular"
+
+        # register font if needed
+        register_font(family = usefont)
+
+        # Turn on showtext automatic rendering
+        showtext::showtext_auto()
+
+      } else if (font %in% fonts_system()) {
 
         # If the font is already available in systemfonts, or showtext, add it
         # One approach: try to see if the font is installed in system; if not,
@@ -34,17 +47,12 @@ set_plot_theme <- function(font = "Arial") {
         usefont <- font
 
         # register font if needed
-        register_font(family = font)
+        register_font(family = usefont)
 
         # Turn on showtext automatic rendering
         showtext::showtext_auto()
       }
     }
-  }
-
-  # if in testing mode, use a common font across operating systems
-  if (is_testing()) {
-    usefont <- "sans"
   }
 
   olink_theme <- ggplot2::theme_bw() +
