a0c9b10fa4f0c60ebdb46185bac9e5377bcf8e47
klevdiamanti <kdiamanti@outlook.com>
2025-09-23 16:05:19 +0200

    Print message

--- OlinkAnalyze/R/Olink_theme.R
+++ OlinkAnalyze/R/Olink_theme.R
@@ -55,6 +55,8 @@ set_plot_theme <- function(font = "Arial") {
     }
   }
 
+  print(paste("Use this font:", usefont))
+
   olink_theme <- ggplot2::theme_bw() +
     ggplot2::theme(
       panel.grid.major = ggplot2::element_blank(),
