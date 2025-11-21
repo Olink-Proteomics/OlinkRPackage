906e530b43e79d5296d89f47743ad381986da001
Klev Diamanti <kdiamanti@outlook.com>
2025-11-21 15:21:10 +0100

    Include check for presence of snapshot

--- OlinkAnalyze/tests/testthat/test-dist_plot.R
+++ OlinkAnalyze/tests/testthat/test-dist_plot.R
@@ -22,8 +22,12 @@ test_that("olink_dist_plot works", {
 test_that("olink_dist_plot works - vdiffr", {
   skip_on_cran()
   skip_if_not_installed("vdiffr")
-  skip_unless_r_compat(">= 4.5.2")
 
-  vdiffr::expect_doppelganger('Distribution plot', distribution_plot)
-  vdiffr::expect_doppelganger('Distribution plot col by treatment', distribution_plot_treatColor)
+  distribution_plot_name <- "Distribution plot"
+  check_snap_exist(test_dir_name = "dist_plot", snap_name = distribution_plot_name)
+  vdiffr::expect_doppelganger(distribution_plot_name, distribution_plot)
+
+  distribution_plot_treatColor_name <- 'Distribution plot col by treatment'
+  check_snap_exist(test_dir_name = "dist_plot", snap_name = distribution_plot_treatColor_name)
+  vdiffr::expect_doppelganger(distribution_plot_treatColor_name, distribution_plot_treatColor)
 })
