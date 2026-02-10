080ee1658aec9c4f5bd34f741b850465082e85eb
Kang Dong <132228364+KangD-dev@users.noreply.github.com>
2026-02-10 10:56:29 -0800

    Add "outdated_darid_panel_archive" list to allow it used in check_darid, and re-used in other functions.

--- OlinkAnalyze/data-raw/utils_spec.R
+++ OlinkAnalyze/data-raw/utils_spec.R
@@ -78,3 +78,15 @@ check_npx_lst_names <- c(
   "non_unique_uniprot",
   "darid_invalid"
 )
+
+## Rules defining outdated DARID and panel archive combinations
+## Used in check_darid(), part of check_npx().
+outdated_darid_panel_archive <- list(
+  darid_list = c(
+    "D10007","D20007","D30007","D40007","D50007","D60007","D70007","D80007",
+    "D10008","D20008","D30008","D40008","D50008","D60008","D70008","D80008",
+    "D10010","D20010","D30010","D40010","D50010","D60010","D70010","D80010",
+    "D10014","D20014","D30014","D40014","D50014","D60014","D70014","D80014"
+  ),
+  min_version = "1.5"
+)
