d43821c3fc86270112ad35d0d0231b8a9d3fd84a
Kang Dong <132228364+KangD-dev@users.noreply.github.com>
2026-02-19 10:29:05 -0800

    Update the test-olink_lod

--- OlinkAnalyze/tests/testthat/test-olink_lod.R
+++ OlinkAnalyze/tests/testthat/test-olink_lod.R
@@ -1,100 +1,100 @@
 # Test check_ht_fixed_lod_version ----
 
-test_that("no message when darid_invalid is empty", {
-  expect_no_message(
-    check_ht_fixed_lod_version(
-      check_log = list(darid_invalid = character(0)),
-      lod_file  = data.frame(Panel = "Explore_HT")
-    )
-  )
-})
-
-test_that("warn when missing Version column", {
-  msgs <- capture_messages(
-    check_ht_fixed_lod_version(
-      check_log = list(darid_invalid = "D10007"),
-      lod_file  = data.frame(Panel = "Explore_HT")
-    )
-  )
-
-  expect_true(
-    any(stringr::str_detect(
-      msgs,
-      "Outdated version of Fixed LOD file detected"
-    )),
-
-    any(stringr::str_detect(
-      msgs,
-      "Please download the newest version from Olink.com"
-    ))
-  )
-})
-
-test_that("warn when multiple version values are presented", {
-  msgs <- capture_messages(
-    check_ht_fixed_lod_version(
-      check_log = list(darid_invalid = "D10007"),
-      lod_file = data.frame(
-        Panel = rep("Explore_HT", 2),
-        Version = c("6.0.0", "7.0.0"),
-        stringsAsFactors = FALSE
+test_that(
+  "no message when darid_invalid is empty",
+  {
+    check_log = list(
+      darid_invalid = dplyr::tibble(
+        "DataAnalysisRefID" = character(0L),
+        "PanelDataArchiveVersion" = character(0L)
       )
     )
-  )
 
-  expect_true(
-    any(stringr::str_detect(
-      msgs,
-      "Multiple Fixed LOD versions detected in the file"
-    ))
-  )
-})
+    lod_file <- dplyr::tibble(
+      "Panel" = "Explore_HT",
+      "Version" = "7.0.0"
+    )
 
-test_that("warn when version lower than minimum", {
-  msgs <- capture_messages(
-    check_ht_fixed_lod_version(
-      check_log = list(darid_invalid = "D10007"),
-      lod_file = data.frame(
-        Panel = "Explore_HT",
-        Version = "5.9.0",
-        stringsAsFactors = FALSE
+    expect_no_message(
+      check_ht_fixed_lod_version(
+        check_log = check_log,
+        lod_file  = lod_file
       )
     )
-  )
+  }
+)
 
-  expect_true(any(stringr::str_detect(
-    msgs,
-    "Outdated version of Fixed LOD file detected"
-  )))
-
-  expect_true(any(stringr::str_detect(
-    msgs,
-    stringr::fixed("Detected version: 5.9.0")
-  )))
-
-  expect_true(any(stringr::str_detect(
-    msgs,
-    stringr::fixed("Minimum required version: 6.0.0")
-  )))
-
-  expect_true(any(stringr::str_detect(
-    msgs,
-    stringr::fixed("Please download the newest version from Olink.com")
-  )))
-
-})
-
-test_that("works - version >= min_version passes silently", {
-
-  expect_no_message(
-    check_ht_fixed_lod_version(
-      check_log = list(darid_invalid = "D10007"),
-      lod_file = data.frame(
-        Panel = "Explore_HT",
-        Version = "6.0.0",
-        stringsAsFactors = FALSE
+test_that(
+  "warn when missing Version column",
+  {
+    check_log = list(
+      darid_invalid = dplyr::tibble(
+        "DataAnalysisRefID" = c("D10007", "D20007", "D30007"),
+        "PanelDataArchiveVersion" = rep("1.3.0", 3L)
       )
     )
-  )
 
-})
+    lod_file <- dplyr::tibble(
+      "Panel" = "Explore_HT"
+    )
+
+    expect_warning(
+      object = check_ht_fixed_lod_version(
+        check_log = check_log,
+        lod_file  = lod_file),
+
+      regexp = "Outdated version of Fixed LOD file detected."
+    )
+  }
+)
+
+test_that(
+  "warn when multiple version values are presented",
+  {
+    check_log = list(
+      darid_invalid = dplyr::tibble(
+        "DataAnalysisRefID" = c("D10007", "D20007", "D30007"),
+        "PanelDataArchiveVersion" = rep("1.3.0", 3L)
+      )
+    )
+
+    lod_file <- dplyr::tibble(
+      "Panel" = rep("Explore_HT", 2L),
+      "Version" = c("7.0.0", "9.0.0")
+    )
+
+    expect_warning(
+      object = check_ht_fixed_lod_version(
+        check_log = check_log,
+        lod_file  = lod_file),
+
+      regexp = "Multiple versions detected: \"7.0.0\" and \"9.0.0\"."
+    )
+  }
+)
+
+test_that(
+  "warn when version < 6.0.0",
+  {
+    check_log = list(
+      darid_invalid = dplyr::tibble(
+        "DataAnalysisRefID" = c("D10007", "D20007", "D30007"),
+        "PanelDataArchiveVersion" = rep("1.3.0", 3L)
+      )
+    )
+
+    lod_file <- dplyr::tibble(
+      "Panel" = "Explore_HT",
+      "Version" = "5.0.0"
+    )
+
+    expect_warning(
+      object = check_ht_fixed_lod_version(
+        check_log = check_log,
+        lod_file  = lod_file),
+
+      regexp = "Outdated version of Fixed LOD file detected."
+    )
+
+  }
+)
