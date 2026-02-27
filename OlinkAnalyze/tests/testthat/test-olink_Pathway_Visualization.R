f7c25b21cb75f8f02a1877473446202b89c7ad4d
kathy-nevola <kathy.nevola@olink.com>
2026-02-27 16:31:16 -0500

    add check_NPX and lint visualization

--- OlinkAnalyze/tests/testthat/test-olink_Pathway_Visualization.R
+++ OlinkAnalyze/tests/testthat/test-olink_Pathway_Visualization.R
@@ -2,27 +2,31 @@ skip_on_cran()
 skip_on_ci()
 skip_if_not_installed("clusterProfiler")
 skip_if_not_installed("ggplot2", minimum_version = "3.4.0")
-skip_if_not_installed("msigdbr", minimum_version = "9.0.0")
-skip_if_not_installed("msigdbdf")
+skip_if_not_installed("msigdbr", minimum_version = "24.1.0")
 
 set.seed(123)
 
 npx_df <- npx_data1 %>%
-  dplyr::filter(!stringr::str_detect(SampleID, "CONTROL")) %>%
+  dplyr::filter(!stringr::str_detect(.data[["SampleID"]], "CONTROL")) %>%
   dplyr::filter(Site %in% c("Site_C", "Site_E")) %>%
   dplyr::filter(Time %in% c("Baseline", "Week.12")) %>%
   dplyr::filter(PlateID == "Example_Data_1_CAM.csv")
 
+check_log <- check_npx(npx_df)
 
-ttest_results <- olink_ttest(df=npx_df,
-                             variable = 'Treatment',
-                             alternative = 'two.sided')
+
+ttest_results <- olink_ttest(df = npx_df,
+                             check_log = check_log,
+                             variable = "Treatment",
+                             alternative = "two.sided")
 
 
 gsea_results <- olink_pathway_enrichment(data = npx_df,
+                                         check_log = check_log,
                                          ontology = "Reactome",
                                          test_results = ttest_results)
 ora_results <- olink_pathway_enrichment(data = npx_df,
+                                        check_log = check_log,
                                         ontology = "Reactome",
                                         test_results = ttest_results,
                                         method = "ORA")
@@ -35,7 +39,7 @@ test_that("Valid Keyword needed", {
                                            keyword = "hfdklahfajikshf"))
 })
 
-test_that("Plot works",{
+test_that("Plot works", {
   gsea_vis <- olink_pathway_visualization(enrich_results = gsea_results)
   gsea_vis_keyword <- olink_pathway_visualization(enrich_results = gsea_results,
                                                   keyword = "immune")
@@ -52,18 +56,22 @@ test_that("Plot works",{
   set.seed(123)
 
   gsea_vis_name <- "GSEA Visualization"
-  check_snap_exist(test_dir_name = "olink_Pathway_Visualization", snap_name = gsea_vis_name)
+  check_snap_exist(test_dir_name = "olink_Pathway_Visualization",
+                   snap_name = gsea_vis_name)
   vdiffr::expect_doppelganger(gsea_vis_name, gsea_vis)
 
   gsea_vis_keyword_name <- "GSEA Vis with Keyword"
-  check_snap_exist(test_dir_name = "olink_Pathway_Visualization", snap_name = gsea_vis_keyword_name)
+  check_snap_exist(test_dir_name = "olink_Pathway_Visualization",
+                   snap_name = gsea_vis_keyword_name)
   vdiffr::expect_doppelganger(gsea_vis_keyword_name, gsea_vis_keyword)
 
   ora_vis_terms_name <- "ORA Vis with Terms"
-  check_snap_exist(test_dir_name = "olink_Pathway_Visualization", snap_name = ora_vis_terms_name)
+  check_snap_exist(test_dir_name = "olink_Pathway_Visualization",
+                   snap_name = ora_vis_terms_name)
   vdiffr::expect_doppelganger(ora_vis_terms_name, ora_vis_terms)
 
   ora_vis_keyword_name <- "ORA Vis with keyword"
-  check_snap_exist(test_dir_name = "olink_Pathway_Visualization", snap_name = ora_vis_keyword_name)
+  check_snap_exist(test_dir_name = "olink_Pathway_Visualization",
+                   snap_name = ora_vis_keyword_name)
   vdiffr::expect_doppelganger(ora_vis_keyword_name, ora_vis_keyword)
 })
