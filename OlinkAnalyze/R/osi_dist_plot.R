0761ad9bd77ede6018be07bf480beeb8a04a39f4
Kathy Nevola <72228735+kathy-nevola@users.noreply.github.com>
2026-03-11 12:11:55 -0400

    osi plot update based on example code

--- OlinkAnalyze/R/osi_dist_plot.R
+++ OlinkAnalyze/R/osi_dist_plot.R
@@ -3,47 +3,102 @@
 #'
 #' @param df data frame with OSI data present
 #' @param check_log check log from check NPX
-#' @param osi_score OSI column to graph, one of OSISummary, 
+#' @param osi_score OSI column to graph, one of OSISummary,
 #' OSITimeToCentrifugation, or OSIPreparationTemperature
 #' @return distribution plot (histogram overlayed with density plot) of
 #' osi values for corresponding osi_score column
 #' @export
 #'
-#' @examples 
-#' npx_data1$OSISummary <- as.numeric(Site)
-#' 
+#' @examples
+#' # Creating fake OSI data from Site data
+#' df1 <-npx_data1 |>
+#'   dplyr::mutate(OSISummary = as.numeric(as.factor(Site))) |>
+#'   dplyr::mutate(OSISummary = OSISummary - min(OSISummary, na.rm = TRUE)) |>
+#'   dplyr::mutate(OSISummary = OSISummary/max(OSISummary, na.rm = TRUE))
+#'
+#' check_log <- check_npx(df1)
+#' # Generate figure
+#' olink_osi_dist_plot(df1,
+#'                     check_log = check_log,
+#'                     osi_score = "OSISummary")
 olink_osi_dist_plot <- function(df,
                                 check_log = NULL,
-                                osi_score = NULL){
+                                osi_score = NULL) {
+  check_osi(df, check_log, osi_score)
 
   check_log <- run_check_npx(df, check_log = check_log)
-  
-  if(is.null(osi_score) || !(osi_score %in% c("OSISummary",
-                        "OSITimeToCentrifugation",
-                        "OSIPreparationTemperature"))){
+
+  # Filter for distinct sampleID and osi_score
+  df1 <- df |>
+    dplyr::filter(!is.na(.data[[osi_score]])) |>
+    dplyr::select(dplyr::any_of(c(column_name_dict$col_names$sample_id,
+                                  osi_score))) |>
+    dplyr::distinct()
+
+  # Check for duplicate sampleIDS
+  if (any(duplicated(df1[["SampleID"]]))) {
+    cli::cli_abort("Multiple OSI values detected for same Sample ID.")
+  }
+
+  xlab <- dplyr::recode_values(osi_score,
+                               "OSITimeToCentrifugation" ~
+                                 "Time to Centrifugation",
+                               "OSIPreparationTemperature" ~
+                                 "Preparation Temperature",
+                               "OSISummary" ~ "OSI Summary",
+                               default = NA_character_)
+
+  p <- ggplot2::ggplot(df1, ggplot2::aes(x = .data[[osi_score]])) +
+    ggplot2::geom_histogram(ggplot2::aes(y = ggplot2::after_stat(density)),
+                            bins = 30,
+                            fill = "skyblue",
+                            color = "grey80",
+                            alpha = 0.5) +
+    ggplot2::geom_density(color = "grey40",
+                          linewidth = 1L) +
+    ggplot2::scale_x_continuous(
+      breaks = seq(from = 0L, to = 1L, by = 0.25),
+      expand = c(0L, 0L)
+    ) +
+    ggplot2::scale_y_continuous(
+      expand = c(0L, 0L)
+    ) +
+    ggplot2::labs(
+      x = xlab,
+      y = "Density"
+    ) +
+    OlinkAnalyze::olink_fill_discrete() +
+    ggplot2::theme(
+      axis.title = ggplot2::element_text(size = 15L),
+      axis.text = ggplot2::element_text(size = 13L),
+      strip.text = ggplot2::element_text(size = 15L),
+      strip.background = ggplot2::element_blank()
+    )
+
+  return(p)
+}
+
+check_osi <- function(df,
+                      check_log,
+                      osi_score) {
+
+  if (is.null(osi_score) || !(osi_score %in% c("OSISummary",
+                                               "OSITimeToCentrifugation",
+                                               "OSIPreparationTemperature"))) {
     cli::cli_abort(paste0("`osi_score` must be one of OSISummary,",
                           " OSITimeToCentrifugation, ",
                           "or OSIPreparationTemperature."))
   }
-  
-  if (all(is.na(df[[osi_score]]))){
+
+  if (all(is.na(df[[osi_score]]))) {
     cli::cli_abort(paste0(osi_score, " are all NA. ",
                           "Please check your data to confirm ",
                           "OSI data is present."))
   }
-  if(any(is.na(df[[osi_score]]))){
+  if (any(is.na(df[[osi_score]]))) {
     cli::cli_warn(paste("NA data detected in",
                         osi_score,
                         "Filtering out NA data."))
   }
-  df1 <- df |> 
-    dplyr::filter(!is.na(.data[[osi_score]])) |> 
-    dplyr::select(dplyr::any_of(c("SampleID", osi_score))) |> 
-    dplyr::distinct()
-  
-  p <- ggplot2::ggplot(df1, ggplot2::aes(x = .data[[osi_score]])) +
-    ggplot2::geom_histogram(ggplot2::aes(y = ggplot2::after_stat(density)))+
-    ggplot2::geom_density()
-  
-  return(p)
+  return(NULL)
 }
--- OlinkAnalyze/tests/testthat/test-osi_dist_plot.R
+++ OlinkAnalyze/tests/testthat/test-osi_dist_plot.R
@@ -10,30 +10,37 @@ test_that("osi_distibution plots works - errors", {
   expect_error(olink_osi_dist_plot(df = data1,
                                    check_log = check_log_1,
                                    osi_score = "not a real score"),
-                regexp = "`osi_score` must be one of OSISummary,")
-  
+               regexp = "`osi_score` must be one of OSISummary,")
+
   expect_error(olink_osi_dist_plot(df = data1,
                                    check_log = check_log_1,
                                    osi_score = "OSICategory"),
                regexp = "`osi_score` must be one of OSISummary,")
   data1$OSIPreparationTemperature <- NA
   expect_error(olink_osi_dist_plot(df = data1,
-                      check_log = check_log_1,
-                      osi_score = "OSIPreparationTemperature"),
+                                   check_log = check_log_1,
+                                   osi_score = "OSIPreparationTemperature"),
                regexp = paste0("OSIPreparationTemperature are all NA. ",
                                "Please check your data to confirm OSI "))
-  
+
+  data1$OSIPreparationTemperature <- 0.5
+  data1$OSIPreparationTemperature[1] <- 0.25
+  expect_error(olink_osi_dist_plot(df = data1,
+                                   check_log = check_log_1,
+                                   osi_score = "OSIPreparationTemperature"),
+               regexp = "Multiple OSI values detected for same Sample ID.")
+
   expect_warning(olink_osi_dist_plot(df = data1,
                                      check_log = check_log_1,
                                      osi_score = "OSITimeToCentrifugation"),
                  regexp = "NA data detected in")
 })
 
-test_that("olink_osi_dist_plot - works",{
-  data1 <- data1 |> 
+test_that("olink_osi_dist_plot - works", {
+  data1 <- data1 |>
     dplyr::filter(!is.na(OSISummary))
   vdiffr::expect_doppelganger("OSISummary Plot",
                               olink_osi_dist_plot(df = data1,
-                                      check_log = check_log_1,
-                                      osi_score = "OSISummary"))
+                                                  check_log = check_log_1,
+                                                  osi_score = "OSISummary"))
 })
