f6e5942ce4a836ca205418388f35d58913347a53
kristianHoden <kristian.hoden@olink.com>
2026-03-17 18:54:09 +0500

    some more lintr:ing

--- OlinkAnalyze/R/olink_bridgeselector.R
+++ OlinkAnalyze/R/olink_bridgeselector.R
@@ -18,7 +18,7 @@
 #'
 #' \itemize{
 #'   \item{SampleID:} Sample ID
-#'   \item{PercAssaysBelowLOD:} Percent of Assays that are below LOD for
+#'   \item{perc_assays_below_lod:} Percent of Assays that are below LOD for
 #'   the sample
 #'   \item{MeanNPX:} Mean NPX for the sample
 #' }
@@ -32,7 +32,6 @@
 #' @importFrom stringr str_detect
 
 olink_bridgeselector <- function(df, sample_missing_freq, n) {
-  # Exclude OlinkIDs with missing NPX
   # Check NPX data
   check_log <- OlinkAnalyze::check_npx(df = df)
   # Clean NPX data
@@ -42,49 +41,53 @@ olink_bridgeselector <- function(df, sample_missing_freq, n) {
   )
   # Re-check NPX data
   check_log_clean <- OlinkAnalyze::check_npx(df = df_clean)
-  #Filtering on valid OlinkID
+  # Exclude OlinkIDs with missing NPX
+  # Filtering on valid OlinkID
   df <- df |>
-    dplyr::filter(!(OlinkID %in% check_log_clean$assay_na)) |>
-    dplyr::filter(stringr::str_detect(OlinkID,
+    dplyr::filter(!(.data$OlinkID %in% check_log_clean$assay_na)) |>
+    dplyr::filter(stringr::str_detect(.data$OlinkID,
                                       "OID[0-9]{5}"))
   #Filtering out control samples
   df <- df |>
-    dplyr::filter(!stringr::str_detect(SampleID, "CONTROL_SAMPLE*"))
+    dplyr::filter(!stringr::str_detect(.data$SampleID, "CONTROL_SAMPLE*"))
   #Outlier calculation as in qc_plot for filtering
   qc_outliers <- df |>
-    dplyr::group_by(Panel, SampleID) |>
-    dplyr::mutate(IQR = IQR(NPX, na.rm = TRUE),
-                  sample_median = median(NPX, na.rm = TRUE)) |>
+    dplyr::group_by(.data$Panel, .data$SampleID) |>
+    dplyr::mutate(IQR = IQR(.data$NPX, na.rm = TRUE),
+                  sample_median = median(.data$NPX, na.rm = TRUE)) |>
     dplyr::ungroup() |>
-    dplyr::select(SampleID, Panel, IQR, sample_median) |>
+    dplyr::select(.data$SampleID, .data$Panel, .data$IQR,
+                  .data$sample_median) |>
     dplyr::distinct() |>
-    dplyr::group_by(Panel) |>
-    dplyr::mutate(median_low = mean(sample_median, na.rm = TRUE) - 3 *
-                    sd(sample_median, na.rm = TRUE),
-                  median_high = mean(sample_median, na.rm = TRUE) + 3 *
-                    sd(sample_median, na.rm = TRUE),
-                  iqr_low = mean(IQR, na.rm = TRUE) - 3 *
-                    sd(IQR, na.rm = TRUE),
-                  iqr_high = mean(IQR, na.rm = TRUE) + 3 *
-                    sd(IQR, na.rm = TRUE)) |>
+    dplyr::group_by(.data$Panel) |>
+    dplyr::mutate(median_low = mean(.data$sample_median, na.rm = TRUE) - 3 *
+                    sd(.data$sample_median, na.rm = TRUE),
+                  median_high = mean(.data$sample_median, na.rm = TRUE) + 3 *
+                    sd(.data$sample_median, na.rm = TRUE),
+                  iqr_low = mean(.data$IQR, na.rm = TRUE) - 3 *
+                    sd(.data$IQR, na.rm = TRUE),
+                  iqr_high = mean(.data$IQR, na.rm = TRUE) + 3 *
+                    sd(.data$IQR, na.rm = TRUE)) |>
     dplyr::ungroup() |>
-    dplyr::mutate(Outlier = dplyr::if_else(sample_median < median_high &
-                                             sample_median > median_low &
-                                             IQR > iqr_low &
-                                             IQR < iqr_high,
+    dplyr::mutate(Outlier = dplyr::if_else(.data$sample_median <
+                                             .data$median_high &
+                                             .data$sample_median >
+                                             .data$median_low &
+                                             .data$IQR > .data$iqr_low &
+                                             .data$IQR < .data$iqr_high,
                                            0, 1)) |>
-    dplyr::select(SampleID, Panel, Outlier)
+    dplyr::select(.data$SampleID, .data$Panel, .data$Outlier)
   # Alternative LODs for when LOD is not present
   alt_plate_lods <- c("Plate LOD", "PlateLOD", "plateLOD", "Plate_LOD")
   alt_max_lods <- c("Max LOD", "MaxLOD", "maxLOD", "Max_LOD")
   if (!("LOD" %in% names(df))) {
     if (any(alt_plate_lods %in% names(df))) {
       df <- df |>
-        dplyr::rename(LOD = any_of(alt_plate_lods))
+        dplyr::rename(LOD = dplyr::any_of(alt_plate_lods))
       message("Using plate LOD as filter criteria...")
     } else if (any(alt_max_lods %in% names(df))) {
       df <- df |>
-        dplyr::rename(LOD = any_of(alt_max_lods))
+        dplyr::rename(LOD = dplyr::any_of(alt_max_lods))
       message("Using max LOD as filter criteria...")
     } else {
       df <- df |>
@@ -94,24 +97,27 @@ olink_bridgeselector <- function(df, sample_missing_freq, n) {
   }
   if ("SampleQC" %in% names(df)) {
     df <- df |>
-      dplyr::mutate(QC_Warning = SampleQC)
+      dplyr::mutate(QC_Warning = .data$SampleQC)
   }
   df_1 <- df |>
     dplyr::left_join(qc_outliers, by = c("SampleID", "Panel")) |>
-    dplyr::mutate(NPX = ifelse(NPX <= LOD, NA, NPX)) |>
-    dplyr::group_by(SampleID) |>
-    dplyr::mutate(QC_Warning = dplyr::if_else(all(toupper(QC_Warning) ==
-                                                    "PASS"), "PASS",
-                                              "WARNING")) |>
-    dplyr::filter(QC_Warning == "PASS") |>
-    dplyr::mutate(Outliers = sum(Outlier)) |>
-    dplyr::filter(Outliers == 0) |>
-    dplyr::mutate(PercAssaysBelowLOD = sum(is.na(NPX)) / dplyr::n()) |>
-    dplyr::mutate(MeanNPX = mean(NPX, na.rm = TRUE)) |>
+    dplyr::mutate(NPX = ifelse(.data$NPX <= .data$LOD, NA, .data$NPX)) |>
+    dplyr::group_by(.data$SampleID) |>
+    dplyr::mutate(.data$QC_Warning <-
+                    dplyr::if_else(all(toupper(.data$QC_Warning) ==
+                                         "PASS"), "PASS",
+                                   "WARNING")) |>
+    dplyr::filter(.data$QC_Warning == "PASS") |>
+    dplyr::mutate(.data$Outliers <-
+                    sum(.data$Outlier)) |>
+    dplyr::filter(.data$Outliers == 0) |>
+    dplyr::mutate(.data$perc_assays_below_lod <-
+                    sum(is.na(.data$NPX)) / dplyr::n()) |>
+    dplyr::mutate(MeanNPX = mean(.data$NPX, na.rm = TRUE)) |>
     dplyr::ungroup() |>
-    dplyr::filter(PercAssaysBelowLOD < sample_missing_freq)
+    dplyr::filter(.data$perc_assays_below_lod < .data$sample_missing_freq)
   df_2 <- df_1 |>
-    dplyr::select(SampleID, PercAssaysBelowLOD, MeanNPX) |>
+    dplyr::select(.data$SampleID, .data$perc_assays_below_lod, .data$MeanNPX) |>
     dplyr::distinct()
   if (nrow(df_2) < n) {
     stop(paste0("With the current settings only ",
@@ -124,13 +130,13 @@ olink_bridgeselector <- function(df, sample_missing_freq, n) {
     selected_bridges <- df_2
   } else { #
     df_2  <- df_2  |>
-      dplyr::arrange(desc(MeanNPX)) |>
-      dplyr::mutate(order = c(seq_len(nrow(.))))
+      dplyr::arrange(dplyr::desc(.data$MeanNPX)) |>
+      dplyr::mutate(order = c(seq_len(nrow(.data))))
     bridge_samples <- floor(seq(1, nrow(df_2), length.out = n + 2)
                             [c(-1, -(n + 2))])
     selected_bridges <- df_2 |>
       dplyr::filter(order %in% bridge_samples) |>
-      dplyr::slice_sample(n = nrow(.)) |>
+      dplyr::slice_sample(n = nrow(.data)) |>
       dplyr::select(-order)
   }
   return(selected_bridges)
