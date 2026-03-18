a1a6ca88a4e1f76df6edc8ba115ec657b35ba1d8
kristianHoden <kristian.hoden@olink.com>
2026-03-18 15:10:19 +0100

    linting

--- OlinkAnalyze/tests/testthat/test-olink_bridgeselector.R
+++ OlinkAnalyze/tests/testthat/test-olink_bridgeselector.R
@@ -6,15 +6,17 @@ testthat::test_that("olink_bridgeselector works", {
                                         n = 8)
   testthat::expect_equal(nrow(bridgeSamples), 8)
   testthat::expect_equal(ncol(bridgeSamples), 3)
-  testthat::expect_equal(bridgeSamples[order(bridgeSamples$MeanNPX, decreasing = TRUE), ]
-               $SampleID[3], "A70")
+  testthat::expect_equal(bridgeSamples[order(bridgeSamples$MeanNPX,
+                                             decreasing = TRUE), ]$SampleID[3],
+                         "A70")
   testthat::expect_equal(round(bridgeSamples[order(bridgeSamples$MeanNPX,
-                                         decreasing = TRUE), ]$MeanNPX[5],
-                     digits = 2), 6.21)
+                                                   decreasing = TRUE),
+                               ]$MeanNPX[5],
+                               digits = 2), 6.21)
   testthat::expect_error(olink_bridgeselector(df = npx_data1,
-                                    n = 8))
+                                              n = 8))
   testthat::expect_error(olink_bridgeselector(npx_data_format221010,
-                                      sampleMissingFreq = 0.1, n = 2))
+                                              sampleMissingFreq = 0.1, n = 2))
   ref_data <- npx_data1 |> olink_bridgeselector(sampleMissingFreq = 0.1, n = 8)
   ml_data <- npx_data1 |>
     dplyr::mutate(`Max LOD` = LOD) |>
@@ -37,21 +39,23 @@ testthat::test_that("olink_bridgeselector works", {
     dplyr::select(-QC_Warning) |>
     olink_bridgeselector(sampleMissingFreq = 0.1, n = 8)
   testthat::expect_length(c(setdiff(ref_data$SampleID, ml_data$SampleID),
-                  setdiff(ml_data$SampleID, ref_data$SampleID)), 0)
+                            setdiff(ml_data$SampleID, ref_data$SampleID)), 0)
   testthat::expect_length(c(setdiff(ref_data$SampleID, pl_data$SampleID),
-                  setdiff(pl_data$SampleID, ref_data$SampleID)), 0)
+                            setdiff(pl_data$SampleID, ref_data$SampleID)), 0)
   testthat::expect_length(c(setdiff(ref_data$SampleID, p_l_data$SampleID),
-                  setdiff(p_l_data$SampleID, ref_data$SampleID)), 0)
+                            setdiff(p_l_data$SampleID, ref_data$SampleID)), 0)
   testthat::expect_length(c(setdiff(ref_data$SampleID, sampleqc_data$SampleID),
-                  setdiff(sampleqc_data$SampleID, ref_data$SampleID)), 0)
+                            setdiff(sampleqc_data$SampleID, ref_data$SampleID)),
+                          0)
   testthat::expect_length(c(setdiff(ref_data$SampleID, lodnpx_data$SampleID),
-                  setdiff(lodnpx_data$SampleID, ref_data$SampleID)), 0)
+                            setdiff(lodnpx_data$SampleID, ref_data$SampleID)),
+                          0)
 })
 
 testthat::test_that("olink_bridgeselector max num of samples works", {
   testthat::expect_equal({
-      olink_bridgeselector(df = npx_data1,
-                           sampleMissingFreq = 0.1,
-                           n = 150) |>
-        nrow()}, 150)
+                          olink_bridgeselector(df = npx_data1,
+                                               sampleMissingFreq = 0.1,
+                                               n = 150) |>
+                            nrow()}, 150)
 })
