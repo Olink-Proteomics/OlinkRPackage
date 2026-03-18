12fb7b2424ebeb563aac25055be380dbe4a15131
klevdiamanti <kdiamanti@outlook.com>
2026-03-18 11:55:10 +0100

    Improve tests for enrichment heatmap

--- OlinkAnalyze/tests/testthat/test-olink_pathway_heatmap.R
+++ OlinkAnalyze/tests/testthat/test-olink_pathway_heatmap.R
@@ -1,58 +1,121 @@
-skip_on_cran()
-skip_if_not_installed("clusterProfiler")
-skip_if_not_installed("ggplot2", minimum_version = "3.4.0")
-skip_if_not_installed("msigdbr", minimum_version = "24.1.0")
-skip_if_not_installed("broom")
-skip_if_not_installed("car")
+test_that(
+  "olink_pathway_visualization - valid Keyword needed",
+  {
+    skip_on_cran()
+    skip_if_not_installed("clusterProfiler")
+    skip_if_not_installed("msigdbr", minimum_version = "24.1.0")
+    skip_if_not_installed("vdiffr")
 
-set.seed(123)
-npx_df <- npx_data1 |>
-  dplyr::filter(!grepl("control",
-                       .data[["SampleID"]],
-                       ignore.case = TRUE))
-check_log <- check_npx(npx_df)
-ttest_results <- olink_ttest(df = npx_df,
-                             check_log = check_log,
-                             variable = "Treatment",
-                             alternative = "two.sided")
+    # data ----
 
-gsea_results <- olink_pathway_enrichment(df = npx_df,
-                                         check_log = check_log,
-                                         test_results = ttest_results)
+    npx_df <- npx_data1 |>
+      dplyr::filter(
+        !grepl(pattern = "control",
+               x = .data[["SampleID"]],
+               ignore.case = TRUE)
+      )
 
-ora_results <- olink_pathway_enrichment(df = npx_df,
-                                        check_log = check_log,
-                                        test_results = ttest_results,
-                                        method = "ORA")
+    check_log <- check_npx(npx_df)
 
+    # statistics ----
 
-test_that("Valid Keyword needed", {
-  set.seed(123)
-  expect_error(olink_pathway_heatmap(enrich_results = gsea_results,
-                                     test_results = ttest_results,
-                                     keyword = "asfhdlk"))
-})
+    ttest_results <- olink_ttest(
+      df = npx_df,
+      check_log = check_log,
+      variable = "Treatment",
+      alternative = "two.sided"
+    ) |>
+      suppressMessages()
 
-test_that("Plot works", {
-  gsea_heatmap <- olink_pathway_heatmap(enrich_results = gsea_results,
-                                        test_results = ttest_results)
-  ora_heatmap <- olink_pathway_heatmap(enrich_results = ora_results,
-                                       test_results = ttest_results,
-                                       method = "ORA",
-                                       keyword = "cell")
+    # pathway enrichment ----
 
-  skip_on_cran()
-  skip_if_not_installed("vdiffr")
+    set.seed(123)
 
-  set.seed(123)
+    ## GSEA ----
 
-  gsea_heatmap_name <- "GSEA Heatmap"
-  check_snap_exist(test_dir_name = "olink_pathway_heatmap",
-                   snap_name = gsea_heatmap_name)
-  vdiffr::expect_doppelganger(gsea_heatmap_name, gsea_heatmap)
+    expect_no_error(
+      object = gsea_results <- olink_pathway_enrichment(
+        df = npx_df,
+        check_log = check_log,
+        test_results = ttest_results
+      ) |>
+        suppressMessages() |>
+        suppressWarnings()
+    )
 
-  ora_heatmap_name <- "ORA Heatmap with Keyword"
-  check_snap_exist(test_dir_name = "olink_pathway_heatmap",
-                   snap_name = ora_heatmap_name)
-  vdiffr::expect_doppelganger(ora_heatmap_name, ora_heatmap)
-})
+    ## ORA ----
+
+    expect_no_error(
+      object = ora_results <- olink_pathway_enrichment(
+        df = npx_df,
+        check_log = check_log,
+        test_results = ttest_results,
+        method = "ORA"
+      ) |>
+        suppressMessages() |>
+        suppressWarnings()
+    )
+
+    # Errors ----
+
+    expect_error(
+      object = olink_pathway_heatmap(),
+      regexp = "Arguments `enrich_results` and `test_results` are required!"
+    )
+
+    expect_error(
+      object = olink_pathway_heatmap(
+        enrich_results = gsea_results
+      ),
+      regexp = "Arguments `enrich_results` and `test_results` are required!"
+    )
+
+    expect_error(
+      object = olink_pathway_heatmap(
+        enrich_results = gsea_results,
+        test_results = ttest_results,
+        keyword = "hfdklahfajikshf"
+      ),
+      regexp = paste("Filtering `enrich_results` for `enrich_results` =",
+                     "\"hfdklahfajikshf\" did not return any results.")
+    )
+
+    expect_error(
+      object = olink_pathway_heatmap(
+        enrich_results = ora_results,
+        test_results = ttest_results,
+        method = "ORA",
+        keyword = "hfdklahfajikshf"
+      ),
+      regexp = paste("Filtering `enrich_results` for `enrich_results` =",
+                     "\"hfdklahfajikshf\" did not return any results.")
+    )
+
+    # Snapshots ----
+
+    ## v1 ----
+
+    gsea_heatmap <- olink_pathway_heatmap(
+      enrich_results = gsea_results,
+      test_results = ttest_results
+    )
+    gsea_heatmap_name <- "GSEA Heatmap"
+    check_snap_exist(test_dir_name = "olink_pathway_heatmap",
+                     snap_name = gsea_heatmap_name)
+    vdiffr::expect_doppelganger(gsea_heatmap_name, gsea_heatmap)
+
+    ## v2 ----
+
+    ora_heatmap <- olink_pathway_heatmap(
+      enrich_results = ora_results,
+      test_results = ttest_results,
+      method = "ORA",
+      keyword = "cell"
+    )
+    ora_heatmap_name <- "ORA Heatmap with Keyword"
+    check_snap_exist(test_dir_name = "olink_pathway_heatmap",
+                     snap_name = ora_heatmap_name)
+    vdiffr::expect_doppelganger(ora_heatmap_name, ora_heatmap)
+
+  }
+)
