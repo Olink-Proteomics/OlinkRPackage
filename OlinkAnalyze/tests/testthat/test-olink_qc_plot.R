367c575868993bafae59879193308c69b4e2a947
klevdiamanti <kdiamanti@outlook.com>
2026-03-21 21:10:28 +0100

    osi tests

--- OlinkAnalyze/tests/testthat/test-olink_qc_plot.R
+++ OlinkAnalyze/tests/testthat/test-olink_qc_plot.R
@@ -1,137 +1,167 @@
-test_that("OSI errors: using npx_data1 and OSI columns", {
+test_that(
+  "olink_qc_plot - works - OSI",
+  {
+    skip_if_not_installed(pkg = "ggrepel")
 
-  testthat::skip_if_not_installed(pkg = "ggrepel")
+    osi_data <- get_example_data("example_osi_data.rds")
 
-  # ----------------------------
-  # OSICategory invalid value
-  # ----------------------------
-  df_bad_cat <- data1 |>
-    dplyr::mutate(OSICategory = ifelse(dplyr::row_number() == 1, "7",
-                                       as.character(OSICategory)))
+    # ----------------------------
+    # OSICategory invalid value
+    # ----------------------------
+    df_bad_cat <- osi_data |>
+      dplyr::mutate(
+        OSICategory = dplyr::if_else(
+          dplyr::row_number() == 1L, "7", as.character(.data[["OSICategory"]])
+        )
+      )
+    df_bad_cat_check <- check_npx(df = df_bad_cat) |>
+      suppressMessages() |>
+      suppressWarnings()
 
-  # No error when not using OSI column
-  testthat::expect_no_error(
-    testthat::expect_message(
-      olink_qc_plot(df_bad_cat, color_g = "QC_Warning"),
-      regexp = "`check_log` not provided. Running `check_npx()`.",
-      fixed = TRUE
-    )
-  )
-
-  testthat::expect_error(
-    testthat::expect_message(olink_qc_plot(df_bad_cat, color_g = "OSICategory"),
-      regexp = paste0("`check_log` not provided. ",
-                      "Running `check_npx()`."),
-      fixed = TRUE
-    ),
-    regexp = paste0('Invalid values detected in OSICategory\\.', #nolint quotes_linter
-                    ' Expected only 0, 1, 2, 3, or 4\\. Found: "7".') #nolint quotes_linter
-  )
-
-  # ----------------------------
-  # OSICategory all NA
-  # ----------------------------
-  df_cat_all_na <- data1 |>
-    dplyr::mutate(OSICategory = NA)
-
-  testthat::expect_error(
-    testthat::expect_message(olink_qc_plot(df_cat_all_na,
-                                           color_g = "OSICategory"),
-                             regexp = paste0("`check_log` not provided. ",
-                                             "Running `check_npx()`."),
-                             fixed = TRUE),
-    regexp = paste0("All values are NA in OSICategory\\. ",
-                    "Please check your data to confirm OSI data is present\\.")
-  )
-
-  # ------------------------------------------
-  # Continuous OSI column non-numeric value
-  # ------------------------------------------
-  df_bad_cont_nonnum <- data1 |>
-    dplyr::mutate(OSITimeToCentrifugation = ifelse(
-      dplyr::row_number() == 1,
-      "oops",
-      as.character(OSITimeToCentrifugation)
-    )
+    # No error when not using OSI column
+    expect_no_error(
+      object = expect_no_message(
+        object = expect_no_warning(
+          object = olink_qc_plot(
+            df = df_bad_cat,
+            color_g = "QC_Warning",
+            check_log = df_bad_cat_check
+          )
+        )
+      )
     )
 
-  testthat::expect_error(
-    testthat::expect_message(olink_qc_plot(df_bad_cont_nonnum, color_g = "OSITimeToCentrifugation"), #nolint line_length_linter
-                             regexp = paste0("`check_log` not provided. ",
-                                             "Running `check_npx()`."),
-                             fixed = TRUE),
-    regexp = paste0(
-      'Invalid values detected in OSITimeToCentrifugation\\.', #nolint quotes_linter
-      ' Expected continuous numeric values between 0 and 1\\.', #nolint quotes_linter
-      ' Found non-numeric value\\(s\\): "oops".' #nolint quotes_linter
+    expect_error(
+      object = expect_no_message(
+        object = expect_no_warning(
+          object = olink_qc_plot(
+            df = df_bad_cat,
+            color_g = "OSICategory",
+            check_log = df_bad_cat_check
+          )
+        )
+      ),
+      regexp = "Invalid values detected in column \"OSICategory\" of `df`!"
     )
-  )
 
-  # ------------------------------------
-  # Continuous OSI column out of range
-  # ------------------------------------
-  df_bad_cont_oor <- data1 |>
-    dplyr::mutate(OSIPreparationTemperature = ifelse(dplyr::row_number() == 1, 1.2, OSIPreparationTemperature)) #nolint line_length_linter
+    # ----------------------------
+    # OSICategory all NA
+    # ----------------------------
+    df_cat_all_na <- osi_data |>
+      dplyr::mutate(OSICategory = NA)
 
-  testthat::expect_error(
-    testthat::expect_message(olink_qc_plot(df_bad_cont_oor, color_g = "OSIPreparationTemperature"), #nolint line_length_linter
-                             regexp = paste0("`check_log` not provided. ",
-                                             "Running `check_npx()`."),
-                             fixed = TRUE),
-    regexp = paste0(
-      "Invalid values detected in OSIPreparationTemperature\\.",
-      " Expected continuous numeric values between 0 and 1\\.",
-      " Found out-of-range value\\(s\\): 1\\.2"
+    expect_error(
+      object = olink_qc_plot(
+        df = df_cat_all_na,
+        color_g = "OSICategory",
+        check_log = check_npx(df = df_cat_all_na) |>
+          suppressMessages() |>
+          suppressWarnings()
+      ),
+      regexp = paste("All values are 'NA' in the column \"OSICategory\" of the",
+                     "dataset `df`!")
     )
-  )
 
-  # ----------------------------
-  # Continuous OSI column all NA
-  # ----------------------------
-  df_cont_all_na <- data1 |>
-    dplyr::mutate(OSISummary = NA)
+    # ------------------------------------------
+    # Continuous OSI column non-numeric value
+    # ------------------------------------------
+    df_bad_cont_nonnum <- osi_data |>
+      dplyr::mutate(
+        OSITimeToCentrifugation = dplyr::if_else(
+          dplyr::row_number() == 1L,
+          "oops",
+          as.character(.data[["OSITimeToCentrifugation"]])
+        )
+      )
 
-  testthat::expect_error(
-    testthat::expect_message(olink_qc_plot(df_cont_all_na, color_g = "OSISummary"), #nolint line_length_linter
-                             regexp = paste0("`check_log` not provided. ",
-                                             "Running `check_npx()`."),
-                             fixed = TRUE),
-    regexp = paste0("All values are NA in OSISummary\\.",
-                    " Please check your data to confirm OSI data is present\\.")
-  )
+    expect_error(
+      object = olink_qc_plot(
+        df = df_bad_cont_nonnum,
+        color_g = "OSITimeToCentrifugation",
+        check_log = check_npx(df = df_bad_cont_nonnum) |>
+          suppressMessages() |>
+          suppressWarnings()
+      ),
+      regexp = paste("Non-numeric values detected in column",
+                     "\"OSITimeToCentrifugation\" of `df`!")
+    )
 
-  # ------------------------------------------------------
-  # Valid OSI values should NOT trigger OSI error strings
-  # ------------------------------------------------------
-  testthat::expect_no_error(
-    testthat::expect_message(olink_qc_plot(data1, color_g = "OSICategory"),
-                             regexp = paste0("`check_log` not provided. ",
-                                             "Running `check_npx()`."),
-                             fixed = TRUE)
-  )
+    # ----------------------------
+    # Continuous OSI column all NA
+    # ----------------------------
+    df_cont_all_na <- osi_data |>
+      dplyr::mutate(OSISummary = NA)
 
-  testthat::expect_no_error(
-    testthat::expect_message(olink_qc_plot(data1, color_g = "OSISummary"),
-                             regexp = paste0("`check_log` not provided. ",
-                                             "Running `check_npx()`."),
-                             fixed = TRUE)
-  )
+    expect_error(
+      object = olink_qc_plot(
+        df = df_cont_all_na,
+        color_g = "OSISummary",
+        check_log = check_npx(df = df_cont_all_na) |>
+          suppressMessages() |>
+          suppressWarnings()
+      ),
+      regexp = paste("All values are 'NA' in the column \"OSISummary\" of the",
+                     "dataset `df`!")
+    )
 
-  testthat::expect_no_error(
-    testthat::expect_message(olink_qc_plot(data1, color_g = "OSIPreparationTemperature"), #nolint line_length_linter
-                             regexp = paste0("`check_log` not provided. ",
-                                             "Running `check_npx()`."),
-                             fixed = TRUE)
-  )
+    # ------------------------------------------------------
+    # Valid OSI values should NOT trigger OSI error strings
+    # ------------------------------------------------------
 
-  testthat::expect_no_error(
-    testthat::expect_message(olink_qc_plot(data1, color_g = "OSITimeToCentrifugation"), #nolint line_length_linter
-                             regexp = paste0("`check_log` not provided. ",
-                                             "Running `check_npx()`."),
-                             fixed = TRUE)
-  )
+    check_log <- check_npx(df = osi_data) |>
+      suppressMessages() |>
+      suppressWarnings()
 
-})
+    expect_no_error(
+      object = expect_no_warning(
+        object = expect_no_message(
+          object = olink_qc_plot(
+            df = osi_data,
+            color_g = "OSICategory",
+            check_log = check_log
+          )
+        )
+      )
+    )
+
+    expect_no_error(
+      object = expect_no_warning(
+        object = expect_no_message(
+          object = olink_qc_plot(
+            df = osi_data,
+            color_g = "OSISummary",
+            check_log = check_log
+          )
+        )
+      )
+    )
+
+    expect_no_error(
+      object = expect_no_warning(
+        object = expect_no_message(
+          object = olink_qc_plot(
+            df = osi_data,
+            color_g = "OSIPreparationTemperature",
+            check_log = check_log
+          )
+        )
+      )
+    )
+
+    expect_no_error(
+      object = expect_no_warning(
+        object = expect_no_message(
+          object = olink_qc_plot(
+            df = osi_data,
+            color_g = "OSITimeToCentrifugation",
+            check_log = check_log
+          )
+        )
+      )
+    )
+
+  }
+)
 
 test_that(
   "olink_qc_plot - works - strange datasets",
