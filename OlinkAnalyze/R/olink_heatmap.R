1e3f58578cd4128c7231d7235a89090b66f8c3f4
klevdiamanti <kdiamanti@outlook.com>
2026-03-23 15:11:12 +0100

    Update

--- OlinkAnalyze/R/olink_heatmap.R
+++ OlinkAnalyze/R/olink_heatmap.R
@@ -1,8 +1,10 @@
 #' Function to plot a heatmap of the NPX data
 #'
+#' @description
 #' Generates a heatmap using \code{pheatmap::pheatmap} of all samples from NPX
 #' data.
 #'
+#' @details
 #' The values are by default scaled across and centered in the heatmap. Columns
 #' and rows are by default sorted by by dendrogram.
 #' Unique sample names are required.
@@ -31,32 +33,50 @@
 #' @param fontsize Fontsize (default 10)
 #' @param na_col Color of cells with \code{NA} (default black)
 #' @param ... Additional arguments used in \code{pheatmap::pheatmap}
+#'
 #' @return An object of class \code{ggplot}, generated from the \code{gtable}
 #' returned by \code{pheatmap::pheatmap}.
+#'
 #' @keywords NPX Heatmap
+#'
 #' @export
+#'
 #' @examples
 #' \donttest{
-#' npx_data <- npx_data1 |>
-#'   dplyr::filter(!stringr::str_detect(SampleID,"CONT"))
-#'   check_log <- check_npx(npx_data)
-#' try({ # This will fail if ggplotify is not installed
-#'   #Heatmap
-#'   olink_heatmap_plot(df = npx_data,
-#'                      check_log = check_log)
-#'   #Heatmap with annotation
-#'   olink_heatmap_plot(df = npx_data,
-#'                      check_log = check_log,
-#'                      variable_row_list = c("Time", "Site"))
-#'   #Heatmap with calls from pheatmap
-#'   olink_heatmap_plot(df = npx_data,
-#'                      check_log = check_log,
-#'                      cutree_rows = 3)
-#' })
+#' if (rlang::is_installed(pkg = c("ggplotify", "pheatmap"))) {
+#'   npx_data <- npx_data1 |>
+#'     dplyr::filter(
+#'       !stringr::str_detect(
+#'         string = .data[["SampleID"]],
+#'         pattern = "CONT"
+#'       )
+#'     )
+#'   check_log <- OlinkAnalyze::check_npx(
+#'     df = npx_data
+#'   )
 #'
+#'   # Heatmap
+#'   OlinkAnalyze::olink_heatmap_plot(
+#'     df = npx_data,
+#'     check_log = check_log
+#'   )
+#'
+#'   # Heatmap with annotation
+#'   OlinkAnalyze::olink_heatmap_plot(
+#'     df = npx_data,
+#'     check_log = check_log,
+#'     variable_row_list = c("Time", "Site")
+#'   )
+#'
+#'   # Heatmap with calls from pheatmap
+#'   OlinkAnalyze::olink_heatmap_plot(
+#'     df = npx_data,
+#'     check_log = check_log,
+#'     cutree_rows = 3L
+#'   )
+#' }
 #' }
 #'
-
 olink_heatmap_plot <- function(df,
                                check_log = NULL,
                                variable_row_list = NULL,
@@ -72,42 +92,42 @@ olink_heatmap_plot <- function(df,
                                na_col = "black",
                                ...) {
 
-  check_heatmap_inputs(colnames, ...)
+  plot_heatmap_check_inputs(colnames = colnames,
+                            ...)
 
   check_log <- run_check_npx(df = df,
                              check_log = check_log)
 
-  df <- clean_heatmap_df(df = df,
-                         check_log = check_log,
-                         colnames = colnames)
+  df <- plot_heatmap_clean_df(df = df,
+                              check_log = check_log,
+                              colnames = colnames)
 
-  df_wide <- df_to_wide(df = df,
-                        check_log = check_log,
-                        colnames = colnames)
-
-  # remove dup uniprot
+  df_wide <- plot_heatmap_df_to_wide(df = df,
+                                     check_log = check_log,
+                                     colnames = colnames)
 
   # Remove samples with only NA
 
-  pheatmap_args <- create_pheatmap_args(df_wide = df_wide,
-                                        df = df,
-                                        check_log = check_log,
-                                        center_scale = center_scale,
-                                        cluster_rows = cluster_rows,
-                                        cluster_cols = cluster_cols,
-                                        na_col = na_col,
-                                        show_rownames = show_rownames,
-                                        show_colnames = show_colnames,
-                                        annotation_legend = annotation_legend,
-                                        fontsize = fontsize,
-                                        variable_row_list = variable_row_list,
-                                        variable_col_list = variable_col_list,
-                                        colnames = colnames,
-                                        ...)
+  pheatmap_args <- plot_heatmap_pheatmap_args(
+    df_wide = df_wide,
+    df = df,
+    check_log = check_log,
+    center_scale = center_scale,
+    cluster_rows = cluster_rows,
+    cluster_cols = cluster_cols,
+    na_col = na_col,
+    show_rownames = show_rownames,
+    show_colnames = show_colnames,
+    annotation_legend = annotation_legend,
+    fontsize = fontsize,
+    variable_row_list = variable_row_list,
+    variable_col_list = variable_col_list,
+    colnames = colnames,
+    ...
+  )
   # set row and column colors for annotations
   # color palette
-  p <- run_pheatmap(pheatmap_args = pheatmap_args)
+  p <- pheatmap_run(pheatmap_args = pheatmap_args)
 
   return(p)
-
 }
--- OlinkAnalyze/tests/testthat/test-plot_heatmap_helper.R
+++ OlinkAnalyze/tests/testthat/test-plot_heatmap_helper.R
@@ -1,5 +1,3 @@
-skip_if_not_installed("ggplotify")
-
 npx_data_format_oct <- get_example_data("npx_data_format-Oct-2022.rds")
 check_log_oct <- check_npx(df = npx_data_format_oct) |>
   suppressWarnings() |>
@@ -9,13 +7,6 @@ npx_data_format <- clean_npx(df = npx_data_format_oct,
                              verbose = FALSE) |>
   suppressWarnings() |>
   suppressMessages()
-
-df <- clean_heatmap_df(df = npx_data_format,
-                       check_log = check_log_oct,
-                       colnames = "assay")
-df_wide <- df_to_wide(df = df,
-                      check_log = check_log_oct,
-                      colnames = "assay")
 
 # Test plot_heatmap_check_inputs ----
 
@@ -213,191 +204,299 @@ test_that(
   }
 )
 
-test_that("create_pheatmap_args - works", {
+# Test plot_heatmap_pheatmap_args ----
 
-  expect_equal(create_pheatmap_args(df_wide = df_wide,
-                                    df = df,
-                                    check_log = check_log_oct,
-                                    variable_col_list = NULL,
-                                    variable_row_list = NULL,
-                                    center_scale = TRUE,
-                                    cluster_rows = TRUE,
-                                    cluster_cols = TRUE,
-                                    show_rownames = TRUE,
-                                    show_colnames = TRUE,
-                                    annotation_legend = TRUE,
-                                    colnames = "assay",
-                                    fontsize = 10,
-                                    na_col = "black"),
-               list(mat = df_wide,
-                    scale = "column",
-                    silent = TRUE,
-                    cluster_rows = TRUE,
-                    cluster_cols = TRUE,
-                    na_col = "black",
-                    show_rownames = TRUE,
-                    show_colnames = TRUE,
-                    annotation_legend = TRUE,
-                    fontsize = 10))
+test_that(
+  "plot_heatmap_pheatmap_args - works",
+  {
+    npx_data_format_clean <- plot_heatmap_clean_df(
+      df = npx_data_format,
+      check_log = check_log_oct,
+      colnames = "assay"
+    ) |>
+      suppressMessages() |>
+      suppressWarnings()
+    npx_data_format_wide <- plot_heatmap_df_to_wide(
+      df = npx_data_format_clean,
+      check_log = check_log_oct,
+      colnames = "assay"
+    )
 
-  expect_equal(create_pheatmap_args(df_wide = df_wide,
-                                    df = df,
-                                    check_log = check_log_oct,
-                                    variable_row_list = c("treatment2"),
-                                    variable_col_list = c("Assay_Warning"),
-                                    center_scale = TRUE,
-                                    cluster_rows = TRUE,
-                                    cluster_cols = TRUE,
-                                    show_rownames = TRUE,
-                                    show_colnames = TRUE,
-                                    annotation_legend = TRUE,
-                                    colnames = "assay",
-                                    fontsize = 10,
-                                    na_col = "black"),
-               list(mat = df_wide,
-                    scale = "column",
-                    silent = TRUE,
-                    cluster_rows = TRUE,
-                    cluster_cols = TRUE,
-                    na_col = "black",
-                    show_rownames = TRUE,
-                    show_colnames = TRUE,
-                    annotation_legend = TRUE,
-                    fontsize = 10,
-                    annotation_row = {
-                      df |>
-                        dplyr::select(SampleID, treatment2) |>
-                        dplyr::distinct() |>
-                        tibble::column_to_rownames("SampleID")
-                    },
-                    annotation_col = {
-                      df |>
-                        dplyr::select(assay, Assay_Warning) |>
-                        dplyr::distinct() |>
-                        tibble::column_to_rownames("assay")
-                    },
-                    annot_col_int = list(
-                      Assay_Warning = olink_pal()(5)[1:2],
-                      treatment2 = olink_pal()(5)[3:5]
-                    )))
-  expect_equal(create_pheatmap_args(df_wide = df_wide,
-                                    df = df,
-                                    check_log = check_log_oct,
-                                    variable_col_list = NULL,
-                                    variable_row_list = NULL,
-                                    center_scale = TRUE,
-                                    cluster_rows = TRUE,
-                                    cluster_cols = TRUE,
-                                    show_rownames = TRUE,
-                                    show_colnames = TRUE,
-                                    annotation_legend = TRUE,
-                                    colnames = "assay",
-                                    fontsize = 10,
-                                    na_col = "black",
-                                    cuttree_rows = 3L),
-               list(mat = df_wide,
-                    scale = "column",
-                    silent = TRUE,
-                    cluster_rows = TRUE,
-                    cluster_cols = TRUE,
-                    na_col = "black",
-                    show_rownames = TRUE,
-                    show_colnames = TRUE,
-                    annotation_legend = TRUE,
-                    fontsize = 10,
-                    cuttree_rows = 3L))
-})
+    expect_equal(
+      object = plot_heatmap_pheatmap_args(
+        df_wide = npx_data_format_wide,
+        df = npx_data_format_clean,
+        check_log = check_log_oct,
+        variable_col_list = NULL,
+        variable_row_list = NULL,
+        center_scale = TRUE,
+        cluster_rows = TRUE,
+        cluster_cols = TRUE,
+        show_rownames = TRUE,
+        show_colnames = TRUE,
+        annotation_legend = TRUE,
+        colnames = "assay",
+        fontsize = 10,
+        na_col = "black"
+      ),
+      list(
+        mat = npx_data_format_wide,
+        scale = "column",
+        silent = TRUE,
+        cluster_rows = TRUE,
+        cluster_cols = TRUE,
+        na_col = "black",
+        show_rownames = TRUE,
+        show_colnames = TRUE,
+        annotation_legend = TRUE,
+        fontsize = 10
+      )
+    )
 
-test_that("extract_ellipsis_arg - works", {
-  expect_equal(
-    extract_ellipsis_arg(list(mat = df_wide,
-                              scale = "column",
-                              silent = TRUE,
-                              cluster_rows = TRUE,
-                              cluster_cols = TRUE,
-                              na_col = "black",
-                              show_rownames = TRUE,
-                              show_colnames = TRUE,
-                              annotation_legend = TRUE,
-                              fontsize = 10),
-                         annotation_colors = "something"),
-    list(mat = df_wide,
-         scale = "column",
-         silent = TRUE,
-         cluster_rows = TRUE,
-         cluster_cols = TRUE,
-         na_col = "black",
-         show_rownames = TRUE,
-         show_colnames = TRUE,
-         annotation_legend = TRUE,
-         fontsize = 10,
-         annot_col_int = "something")
-  )
+    expect_equal(
+      object = plot_heatmap_pheatmap_args(
+        df_wide = npx_data_format_wide,
+        df = npx_data_format_clean,
+        check_log = check_log_oct,
+        variable_row_list = c("treatment2"),
+        variable_col_list = c("Assay_Warning"),
+        center_scale = TRUE,
+        cluster_rows = TRUE,
+        cluster_cols = TRUE,
+        show_rownames = TRUE,
+        show_colnames = TRUE,
+        annotation_legend = TRUE,
+        colnames = "assay",
+        fontsize = 10,
+        na_col = "black"
+      ),
+      list(
+        mat = npx_data_format_wide,
+        scale = "column",
+        silent = TRUE,
+        cluster_rows = TRUE,
+        cluster_cols = TRUE,
+        na_col = "black",
+        show_rownames = TRUE,
+        show_colnames = TRUE,
+        annotation_legend = TRUE,
+        fontsize = 10,
+        annotation_row = npx_data_format_clean |>
+            dplyr::select(
+              dplyr::all_of(
+                c("SampleID", "treatment2")
+              )
+            ) |>
+            dplyr::distinct() |>
+            tibble::column_to_rownames(
+              var = "SampleID"
+            ),
+        annotation_col = npx_data_format_clean |>
+            dplyr::select(
+              dplyr::all_of(
+                c("assay", "Assay_Warning")
+              )
+            ) |>
+            dplyr::distinct() |>
+            tibble::column_to_rownames(
+              var = "assay"
+            ),
+        annot_col_int = list(
+          Assay_Warning = olink_pal()(5L)[1L:2L],
+          treatment2 = olink_pal()(5L)[3L:5L]
+        )
+      )
+    )
 
-  expect_equal(
-    extract_ellipsis_arg(list(mat = df_wide,
-                              scale = "column",
-                              silent = TRUE,
-                              cluster_rows = TRUE,
-                              cluster_cols = TRUE,
-                              na_col = "black",
-                              show_rownames = TRUE,
-                              show_colnames = TRUE,
-                              annotation_legend = TRUE,
-                              fontsize = 10),
-                         annotation_colors = "something",
-                         cuttree_rows = 3L),
-    list(mat = df_wide,
-         scale = "column",
-         silent = TRUE,
-         cluster_rows = TRUE,
-         cluster_cols = TRUE,
-         na_col = "black",
-         show_rownames = TRUE,
-         show_colnames = TRUE,
-         annotation_legend = TRUE,
-         fontsize = 10,
-         cuttree_rows = 3L,
-         annot_col_int = "something")
-  )
-})
+    expect_equal(
+      object = plot_heatmap_pheatmap_args(
+        df_wide = npx_data_format_wide,
+        df = npx_data_format_clean,
+        check_log = check_log_oct,
+        variable_col_list = NULL,
+        variable_row_list = NULL,
+        center_scale = TRUE,
+        cluster_rows = TRUE,
+        cluster_cols = TRUE,
+        show_rownames = TRUE,
+        show_colnames = TRUE,
+        annotation_legend = TRUE,
+        colnames = "assay",
+        fontsize = 10,
+        na_col = "black",
+        cuttree_rows = 3L
+      ),
+      list(
+        mat = npx_data_format_wide,
+        scale = "column",
+        silent = TRUE,
+        cluster_rows = TRUE,
+        cluster_cols = TRUE,
+        na_col = "black",
+        show_rownames = TRUE,
+        show_colnames = TRUE,
+        annotation_legend = TRUE,
+        fontsize = 10,
+        cuttree_rows = 3L
+      )
+    )
+  }
+)
 
-test_that("annotate_heatmap - works", {
-  expect_equal(annotate_heatmap(df = df,
-                                check_log = check_log_oct,
-                                colnames = "assay",
-                                pheatmap_args = list(mat = df_wide,
-                                                     scale = "column",
-                                                     silent = TRUE,
-                                                     cluster_rows = TRUE,
-                                                     cluster_cols = TRUE,
-                                                     na_col = "black",
-                                                     show_rownames = TRUE,
-                                                     show_colnames = TRUE,
-                                                     annotation_legend = TRUE,
-                                                     fontsize = 10),
-                                variable_row_list = c("treatment2"),
-                                variable_col_list = c("Assay_Warning")),
-               list(mat = df_wide,
-                    scale = "column",
-                    silent = TRUE,
-                    cluster_rows = TRUE,
-                    cluster_cols = TRUE,
-                    na_col = "black",
-                    show_rownames = TRUE,
-                    show_colnames = TRUE,
-                    annotation_legend = TRUE,
-                    fontsize = 10,
-                    annotation_row = {
-                      df |>
-                        dplyr::select(SampleID, treatment2) |>
-                        dplyr::distinct() |>
-                        tibble::column_to_rownames("SampleID")
-                    },
-                    annotation_col = {
-                      df |>
-                        dplyr::select(assay, Assay_Warning) |>
-                        dplyr::distinct() |>
-                        tibble::column_to_rownames("assay")
-                    }))
-})
+# Test plot_heatmap_pheatmap_args ----
+
+test_that(
+  "pheatmap_extract_ellipsis_arg - works",
+  {
+    npx_data_format_clean <- plot_heatmap_clean_df(
+      df = npx_data_format,
+      check_log = check_log_oct,
+      colnames = "assay"
+    ) |>
+      suppressMessages() |>
+      suppressWarnings()
+    npx_data_format_wide <- plot_heatmap_df_to_wide(
+      df = npx_data_format_clean,
+      check_log = check_log_oct,
+      colnames = "assay"
+    )
+
+    expect_equal(
+      object = pheatmap_extract_ellipsis_arg(
+        list(
+          mat = npx_data_format_wide,
+          scale = "column",
+          silent = TRUE,
+          cluster_rows = TRUE,
+          cluster_cols = TRUE,
+          na_col = "black",
+          show_rownames = TRUE,
+          show_colnames = TRUE,
+          annotation_legend = TRUE,
+          fontsize = 10
+        ),
+        annotation_colors = "something"
+      ),
+      list(
+        mat = npx_data_format_wide,
+        scale = "column",
+        silent = TRUE,
+        cluster_rows = TRUE,
+        cluster_cols = TRUE,
+        na_col = "black",
+        show_rownames = TRUE,
+        show_colnames = TRUE,
+        annotation_legend = TRUE,
+        fontsize = 10,
+        annot_col_int = "something"
+      )
+    )
+
+    expect_equal(
+      object = pheatmap_extract_ellipsis_arg(
+        list(
+          mat = npx_data_format_wide,
+          scale = "column",
+          silent = TRUE,
+          cluster_rows = TRUE,
+          cluster_cols = TRUE,
+          na_col = "black",
+          show_rownames = TRUE,
+          show_colnames = TRUE,
+          annotation_legend = TRUE,
+          fontsize = 10
+        ),
+        annotation_colors = "something",
+        cuttree_rows = 3L
+      ),
+      list(
+        mat = npx_data_format_wide,
+        scale = "column",
+        silent = TRUE,
+        cluster_rows = TRUE,
+        cluster_cols = TRUE,
+        na_col = "black",
+        show_rownames = TRUE,
+        show_colnames = TRUE,
+        annotation_legend = TRUE,
+        fontsize = 10,
+        cuttree_rows = 3L,
+        annot_col_int = "something"
+      )
+    )
+  }
+)
+
+# Test pheatmap_annotate_heatmap ----
+
+test_that(
+  "pheatmap_annotate_heatmap - works",
+  {
+    npx_data_format_clean <- plot_heatmap_clean_df(
+      df = npx_data_format,
+      check_log = check_log_oct,
+      colnames = "assay"
+    ) |>
+      suppressMessages() |>
+      suppressWarnings()
+    npx_data_format_wide <- plot_heatmap_df_to_wide(
+      df = npx_data_format_clean,
+      check_log = check_log_oct,
+      colnames = "assay"
+    )
+
+    expect_equal(
+      object = pheatmap_annotate_heatmap(
+        df = npx_data_format_clean,
+        check_log = check_log_oct,
+        colnames = "assay",
+        pheatmap_args = list(
+          mat = npx_data_format_wide,
+          scale = "column",
+          silent = TRUE,
+          cluster_rows = TRUE,
+          cluster_cols = TRUE,
+          na_col = "black",
+          show_rownames = TRUE,
+          show_colnames = TRUE,
+          annotation_legend = TRUE,
+          fontsize = 10
+        ),
+        variable_row_list = c("treatment2"),
+        variable_col_list = c("Assay_Warning")
+      ),
+      list(
+        mat = npx_data_format_wide,
+        scale = "column",
+        silent = TRUE,
+        cluster_rows = TRUE,
+        cluster_cols = TRUE,
+        na_col = "black",
+        show_rownames = TRUE,
+        show_colnames = TRUE,
+        annotation_legend = TRUE,
+        fontsize = 10,
+        annotation_row = npx_data_format_clean |>
+            dplyr::select(
+              dplyr::all_of(
+                c("SampleID", "treatment2")
+              )
+            ) |>
+            dplyr::distinct() |>
+            tibble::column_to_rownames(
+              var = "SampleID"
+            ),
+        annotation_col = npx_data_format_clean |>
+            dplyr::select(
+              dplyr::all_of(
+                c("assay", "Assay_Warning")
+              )
+            ) |>
+            dplyr::distinct() |>
+            tibble::column_to_rownames(
+              var = "assay"
+            )
+      )
+    )
+  }
+)
