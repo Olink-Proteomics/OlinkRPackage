5422c09b4a26218a5b1ca33790fa173cbef6a8c8
klevdiamanti <kdiamanti@outlook.com>
2026-03-23 18:45:21 +0100

    Improve testing code

--- OlinkAnalyze/tests/testthat/test-npxProcessing.R
+++ OlinkAnalyze/tests/testthat/test-npxProcessing.R
@@ -5,7 +5,9 @@ test_that(
     reference_results <- get_example_data(filename = "reference_results.rds")
 
     npx_data1_uniqueid <- npx_data1 |>
-      dplyr::mutate(SampleID = paste0(SampleID, "_", Index))
+      dplyr::mutate(
+        SampleID = paste0(.data[["SampleID"]], "_", .data[["Index"]])
+      )
 
     check_log <- check_npx(df = npx_data1_uniqueid) |>
       suppressMessages() |>
@@ -31,8 +33,12 @@ test_that(
     expect_equal(
       object = proc_data$df_wide_matrix,
       expected = reference_results$preprocessing_dim_red |>
-        dplyr::select(!colors) |>
-        tibble::column_to_rownames(var = "SampleID") |>
+        dplyr::select(
+          -dplyr::all_of("colors")
+        ) |>
+        tibble::column_to_rownames(
+          var = "SampleID"
+        ) |>
         as.matrix()
     )
 
@@ -49,36 +55,37 @@ test_that(
     reference_results <- get_example_data(filename = "reference_results.rds")
 
     npx_data1_uniqueid <- npx_data1 |>
-      dplyr::mutate(SampleID = paste0(SampleID, "_", Index))
+      dplyr::mutate(
+        SampleID = paste0(.data[["SampleID"]], "_", .data[["Index"]])
+      )
 
     samples <- sample(
-      {
-        npx_data1_uniqueid$SampleID |> unique()
-      },
-      size = {
-        ceiling(
-          (npx_data1_uniqueid$SampleID |> unique() |> length()) * .15
-        )
-      }
+      x = npx_data1_uniqueid$SampleID |> unique(),
+      size = npx_data1_uniqueid$SampleID |>
+        unique() |>
+        length() |>
+        (\(.) . * 0.15)() |>
+        ceiling()
     )
 
     npx_data1_wth_missing <- npx_data1_uniqueid |>
       dplyr::mutate(
         # assays to be removed due to high missingness
-        NPX = ifelse(
-          SampleID %in% samples &
-            OlinkID %in% c("OID00482", "OID00483", "OID00484", "OID00485"),
-          NA,
-          NPX
+        NPX = dplyr::if_else(
+          .data[["SampleID"]] %in% .env[["samples"]] &
+            .data[["OlinkID"]] %in% c("OID00482", "OID00483",
+                                      "OID00484", "OID00485"),
+          NA_real_,
+          .data[["NPX"]]
         )
       ) |>
       # assays to be median imputed
       dplyr::mutate(
-        NPX = ifelse(
-          SampleID %in% c("A18_19", "B8_87") &
-            OlinkID %in% c("OID00562", "OID01213", "OID05124"),
-          NA,
-          NPX
+        NPX = dplyr::if_else(
+          .data[["SampleID"]] %in% c("A18_19", "B8_87") &
+            .data[["OlinkID"]] %in% c("OID00562", "OID01213", "OID05124"),
+          NA_real_,
+          .data[["NPX"]]
         )
       )
 
@@ -86,31 +93,22 @@ test_that(
       suppressMessages() |>
       suppressWarnings()
 
-    # run npxProcessing_forDimRed with missing data
-    warnings <- capture_warnings(
-      proc_data_wth_missing <- npxProcessing_forDimRed(
-        df = npx_data1_wth_missing,
-        check_log = check_log,
-        color_g = "QC_Warning",
-        drop_assays = FALSE,
-        drop_samples = FALSE,
-        verbose = TRUE
-      )
+    expect_warning(
+      object = expect_warning(
+        object = proc_data_wth_missing <- npxProcessing_forDimRed(
+          df = npx_data1_wth_missing,
+          check_log = check_log,
+          color_g = "QC_Warning",
+          drop_assays = FALSE,
+          drop_samples = FALSE,
+          verbose = TRUE
+        ),
+        regexp = "There are 4 assays dropped due to high missingness (>10%).",
+        fixed = TRUE
+      ),
+      regexp = "There are 3 assays were imputed using their median values.",
+      fixed = TRUE
     )
-
-    # test warnings
-    expect_true(any(
-      stringr::str_detect(
-        warnings,
-        "There are 4 assays dropped due to high missingness"
-      )
-    ))
-    expect_true(any(
-      stringr::str_detect(
-        warnings,
-        "There are 3 assays were imputed using their median values"
-      )
-    ))
 
     # test df_wide
     expect_equal(
@@ -122,13 +120,17 @@ test_that(
     expect_equal(
       object = proc_data_wth_missing$df_wide_matrix,
       expected = reference_results$preprocessing_dim_red_miss |>
-        dplyr::select(!colors) |>
-        tibble::column_to_rownames(var = "SampleID") |>
+        dplyr::select(
+          -dplyr::all_of("colors")
+        ) |>
+        tibble::column_to_rownames(
+          var = "SampleID"
+        ) |>
         as.matrix()
     )
 
     # expect null in dropped_assays.na
-    expect_null(proc_data_wth_missing$dropped_assays.na)
+    expect_null(object = proc_data_wth_missing$dropped_assays.na)
 
     # test dropped_assays.missingness
     expect_equal(
