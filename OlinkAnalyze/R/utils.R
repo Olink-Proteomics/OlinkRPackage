92476708bca6f57d0f99d837cc143487e6eef7ce
klevdiamanti <kdiamanti@outlook.com>
2026-04-17 14:37:03 +0200

    Move 'convert_read_npx_output' and 'convert_read_npx_output' from 'read_npx_utils.R' to 'utils.R'

--- OlinkAnalyze/R/utils.R
+++ OlinkAnalyze/R/utils.R
@@ -36,6 +36,87 @@ remove_all_na_cols <- function(df) {
   return(df)
 }
 
+#' Help function checking that the requested output class of the read_npx*
+#' functions is acceptable.
+#'
+#' @author
+#'   Klev Diamanti
+#'
+#' @inheritParams .read_npx_args
+#'
+#' @return Error if \var{out_df} is not one of
+#' `r ansi_collapse_quot(x = read_npx_df_output, sep = "and")`.
+#'
+#' @keywords internal
+#'
+check_out_df_arg <- function(out_df) {
+
+  # check that out_df is a string
+  check_is_scalar_character(x = out_df,
+                            error = TRUE)
+
+  if (!(out_df %in% read_npx_df_output)) {
+
+    cli::cli_abort( # nolint: return_linter
+      message = c(
+        "x" = "Unknown output argument {.arg out_df}!",
+        "i" = "Expected one of {.val {read_npx_df_output}}"
+      ),
+      call = rlang::caller_env(),
+      wrap = FALSE
+    )
+
+  }
+
+}
+
+#' Help function converting the output dataset from read_npx* functions to
+#' `r ansi_collapse_quot(x = get_df_output_print(), sep = "or")`.
+#'
+#' @author
+#'   Klev Diamanti
+#'
+#' @inheritParams .read_npx_args
+#' @inheritParams .downstream_fun_args
+#'
+#' @return The dataset in the requested class.
+#'
+#' @keywords internal
+#'
+convert_read_npx_output <- function(df,
+                                    out_df) {
+
+  # check that out_df is ok
+  check_out_df_arg(out_df = out_df)
+
+  if (check_is_dataset(x = df, error = FALSE)) {
+
+    if (out_df == "tibble") {
+
+      return(dplyr::as_tibble(df))
+
+    } else if (out_df == "arrow") {
+
+      return(arrow::as_arrow_table(df))
+
+    }
+
+  } else {
+
+    # if nont of the above throw an error
+    cli::cli_abort( # nolint: return_linter
+      message = c(
+        "x" = "Unexpected input dataset {.arg df}!",
+        "i" = "Expected one of: {.val {read_npx_df_output}}."
+      ),
+      call = rlang::caller_env(),
+      wrap = FALSE
+    )
+
+  }
+
+}
+
 #' Utility function that adds quotation marks on elements printed by
 #' ansi_collapse from cli.
 #'
