e9184af485aadc6f55a694876da5448268414170
kristianHoden <kristian.hoden@olink.com>
2026-05-05 14:02:53 +0200

    number_of_sample_w_more_than_one_color -> n_sample_w_multiple_colors

--- OlinkAnalyze/R/npxProcessing.R
+++ OlinkAnalyze/R/npxProcessing.R
@@ -62,7 +62,7 @@ npxProcessing_forDimRed <- function(df, # nolint: object_name_linter
 
     if (n_sample_w_multiple_colors > 0L) {
       cli::cli_abort(
-        "There are {number_of_sample_w_more_than_one_color} samples that do
+        "There are {n_sample_w_multiple_colors} samples that do
         not have a unique color. Only one color per sample is allowed."
       )
     } else {
