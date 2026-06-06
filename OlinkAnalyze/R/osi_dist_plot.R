d91a931ead5627f0433b69cc865ed0898617aaf4
klevdiamanti <kdiamanti@outlook.com>
2026-06-06 15:00:11 +0200

    `osi_dist_plot()` supports `olink_class`

--- OlinkAnalyze/R/osi_dist_plot.R
+++ OlinkAnalyze/R/osi_dist_plot.R
@@ -48,12 +48,11 @@
 olink_osi_dist_plot <- function(df,
                                 check_log = NULL,
                                 osi_score = NULL) {
-  check_log <- run_check_npx(df, check_log = check_log)
+  check_log <- get_check_npx(df = df, check_log = check_log)
 
   # General OSI checks
   df <- check_osi(
     df = df,
-    check_log = check_log,
     osi_score = osi_score
   )
 
