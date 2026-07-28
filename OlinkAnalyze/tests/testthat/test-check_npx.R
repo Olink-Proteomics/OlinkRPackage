10554c21b333f605874def965cb87d7b4189ebe4
klevdiamanti <kdiamanti@outlook.com>
2026-07-28 11:03:24 +0200

    Revert to checking full message in `check_npx`

--- OlinkAnalyze/tests/testthat/test-check_npx.R
+++ OlinkAnalyze/tests/testthat/test-check_npx.R
@@ -362,7 +362,9 @@ test_that(
                            preferred_names = NULL),
         expected = expected_result
       ),
-      regexp = "\"DataAnalysisRefID\": \"D10007\""
+      regexp = paste("\"DataAnalysisRefID\": \"D10007\", \"D20007\",",
+                     "\"D30007\", and \"D40007\"; \"PanelDataArchiveVersion\":",
+                     "\"1.2.1\"")
     )
 
   }
@@ -2091,7 +2093,8 @@ test_that(
                              col_names = col_names),
         expected = expected_result
       ),
-      regexp = "\"DataAnalysisRefID\": \"D10007\""
+      regexp = paste("\"DataAnalysisRefID\": \"D10007\", \"D20007\", and",
+                     "\"D30007\"; \"PanelDataArchiveVersion\": \"1.2.0\"")
     )
   }
 )
